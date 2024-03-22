993058f307c62f94fa51349f2aa9cc8b40194757
Ubuntu <ubuntu@ip-10-240-255-43.us-west-2.compute.internal>
2024-03-22 09:29:53 +0000

    Add Doxygen comments to template aliases in types.cuh

--- src/types/types.cuh
+++ src/types/types.cuh
@@ -5,9 +5,29 @@
 
 /* ----------  WRAPPERS FOR PRETTINESS  ---------- */
 
-// The point of these two wrappers is to enable the following syntax:
-// kittens::col_vec<decltype(some_tile)> tile_accumulator;
+/**
+ * @file types.cuh
+ * @brief Wrappers for vector types to enable cleaner syntax in kernel code.
+ */
+
+/**
+ * @brief Wrapper to enable syntax for creating a row vector from a tile type.
+ * 
+ * This wrapper allows the following syntax:
+ * kittens::row_vec<decltype(some_tile)> tile_accumulator;
+ * 
+ * @tparam T The tile type from which to create a row vector.
+ */
 template<typename T> using row_vec = T::row_vec;
+
+/**
+ * @brief Wrapper to enable syntax for creating a column vector from a tile type.
+ * 
+ * This wrapper allows the following syntax:
+ * kittens::col_vec<decltype(some_tile)> tile_accumulator;
+ * 
+ * @tparam T The tile type from which to create a column vector.
+ */
 template<typename T> using col_vec = T::col_vec;
 
 // ^ this code lives here because it applies to both sv and rv types
