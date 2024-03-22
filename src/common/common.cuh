f50a9e225772e195d3c59d689dfb184588bd24e7
Ubuntu <ubuntu@ip-10-240-255-43.us-west-2.compute.internal>
2024-03-22 05:29:23 +0000

    Add file-level Doxygen comment to common.cuh

--- src/common/common.cuh
+++ src/common/common.cuh
@@ -1,5 +1,14 @@
 #pragma once
 
+/**
+ * @file common.cuh
+ * @brief Includes common utilities, base type aliases, and operations for the project.
+ *
+ * This header includes essential utilities, type definitions, and operations that are
+ * widely used throughout the project. It serves as a convenience include for other
+ * parts of the codebase to access common functionality.
+ */
+
 #include "util.cuh"
 #include "base_types.cuh"
 #include "base_ops.cuh"
