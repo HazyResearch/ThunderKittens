c29d85a6873a9fc83f75cac6d2c6e383172a65f7
Ubuntu <ubuntu@ip-10-240-255-43.us-west-2.compute.internal>
2024-03-22 05:46:22 +0000

    Add Doxygen comments to ops.cuh

--- src/ops/ops.cuh
+++ src/ops/ops.cuh
@@ -1,3 +1,13 @@
+/**
+ * @file ops.cuh
+ * @brief Header file that includes all operation-related headers.
+ *
+ * This file acts as an aggregate header to include all the operation
+ * categories like warp, warpgroup, and block operations. It simplifies
+ * the inclusion process for files that require access to multiple
+ * operation-related functionalities.
+ */
+
 #pragma once
 
 #include "warp/warp.cuh"
