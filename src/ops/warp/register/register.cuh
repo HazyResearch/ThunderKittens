ee921b9919e3491fb3ad8f2b67a996da8d00172e
Ubuntu <ubuntu@ip-10-240-255-43.us-west-2.compute.internal>
2024-03-22 07:37:50 +0000

    Add file-level Doxygen comment to register.cuh

--- src/ops/warp/register/register.cuh
+++ src/ops/warp/register/register.cuh
@@ -1,5 +1,15 @@
 #pragma once
 
+/**
+ * @file register.cuh
+ * @brief Aggregates includes for register-level operations within CUDA kernels.
+ *
+ * This file includes a set of headers that provide functionality for register-level
+ * operations used in warp-level computations. It serves as a convenience header to
+ * include common operations such as conversions, vector operations, mappings,
+ * reductions, and matrix multiply-accumulate operations at the register level.
+ */
+
 #include "conversions.cuh"
 #include "vec.cuh"
 #include "maps.cuh"
