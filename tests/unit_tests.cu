1752c1bb93bee3d258f47253b04535f8c5b8b995
Benjamin Spector <benjaminfspector@gmail.com>
2024-03-25 02:46:37 -0700

    moving towards a tempalted testing scheme, 1900 tests in

--- tests/unit_tests.cu
+++ tests/unit_tests.cu
@@ -1,68 +1,52 @@
-#define WIDTH 2
-#define HEIGHT 4
-#define WARPSIZE 32
-
 #include "testing_commons.cuh"
 
-#include "warp/mem_tests.impl"
-#include "warp/st_vec_tests.impl"
-#include "warp/rt_layout_tests.impl"
-#include "warp/st_layout_tests.impl"
-#include "warp/st_subtile_tests.impl"
-#include "warp/map_tests.impl"
-#include "warp/reduction_tests.impl"
-#include "warp/broadcast_tests.impl"
-#include "warp/mma_tests.impl"
-#include "block/global_to_shared.impl"
+#include "warp/warp.impl"
+// #include "warp/st_vec_tests.impl"
+// #include "warp/rt_layout_tests.impl"
+// #include "warp/st_layout_tests.impl"
+// #include "warp/st_subtile_tests.impl"
+// #include "warp/map_tests.impl"
+// #include "warp/reduction_tests.impl"
+// #include "warp/broadcast_tests.impl"
+// #include "warp/mma_tests.impl"
+// #include "block/global_to_shared.impl"
 
-#ifdef KITTENS_HOPPER
-#include "warp/tma_tests.impl"
+// #ifdef KITTENS_HOPPER
+// #include "warp/tma_tests.impl"
 
-#include "warpgroup/wgmma_tests.impl"
-#include "warpgroup/tall_wgmma_tests.impl"
+// #include "warpgroup/wgmma_tests.impl"
+// #include "warpgroup/tall_wgmma_tests.impl"
 
-#include "block/dsmem.impl"
+// #include "block/dsmem.impl"
 
-#include "integration/wgmma_tma_tests.impl"
-#endif
+// #include "integration/wgmma_tma_tests.impl"
+// #endif
 
 int main(int argc, char **argv) {
 
     should_write_outputs = argc>1; // write outputs if user says so
 
-    int failures = 0;
-    
-    std::cout << " ---------------  BEGINNING WARP TESTS  ---------------\n";
-    failures += mem_tests();
-    failures += st_vec_tests();
-    failures += rt_layout_tests();
-    failures += st_layout_tests();
-    failures += st_subtile_tests();
-    failures += map_tests();
-    failures += reduction_tests();
-    failures += broadcast_tests();
-    failures += mma_tests();
-#ifdef KITTENS_HOPPER
-    failures += tma_tests();
-#endif
-    std::cout << " ---------------  BEGINNING WARPGROUP TESTS  ---------------\n";
-#ifdef KITTENS_HOPPER
-    failures += wgmma_tests();
-    failures += tall_wgmma_tests();
-#endif
-    std::cout << " ---------------  BEGINNING BLOCK TESTS  ---------------\n";
-    failures += block_mem_tests();
-#ifdef KITTENS_HOPPER
-    failures += dsmem_tests();
-#endif
-    std::cout << " ---------------  BEGINNING INTEGRATION TESTS  ---------------\n";
-#ifdef KITTENS_HOPPER
-    failures += wgmma_tma_tests();
-#endif
+    test_data data;
+
+    warp::tests(data);
 
     std::cout << " ---------------  SUMMARY  ---------------\n";
-    if(failures == 0) std::cout << "ALL TESTS PASSED!\n";
-    else std::cout << failures << " TESTS FAILED :(\n";
+
+    std::cout << "Failed tests:\n";
+    int passes = 0, fails = 0, invalids = 0;
+    for(auto it = data.begin(); it != data.end(); it++) {
+        if(it->result == test_result::PASSED)  passes++;
+        if(it->result == test_result::INVALID) invalids++;
+        if(it->result == test_result::FAILED) {
+            fails++;
+            std::cout << it->label << std::endl;
+        }
+    }
+    if(fails == 0) std::cout << "ALL TESTS PASSED!\n";
+
+    std::cout << passes   << " tests passed\n";
+    std::cout << fails    << " tests failed\n";
+    std::cout << invalids << " tests skipped (this is normal)\n";
 
     return 0;
 }
