8d41b7ceed3dc9d52e33d5982024b4ae82c28a9f
Benjamin Spector <benjaminfspector@gmail.com>
2024-04-01 21:37:30 -0700

    Update mma.cu

--- tests/warpgroup/wgmma/mma.cu
+++ tests/warpgroup/wgmma/mma.cu
@@ -11,21 +11,21 @@ void warpgroup::wgmma::mma::tests(test_data &results) {
     std::cout << "\n ----- Starting ops/warpgroup/wgmma/mma tests! -----\n" << std::endl;
     constexpr int SIZE = 4;
     mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I1_t, wgmma_row_0b , wgmma_col_t_0b >::run(results);
-    // // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I1_t, wgmma_row_0b , wgmma_col_t_32b>::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I1_t, wgmma_row_32b, wgmma_col_t_0b >::run(results);
-    // // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I1_t, wgmma_row_32b, wgmma_col_t_32b>::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_0b , wgmma_col_t_0b >::run(results);
-    // // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_0b , wgmma_col_t_32b>::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_32b, wgmma_col_t_0b >::run(results);
-    // // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_32b, wgmma_col_t_32b>::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_0b , wgmma_row_0b >::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_0b , wgmma_row_32b>::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_32b, wgmma_row_0b >::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_32b, wgmma_row_32b>::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_0b , wgmma_row_0b >::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_0b , wgmma_row_32b>::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_32b, wgmma_row_0b >::run(results);
-    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_32b, wgmma_row_32b>::run(results);
+    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I1_t, wgmma_row_0b , wgmma_col_t_32b>::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I1_t, wgmma_row_32b, wgmma_col_t_0b >::run(results);
+    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I1_t, wgmma_row_32b, wgmma_col_t_32b>::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_0b , wgmma_col_t_0b >::run(results);
+    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_0b , wgmma_col_t_32b>::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_32b, wgmma_col_t_0b >::run(results);
+    // mma_sweep_width_warpgroup<warpgroup::wgmma::mma::mma, SIZE, I3_t, wgmma_row_32b, wgmma_col_t_32b>::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_0b , wgmma_row_0b >::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_0b , wgmma_row_32b>::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_32b, wgmma_row_0b >::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I1_t, wgmma_row_32b, wgmma_row_32b>::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_0b , wgmma_row_0b >::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_0b , wgmma_row_32b>::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_32b, wgmma_row_0b >::run(results);
+    mma_sweep_width_warpgroup<warpgroup::wgmma::mma::dot, SIZE, I3_t, wgmma_row_32b, wgmma_row_32b>::run(results);
 }
 
 #endif
