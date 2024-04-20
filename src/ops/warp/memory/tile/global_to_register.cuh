d25eafc4b2b5f3bc9b6797190e8303f28e0db622
Benjamin Spector <benjaminfspector@gmail.com>
2024-04-20 01:26:19 -0700

    put back in improved global to register coalescing

--- src/ops/warp/memory/tile/global_to_register.cuh
+++ src/ops/warp/memory/tile/global_to_register.cuh
@@ -23,21 +23,31 @@ template<ducks::rt::row_layout RT, typename U>
 __device__ inline static void load(RT &dst, const U *src, const int row_stride) {
     using T2 = RT::dtype;
     using U2 = base_types::packing<U>::packed_type;
-    int laneid = threadIdx.x % 32;
+    int laneid = kittens::laneid();
+    int warphalf = (laneid & 16) > 0;
+    int warphalflaneid = laneid % 16;
     #pragma unroll
     for(int i = 0; i < dst.height; i++) {
-        int row = i*dst.tile_size + (laneid / 4);
+        int row_0to3 = i*dst.tile_size + (warphalflaneid / 4);
         #pragma unroll
         for(int j = 0; j < dst.width; j++) {
-            int col = j*dst.tile_size + 2*(laneid % 4);
-            dst.tiles[i][j].data[0] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row+0)*row_stride + (col+0)]));
-            dst.tiles[i][j].data[2] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row+0)*row_stride + (col+8)]));
+            int col = j*dst.tile_size + warphalf*8 + 2*(laneid % 4);
+            T2 transfers[2];
+            transfers[0] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row_0to3+0)*row_stride + col]));
+            transfers[1] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row_0to3+4)*row_stride + col]));
+            transfers[1-warphalf] = packed_shfl_sync(MASK_ALL, transfers[1-warphalf], laneid^16);
+            dst.tiles[i][j].data[0] = transfers[0];
+            dst.tiles[i][j].data[2] = transfers[1];
         }
         #pragma unroll
         for(int j = 0; j < dst.width; j++) {
-            int col = j*dst.tile_size + 2*(laneid % 4);
-            dst.tiles[i][j].data[1] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row+8)*row_stride + (col+0)]));
-            dst.tiles[i][j].data[3] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row+8)*row_stride + (col+8)]));
+            int col = j*dst.tile_size + warphalf*8 + 2*(laneid % 4);
+            T2 transfers[2];
+            transfers[0] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row_0to3+ 8)*row_stride + col]));
+            transfers[1] = base_types::convertor<T2, U2>::convert(*(U2*)(&src[(row_0to3+12)*row_stride + col]));
+            transfers[1-warphalf] = packed_shfl_sync(MASK_ALL, transfers[1-warphalf], laneid^16);
+            dst.tiles[i][j].data[1] = transfers[0];
+            dst.tiles[i][j].data[3] = transfers[1];
         }
     }
 }
@@ -97,21 +107,32 @@ template<ducks::rt::row_layout RT, typename U>
 __device__ inline static void store(U *dst, const RT &src, const int row_stride) {
     using T2 = RT::dtype;
     using U2 = base_types::packing<U>::packed_type;
-    int laneid = threadIdx.x % 32;
+    int laneid = kittens::laneid();
+    int warphalf = (laneid & 16) > 0;
+    int warphalflaneid = laneid % 16;
     #pragma unroll
     for(int i = 0; i < src.height; i++) {
+        int row_0to3 = i*src.tile_size + (warphalflaneid / 4);
         int row = i*src.tile_size + (laneid / 4);
         #pragma unroll
         for(int j = 0; j < src.width; j++) {
-            int col = j*src.tile_size + 2*(laneid % 4);
-            *(U2*)(&dst[(row+0)*row_stride + (col+0)]) = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[0]);
-            *(U2*)(&dst[(row+0)*row_stride + (col+8)]) = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[2]);
+            int col = j*src.tile_size + warphalf*8 + 2*(laneid % 4);
+            U2 transfers[2];
+            transfers[0] = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[0]);
+            transfers[1] = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[2]);
+            transfers[1-warphalf] = packed_shfl_sync(MASK_ALL, transfers[1-warphalf], laneid^16);
+            *(U2*)(&dst[(row_0to3+0)*row_stride + col]) = transfers[0];
+            *(U2*)(&dst[(row_0to3+4)*row_stride + col]) = transfers[1];
         }
         #pragma unroll
         for(int j = 0; j < src.width; j++) {
-            int col = j*src.tile_size + 2*(laneid % 4);
-            *(U2*)(&dst[(row+8)*row_stride + (col+0)]) = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[1]);
-            *(U2*)(&dst[(row+8)*row_stride + (col+8)]) = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[3]);
+            int col = j*src.tile_size + warphalf*8 + 2*(laneid % 4);
+            U2 transfers[2];
+            transfers[0] = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[1]);
+            transfers[1] = base_types::convertor<U2, T2>::convert(src.tiles[i][j].data[3]);
+            transfers[1-warphalf] = packed_shfl_sync(MASK_ALL, transfers[1-warphalf], laneid^16);
+            *(U2*)(&dst[(row_0to3+ 8)*row_stride + col]) = transfers[0];
+            *(U2*)(&dst[(row_0to3+12)*row_stride + col]) = transfers[1];
         }
     }
 }
