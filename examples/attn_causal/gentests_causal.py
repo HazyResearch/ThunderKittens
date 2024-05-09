448a6994e2d02eeb15a8e5b93514a58562ed3513
Aaryan0404 <aaryan0404@gmail.com>
2024-05-09 04:58:12 -0700

    h100 fwd causal - 335

--- examples/attn_causal/gentests_causal.py
+++ examples/attn_causal/gentests_causal.py
@@ -54,3 +54,55 @@ with open(f'{TESTNAME}_causal_{N}N_{D}D.txt', 'w') as f:
         f.write(' ')
 
 print(f'Run the harness like `cat {TESTNAME}.txt | ./harness`')
+
+def flops(batch, seqlen, headdim, nheads, causal, mode="fwd"):
+    assert mode in ["fwd", "bwd", "fwd_bwd"]
+    f = 4 * batch * seqlen**2 * nheads * headdim // (2 if causal else 1)
+    return f if mode == "fwd" else (2.5 * f if mode == "bwd" else 3.5 * f)
+
+def efficiency(flop, time):
+    # calculate in teraflops
+    flop = flop / 1e12
+    time = time / 1e6
+    return flop / time
+
+B = 32
+H = 16
+N = int(sys.argv[1])
+D = int(sys.argv[2])
+
+print("-" * 60)
+print(f'Timing forward pass for for B={B}, H={H}, N={N}, D={D}')
+with torch.backends.cuda.sdp_kernel(
+    enable_flash=True, 
+    enable_math=False, 
+    enable_mem_efficient=False
+):
+    q = torch.randn((B, H, N, D), dtype=torch.float16, device='cuda').requires_grad_()
+    k = torch.randn((B, H, N, D), dtype=torch.float16, device='cuda').requires_grad_()
+    v = torch.randn((B, H, N, D), dtype=torch.float16, device='cuda').requires_grad_()
+
+    # warmup
+    for _ in range(10):
+        o = torch.nn.functional.scaled_dot_product_attention(q, k, v, is_causal=True)
+    
+    # Prepare for timing
+    start_events = [torch.cuda.Event(enable_timing=True) for _ in range(30)]
+    end_events = [torch.cuda.Event(enable_timing=True) for _ in range(30)]
+
+    # Time the forward pass
+
+    for i in range(30):
+        start_events[i].record()
+        torch.cuda.synchronize()
+        o = torch.nn.functional.scaled_dot_product_attention(q, k, v, is_causal=True)
+        torch.cuda.synchronize()
+        end_events[i].record()
+    
+torch.cuda.synchronize()
+times = [s.elapsed_time(e) for s, e in zip(start_events, end_events)]
+time_us = np.mean(times) * 1000
+print(f'Average time for forward pass in us: {time_us:.2f}')
+print(f'Average efficiency for forward pass in TFLOPS: {efficiency(flops(B, N, D, H, True, "fwd"), time_us):.2f}')
+
+print("-" * 60)
--- examples/attn_causal/h100_fwd.cu
+++ examples/attn_causal/h100_fwd.cu
@@ -1,92 +1,137 @@
-#include "../../src/kittens.cuh"
+#include "../../src/kittens.cuh" // for harness_h100_fwd.impl
+#include <cuda/pipeline>
+#include <cooperative_groups.h>
 
-#define NUM_WORKERS 8
+#define NUM_WORKERS (8)
+#define NUM_WARPGROUPS (NUM_WORKERS/(kittens::WARPGROUP_WARPS))
+#define NUM_WORKERS_KV (1)
+
 using namespace kittens;
-__global__ __launch_bounds__((NUM_WORKERS)*kittens::WARP_THREADS, 1)
-void attend_ker(int n, int d, const bf16* __restrict__ __q__, const bf16* __restrict__ __k__, const bf16* __restrict__ __v__, bf16* __o__) {
 
-    auto warpid        = kittens::warpid();
-    auto block_start   = blockIdx.x*(n*64);
-    const bf16 *_q = __q__ + block_start, *_k = __k__ + block_start, *_v = __v__ + block_start;
-          bf16 *_o = __o__ + block_start;
+using layout_q = kittens::ducks::st_layout::wgmma_swizzle; 
+using layout_k = kittens::ducks::st_layout::wgmma_swizzle; 
+using layout_v = kittens::ducks::st_layout::wgmma_interleave; 
+using layout_o = kittens::ducks::st_layout::swizzle;
 
-    extern __shared__ alignment_dummy __shm[]; // this is the CUDA shared memory
-    shared_allocator al((int*)&__shm[0]);
-    
-    st_bf_2x4<ducks::st_layout::swizzle> (&k_smem)[NUM_WORKERS] = al.allocate<st_bf_2x4<ducks::st_layout::swizzle>, NUM_WORKERS>();
-    st_bf_2x4<ducks::st_layout::swizzle> (&v_smem)[NUM_WORKERS] = al.allocate<st_bf_2x4<ducks::st_layout::swizzle>, NUM_WORKERS>();
+__global__  __launch_bounds__((NUM_WORKERS)*kittens::WARP_THREADS, 2)
+void fwd_attend_ker_dim64(int N, const CUtensorMap* tma_q, const CUtensorMap* tma_k, const CUtensorMap* tma_v, CUtensorMap* tma_o) {
+    extern __shared__ int __shm[]; // this is the CUDA shared memory
+    tma_swizzle_allocator al((int*)&__shm[0]);
 
-    rt_bf<2, 4> q_reg, k_reg, v_reg;
-    rt_fl<2, 2> att_block;
-    rt_bf<2, 2> att_block_mma;
-    rt_fl<2, 4> o_prev;
-    rt_fl<2, 2>::col_vec max_vec_last, max_vec;
-    rt_fl<2, 2>::col_vec norm_vec_last, norm_vec;
-    
-    int qo_blocks = n / (q_reg.rows*NUM_WORKERS); 
-    int kv_blocks = n / (q_reg.rows*NUM_WORKERS);
+    st_bf<4, 4, layout_q>          (&q_smem)   [NUM_WARPGROUPS] = al.allocate<st_bf<4, 4, layout_q>,          NUM_WARPGROUPS>();
+    st_bf<4, 4, layout_k>          (&k_smem)[2][NUM_WORKERS_KV] = al.allocate<st_bf<4, 4, layout_k>, 2,       NUM_WORKERS_KV>();
+    st_bf<4, 4, layout_v>          (&v_smem)[2][NUM_WORKERS_KV] = al.allocate<st_bf<4, 4, layout_v>, 2,       NUM_WORKERS_KV>();
 
-    for(auto q_blk = 0; q_blk < qo_blocks; q_blk++) {
+    int tic = 0, toc = 1;
+ 
+    rt_fl<1, 4> att_block;
+    rt_bf<1, 4> att_block_mma;
+    rt_fl<1, 4> o_prev;
+    rt_fl<1, 4>::col_vec max_vec_last, max_vec;
+    rt_fl<1, 4>::col_vec norm_vec_last, norm_vec;
 
-        int qo_index = q_blk*NUM_WORKERS + warpid;
+    int warpid      = kittens::warpid();
+    int warpgroupid = warpid/kittens::WARPGROUP_WARPS;
 
-        load(q_reg, _q + (qo_index)*q_reg.num_elements, q_reg.cols);
-        mul(q_reg, q_reg, __float2bfloat16(0.125f)); // temperature adjustment
+    int qo_index    = (blockIdx.x * NUM_WARPGROUPS) + warpgroupid;
 
-        neg_infty(max_vec); // zero registers for the Q chunk
-        zero(norm_vec);
-        zero(o_prev);
+    int kv_blocks = N / (NUM_WORKERS_KV*k_smem[0][0].rows);
 
-        for(auto kv_idx = 0; kv_idx < kv_blocks; kv_idx++) {
+    __shared__ uint64_t qsmem_barrier, kvsmem_barrier;//, vsmem_barrier;
 
-            load(v_smem[warpid], _v + (kv_idx*NUM_WORKERS + warpid)*q_reg.num_elements, q_reg.cols);
-            load(k_smem[warpid], _k + (kv_idx*NUM_WORKERS + warpid)*q_reg.num_elements, q_reg.cols);
-            __syncthreads(); // we need to make sure all memory is loaded before we can begin the compute phase
+    int q_phasebit = 0;
+    int kv_phasebit = 0;
 
-            for(int subtile = 0; 
-                subtile <= (qo_index - (kv_idx*NUM_WORKERS)) && subtile < NUM_WORKERS; 
-                subtile++) 
-            {
-                load(k_reg, k_smem[subtile]);
+    if (threadIdx.x == 0) {
+        tma::init_barrier<st_bf<4, 4, layout_q>, NUM_WARPGROUPS>(qsmem_barrier, 1);
+        tma::init_barrier<st_bf<4, 4, layout_k>, NUM_WORKERS_KV*2>(kvsmem_barrier, 1); 
+    }
+
+    if (warpid == 0) {
+        for (int wg = 0; wg < NUM_WORKERS/kittens::WARPGROUP_WARPS; wg++) { // load q
+            int tile_idx = (blockIdx.y * NUM_WARPGROUPS * gridDim.x) + (blockIdx.x * NUM_WARPGROUPS) + wg;
+            tma::load_async((q_smem[wg]), tma_q, qsmem_barrier, tile_idx); 
+        }
+        for (int w = 0; w < NUM_WORKERS_KV; w++) { // load k, v      
+            int tile_idx = (blockIdx.y * NUM_WORKERS_KV * kv_blocks) + (0 * NUM_WORKERS_KV) + w; 
+            tma::load_async((k_smem[tic][w]), tma_k, kvsmem_barrier, tile_idx); 
+            tma::load_async((v_smem[tic][w]), tma_v, kvsmem_barrier, tile_idx); 
+        }
+    }
+
+    neg_infty(max_vec); // zero registers for the Q chunk
+    zero(norm_vec);
+    zero(o_prev);
+    __syncthreads();
+
+    tma::arrive_and_wait(qsmem_barrier, q_phasebit);
+    q_phasebit ^= 1;
+
+    warpgroup::mul(q_smem[warpgroupid], q_smem[warpgroupid], __float2bfloat16(0.125f));
+
+    for (auto kv_idx = 0; kv_idx <= qo_index; kv_idx++, tic ^= 1, toc ^= 1) {
+        tma::arrive_and_wait(kvsmem_barrier, kv_phasebit);
+        kv_phasebit ^= 1;
+
+        __syncthreads();
+        if (warpid == 0) {
+            if (kv_idx + 1 < kv_blocks) {
+                tma::set_bytes(kvsmem_barrier, 2 * NUM_WORKERS_KV * k_smem[0][0].num_elements * sizeof(bf16));
                 
-                zero(att_block);
-                mma_ABt(att_block, q_reg, k_reg, att_block);
-
-                if ((kv_idx*NUM_WORKERS + subtile) == qo_index) 
-                {
-                    make_causal(att_block, att_block, -INFINITY);
+                for (int w = 0; w < NUM_WORKERS_KV; w++) {        
+                    int tile_idx = (blockIdx.y * NUM_WORKERS_KV * kv_blocks) + ((kv_idx + 1) * NUM_WORKERS_KV) + w; 
+                    tma::load_async((k_smem[toc][w]), tma_k, kvsmem_barrier, tile_idx); 
+                    tma::load_async((v_smem[toc][w]), tma_v, kvsmem_barrier, tile_idx);
                 }
-
-                copy(norm_vec_last, norm_vec);
-                copy(max_vec_last,  max_vec);
-
-                row_max(max_vec, att_block, max_vec); // accumulate onto the max_vec
-                sub_row(att_block, att_block, max_vec);
-                exp(att_block, att_block);
-
-                sub(max_vec_last, max_vec_last, max_vec);
-                exp(max_vec_last, max_vec_last);
-                mul(norm_vec, norm_vec, max_vec_last);
-
-                row_sum(norm_vec, att_block, norm_vec); // accumulate onto the norm_vec
-                div_row(att_block, att_block, norm_vec);
-
-                mul(norm_vec_last, norm_vec_last, max_vec_last);
-                div(norm_vec_last, norm_vec_last, norm_vec);
-
-                copy(att_block_mma, att_block); // convert to bf16 for mma_AB
-
-                load(v_reg, v_smem[subtile]);
-                rt_bf<2, 4, ducks::rt_layout::col> &v_reg_col = swap_layout_inplace(v_reg); // this is a reference and the call has invalidated v_reg
-
-                mul_row(o_prev, o_prev, norm_vec_last); // normalize o_prev in advance of mma_AB'ing onto it
-                mma_AB(o_prev, att_block_mma, v_reg_col, o_prev);
             }
-            __syncthreads(); // we need to make sure all warps are done before we can start loading the next kv chunk
         }
 
-        store(_o + (q_blk*NUM_WORKERS + warpid)*q_reg.num_elements, o_prev, d); // write out o. compiler has an issue with register usage if d is made constexpr q_reg.rows :/
+        warpgroup::mma_fence(att_block);
+        warpgroup::mm_ABt(att_block, q_smem[warpgroupid], k_smem[tic][0]);
+        warpgroup::mma_commit_group();
+
+        copy(norm_vec_last, norm_vec);
+        copy(max_vec_last,  max_vec);
+
+        warpgroup::mma_async_wait();
+
+        if (kv_idx == qo_index) {
+            wg_make_causal(att_block, att_block, -INFINITY); 
+        }
+
+        row_max(max_vec, att_block, max_vec); // accumulate onto the max_vec
+        sub_row(att_block, att_block, max_vec);
+        exp(att_block, att_block);
+
+        sub(max_vec_last, max_vec_last, max_vec);
+        exp(max_vec_last, max_vec_last);
+        mul(norm_vec, norm_vec, max_vec_last);
+
+        row_sum(norm_vec, att_block, norm_vec); // accumulate onto the norm_vec
+        div_row(att_block, att_block, norm_vec);
+
+        mul(norm_vec_last, norm_vec_last, max_vec_last);
+        div(norm_vec_last, norm_vec_last, norm_vec);
+
+        copy(att_block_mma, att_block); // convert to bf16 for mma
+        mul_row(o_prev, o_prev, norm_vec_last); // normalize o_prev in advance of mma'ing onto it
+
+        warpgroup::mma_fence(o_prev);
+        warpgroup::mma_AB(o_prev, att_block_mma, v_smem[tic][0]);
+        warpgroup::mma_commit_group();
     }
+
+    auto (*o_smem) = reinterpret_cast<st_bf<4, 4, layout_o>(*)>(q_smem); // reuse q memory
+    warpgroup::store(o_smem[warpgroupid], o_prev); 
+    __syncthreads();
+    
+    if (warpid % 4 == 0) { // store o
+        int tile_idx = (blockIdx.y * NUM_WARPGROUPS * gridDim.x) + (blockIdx.x * NUM_WARPGROUPS) + warpgroupid;
+        tma::store_async(tma_o, (o_smem[warpgroupid]), tile_idx); 
+        tma::store_commit_group(); 
+    }
+
+    tma::store_async_wait();
 }
 
 #include "harness_h100_fwd.impl"
