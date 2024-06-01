badf5f0160c37d8da80fcf8825b4c5ba8d82e0ec
zhang677 <zgh23@stanford.edu>
2024-06-01 22:58:38 +0000

    Wrong results

--- examples/M1_loop_one_body/tk_m1_prefill.cu
+++ examples/M1_loop_one_body/tk_m1_prefill.cu
@@ -25,8 +25,7 @@ void prefill_ker(
         const T* __XA, const T* __XB, const T* __XC,
         T* __Output
 ) {
-
-    const H *_W1       = reinterpret_cast<const H*>(__W1)+blockIdx.x*(HF*HF);
+    H *_W1       = reinterpret_cast<H*>(__W1)+blockIdx.x*(HF*HF);
     const H *_XA       = reinterpret_cast<const H*>(__XA)+blockIdx.x*(CS*HF);
     const H *_XB       = reinterpret_cast<const H*>(__XB)+blockIdx.x*(CS*HF);
     const H *_XC       = reinterpret_cast<const H*>(__XC)+blockIdx.x*(CS*HF);
@@ -35,53 +34,64 @@ void prefill_ker(
     /*********
     REGISTER
     **********/
-//    rt_bf<4, 4, kittens::ducks::rt_layout::col> W1_reg;
+    rt_bf<4, 4, kittens::ducks::rt_layout::col> W1_reg;
     rt_bf<1, 4> XA_reg;
-//    rt_bf<1, 4> XB_reg;
-//    rt_bf<1, 4> XC_reg;
+    rt_bf<1, 4> XB_reg;
+    rt_bf<1, 4> XC_reg;
 
-//    rt_fl<1, 4> Z1_fl_reg;
-//    rt_bf<1, 4> Z1_reg;
+    rt_fl<1, 4> Z1_fl_reg;
+    rt_bf<1, 4> Z1_reg;
 
-//    rt_bf<1, 4> Output_reg;
-//    rt_fl<1, 4> Z1_bar_term_1_fl_reg;
-//    rt_bf<1, 4> Z1_bar_term_1_reg;
-//    rt_fl<1, 4> Z1_bar_term_2_fl_reg;
-//    rt_bf<1, 4> Z1_bar_term_2_reg;
-//    rt_fl<1, 1> Attn1_fl_reg;
-//    rt_bf<1, 1> Attn1_reg;
+    rt_bf<1, 4> Output_reg;
+    rt_fl<1, 4> Output_fl_reg;
+    rt_fl<1, 4> Z1_bar_term_1_fl_reg;
+    rt_fl<1, 4> Z1_bar_term_2_fl_reg;
+    rt_fl<1, 1> Attn1_fl_reg;
+    rt_bf<1, 1> Attn1_reg;
 
-//    load(W1_reg, _W1, W1_reg.cols);
-//    load(XB_reg, _XB, XB_reg.cols);
+    load(W1_reg, _W1, W1_reg.cols);
+    load(XB_reg, _XB, XB_reg.cols);
     load(XA_reg, _XA, XA_reg.cols);
-//    load(XC_reg, _XC, XC_reg.cols);
+    load(XC_reg, _XC, XC_reg.cols);
 
-//    store(_Output, XA_reg, XA_reg.cols); // debug kernel launch overhead
+    zero(Z1_fl_reg);
+    mma_AB(Z1_fl_reg, XB_reg, W1_reg, Z1_fl_reg); // [K,f] r, [f,f] c -> [K,f] r
 
-//    zero(Z1_fl_reg);
-//    mma_AB(Z1_fl_reg, XB_reg, W1_reg, Z1_fl_reg); // [K,f] r, [f,f] c -> [K,f] r
-//
-//    copy(Z1_reg, Z1_fl_reg);
-//    sub(Z1_reg, Z1_reg, XA_reg);
-//
-//    rt_bf<1, 4, ducks::rt_layout::col> &Z1_col_reg = swap_layout_inplace(Z1_reg); // row-maj -> col-maj
-//
-//    zero(Attn1_fl_reg);
-//    mma_ABt(Attn1_fl_reg, XC_reg, XB_reg, Attn1_fl_reg);  // [N,K] r, [M,K] r -> [N,M] r
-//    copy(Attn1_reg, Attn1_fl_reg);
-//    make_causal(Attn1_reg, Attn1_reg, base_types::constants<bf16>::zero());
-//
-//    zero(Z1_bar_term_1_fl_reg);
-//    mma_AB(Z1_bar_term_1_fl_reg, XC_reg, W1_reg, Z1_bar_term_1_fl_reg); // [N,K] r, [K,M] c -> [N,M] r
-//    copy(Z1_bar_term_1_reg, Z1_bar_term_1_fl_reg);
-//
-//    zero(Z1_bar_term_2_fl_reg);
-//    mma_AB(Z1_bar_term_2_fl_reg, Attn1_reg, Z1_col_reg, Z1_bar_term_2_fl_reg);  // [K,K] r, [K,f] c -> [K,f] r
-//    copy(Z1_bar_term_2_reg, Z1_bar_term_2_fl_reg);
-//
-//    sub(Output_reg, Z1_bar_term_1_reg, Z1_bar_term_2_reg);
+    copy(Z1_reg, Z1_fl_reg);
+    sub(Z1_reg, Z1_reg, XA_reg);
 
-//    store(_Output, Output_reg, Output_reg.cols);
+    rt_bf<1, 4, ducks::rt_layout::col> &Z1_col_reg = swap_layout_inplace(Z1_reg); // row-maj -> col-maj
+
+    zero(Attn1_fl_reg);
+    mma_ABt(Attn1_fl_reg, XC_reg, XB_reg, Attn1_fl_reg);  // [N,K] r, [M,K] r -> [N,M] r
+    copy(Attn1_reg, Attn1_fl_reg);
+    make_causal(Attn1_reg, Attn1_reg, base_types::constants<bf16>::zero());
+
+    zero(Z1_bar_term_1_fl_reg);
+    mma_AB(Z1_bar_term_1_fl_reg, XC_reg, W1_reg, Z1_bar_term_1_fl_reg); // [N,K] r, [K,M] c -> [N,M] r
+
+    zero(Z1_bar_term_2_fl_reg);
+    mma_AB(Z1_bar_term_2_fl_reg, Attn1_reg, Z1_col_reg, Z1_bar_term_2_fl_reg);  // [K,K] r, [K,f] c -> [K,f] r
+
+    
+    sub(Output_fl_reg, Z1_bar_term_1_fl_reg, Z1_bar_term_2_fl_reg);
+    copy(Output_reg, Output_fl_reg);
+
+    store(_Output, Output_reg, Output_reg.cols);
+
+    rt_bf<1, 4, kittens::ducks::rt_layout::col> &XB_col_reg = swap_layout_inplace(XB_reg);
+
+    rt_fl<4, 4> W1_fl_reg;
+    zero(W1_fl_reg);
+    mma_AtB(W1_fl_reg, XB_col_reg, Z1_col_reg, W1_fl_reg);
+
+    rt_bf<4, 4> W1_row_reg;
+    copy(W1_row_reg, W1_fl_reg);
+    rt_bf<4, 4, kittens::ducks::rt_layout::col> &W1_col_reg = swap_layout_inplace(W1_row_reg);
+
+    sub(W1_reg, W1_reg, W1_col_reg);
+
+    store(_W1, W1_reg, W1_reg.cols);
 
 }
 
@@ -113,5 +123,5 @@ prefill
             Output.data_ptr<T>()
     );
 
-//    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+    // CHECK_CUDA_ERROR(cudaDeviceSynchronize());
 }
---
+++ git_config.sh
@@ -0,0 +1,2 @@
+git config user.email "zgh23@stanford.edu"
+git config user.name "zhang677"
