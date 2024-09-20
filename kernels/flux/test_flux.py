898016fac1d8b266e106b7100cde8a08e2ef33a2
Dan Fu <danfu09@gmail.com>
2024-09-20 16:09:57 -0700

    Update benchmark

--- kernels/flux/test_flux.py
+++ kernels/flux/test_flux.py
@@ -4,16 +4,7 @@ import time
 
 from triton.testing import do_bench
 
-M = 3072
-N = 3072
-K = 12288
 repeats = 100
-
-with torch.device('cuda'):
-    x = torch.randn(M, K, dtype=torch.bfloat16)
-    linear = torch.nn.Linear(K, N, bias=True).to(torch.bfloat16)
-    gate = torch.randn(N, dtype=torch.bfloat16)
-    y = torch.randn(M, N, dtype=torch.bfloat16)
 
 def ref_gate(x, linear, gate, y):
     return linear(x) * gate + y
@@ -27,32 +18,60 @@ def ref_gelu(x, linear):
 def tk_gelu(x, linear):
     return tk_flux_linear_gelu(x, linear.weight, linear.bias)
 
-print('Linear + gate')
-output_ref = ref_gate(x, linear, gate, y)
-output = tk_gate(x, linear, gate, y)
-print((output_ref-output).abs().max())
+def benchmark_gated(M, N, K):
+    with torch.device('cuda'):
+        x = torch.randn(M, K, dtype=torch.bfloat16)
+        linear = torch.nn.Linear(K, N, bias=True).to(torch.bfloat16)
+        gate = torch.randn(N, dtype=torch.bfloat16)
+        y = torch.randn(M, N, dtype=torch.bfloat16)
 
-with torch.inference_mode():
+    with torch.inference_mode():
 
-    timing = do_bench(lambda: ref_gate(x, linear, gate, y), warmup=1, rep=repeats) * 1e-3
-    tflops = (2 * M * K * N) / 1e12 / (timing)
-    print(f"Reference: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
+        timing = do_bench(lambda: ref_gate(x, linear, gate, y), warmup=1, rep=repeats) * 1e-3
+        tflops = (2 * M * K * N) / 1e12 / (timing)
+        print(f"PyTorch Reference: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
 
-    timing = do_bench(lambda: tk_gate(x, linear, gate, y), warmup=1, rep=repeats) * 1e-3
-    tflops = (2 * M * K * N) / 1e12 / (timing)
-    print(f"ThunderKittens: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
+        timing = do_bench(lambda: tk_gate(x, linear, gate, y), warmup=1, rep=repeats) * 1e-3
+        tflops = (2 * M * K * N) / 1e12 / (timing)
+        print(f"ThunderKittens: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
 
-print("Linear + gelu")
-output_ref = ref_gelu(x, linear)
-output = tk_gelu(x, linear)
-print((output_ref-output).abs().max())
+def benchmark_gelu(M, N, K):
+    with torch.device('cuda'):
+        x = torch.randn(M, K, dtype=torch.bfloat16)
+        linear = torch.nn.Linear(K, N, bias=True).to(torch.bfloat16)
 
-with torch.inference_mode():
+    with torch.inference_mode():
 
-    timing = do_bench(lambda: ref_gelu(x, linear), warmup=1, rep=repeats) * 1e-3
-    tflops = (2 * M * K * N) / 1e12 / (timing)
-    print(f"Reference: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
+        timing = do_bench(lambda: ref_gelu(x, linear), warmup=1, rep=repeats) * 1e-3
+        tflops = (2 * M * K * N) / 1e12 / (timing)
+        print(f"PyTorch Reference: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
 
-    timing = do_bench(lambda: tk_gelu(x, linear), warmup=1, rep=repeats) * 1e-3
-    tflops = (2 * M * K * N) / 1e12 / (timing)
-    print(f"ThunderKittens: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
+        timing = do_bench(lambda: tk_gelu(x, linear), warmup=1, rep=repeats) * 1e-3
+        tflops = (2 * M * K * N) / 1e12 / (timing)
+        print(f"ThunderKittens: {timing * 1e3:.3f}ms, {tflops:.2f}TFLOPS")
+
+gelu_sizes = [
+    (3072, 12288, 3072),
+    (512, 12288, 3072),
+    (256, 12288, 3072)
+]
+
+gated_size = [
+    (3072, 3072, 12288),
+    (512, 3072, 12288),
+    (256, 3072, 12288),
+    (3584, 3072, 15360),
+    (3328, 3072, 15360)
+]
+
+print('Benchmarking Gelu')
+for size in gelu_sizes:
+    print('(M, N, K):', size)
+    benchmark_gelu(*size)
+    print()
+
+print('Benchmarking Gated')
+for size in gated_size:
+    print('(M, N, K):', size)
+    benchmark_gated(*size)
+    print()
