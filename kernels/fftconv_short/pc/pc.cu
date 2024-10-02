09e75ddace4a0c2e0ba525b4aa06ff98b3d9941d
Arjun Parthasarathy <56085610+ArjunParthasarathy@users.noreply.github.com>
2024-10-01 22:26:53 -0700

    256 kernel

--- kernels/fftconv_short/pc/pc.cu
+++ kernels/fftconv_short/pc/pc.cu
@@ -1,14 +1,138 @@
-// #ifdef TORCH_COMPILE
-// #define TORCH_COMPILE_FFTCONV
-// #else
-// #include "harness_async.impl"
-// #endif
+#ifdef TORCH_COMPILE
+#define TORCH_COMPILE_FFTCONV
+#endif
 
 #include "kittens.cuh"
 #include "prototype.cuh"
 
 using namespace kittens;
 using namespace kittens::prototype;
+template<int _wg> struct fftconv_256_layout {
+    static constexpr int wg = _wg;
+    using seq_tile      = st_bf<64, 64>;
+    using seq_layout    =     gl<bf16, -1, -1, 16, 16>;
+    using filter_layout = cgl<gl<bf16,  1, -1, 64, 64>>;
+    using fft_layout    = cgl<gl<bf16,  1,  1, 64, 64>>;
+    struct globals {
+        seq_layout o, x;
+        filter_layout kf;
+        fft_layout f, finv, tw, twinv_t;
+    };
+    struct input_block    { seq_tile x[wg]; };
+    struct output_block   { seq_tile o[wg]; };
+    struct scratch_block  {
+        cst_bf<64, 64> kf, f, finv, tw, twinv_t, tmp[2];
+    };
+    struct consumer_state { int current_head; };
+};
+struct fft_256_template {
+    static constexpr int NUM_CONSUMER_WARPS=8, NUM_CONSUMER_WARPGROUPS=NUM_CONSUMER_WARPS/4, NUM_BLOCKS=1, OUTPUT_PIPE_STAGES=3, INPUT_PIPE_STAGES=3;
+    using layout = fftconv_256_layout<NUM_CONSUMER_WARPGROUPS>;
+    // mine
+    __device__ static inline void load_head_data(typename layout::scratch_block &scratch, const layout::globals &g, int head) {
+        using consumers = group<NUM_CONSUMER_WARPS>;
+        consumers::sync(1);
+        consumers::load(scratch.kf, g.kf, {0, head, 0, 0}); // next chunk
+        consumers::sync(1);
+    }
+    // tk
+    __device__ static inline int iters(const typename layout::globals &g) {
+        int heads_handled = (g.x.depth+131-blockIdx.x) / 132; // I am guaranteeing batch is handled by just one block.
+        int iters_per_head = (g.x.batch + (NUM_CONSUMER_WARPGROUPS*16)-1) / (NUM_CONSUMER_WARPGROUPS*16);
+        return heads_handled * iters_per_head;
+    }
+    struct producer {
+        __device__ static void setup(producer_setup_args<layout> args) {
+            warpgroup::decrease_registers<40>();
+        }
+        __device__ static void load(producer_load_args<layout> args) {
+            int iters_per_head = (args.globals.x.batch + (NUM_CONSUMER_WARPGROUPS*16)-1) / (NUM_CONSUMER_WARPGROUPS*16);
+            int head  = (args.iter / iters_per_head)*132 + blockIdx.x;
+            int batch = (args.iter % iters_per_head) * (NUM_CONSUMER_WARPGROUPS*16); // 16 batches per warpgroup
+            if(warpgroup::warpid() == args.iter%4) {
+                for(int b = batch; b < batch+(NUM_CONSUMER_WARPGROUPS*16) && b < args.globals.x.batch; b++) {
+                    int diff = b-batch;
+                    auto st = subtile_inplace<16,16>(args.input.x[diff/16], (diff%16)/2, diff%2);
+                    load_async(st, args.globals.x, { b, head, 0, 0 });
+                }
+                load_async_wait();
+                arrive(args.inputs_arrived, 16); // extra arrivals needed
+            }
+        }
+        __device__ static void store(producer_store_args<layout> args) {
+            int iters_per_head = (args.globals.x.batch + (NUM_CONSUMER_WARPGROUPS*16)-1) / (NUM_CONSUMER_WARPGROUPS*16);
+            int head  = (args.iter / iters_per_head)*132 + blockIdx.x;
+            int batch = (args.iter % iters_per_head) * (NUM_CONSUMER_WARPGROUPS*16); // 16 batches per warpgroup
+            if(warpgroup::warpid() == args.iter%4) {
+                for(int b = batch; b < batch+(NUM_CONSUMER_WARPGROUPS*16) && b < args.globals.x.batch; b++) {
+                    int diff = b-batch;
+                    auto st = subtile_inplace<16,16>(args.output.o[diff/16], (diff%16)/2, diff%2);
+                    kittens::store(args.globals.o, st, { b, head, 0, 0 });
+                }
+                __syncwarp(); // memory must arrive before arrival
+                arrive(args.outputs_finished, 16);
+            }
+        }
+    };
+    struct consumer {
+        __device__ static void setup(consumer_setup_args<layout> args) {
+            warpgroup::increase_registers<232>();
+            int iters_per_head = (args.globals.x.batch + (NUM_CONSUMER_WARPGROUPS*16)-1) / (NUM_CONSUMER_WARPGROUPS*16);
+            args.state.current_head = (0 / iters_per_head)*132 + blockIdx.x; // start for iter 0
+            group<NUM_CONSUMER_WARPS>::load(args.scratch.f,       args.globals.f,       {0, 0, 0, 0});
+            group<NUM_CONSUMER_WARPS>::load(args.scratch.finv,    args.globals.finv,    {0, 0, 0, 0});
+            group<NUM_CONSUMER_WARPS>::load(args.scratch.tw,      args.globals.tw,      {0, 0, 0, 0});
+            group<NUM_CONSUMER_WARPS>::load(args.scratch.twinv_t, args.globals.twinv_t, {0, 0, 0, 0});
+            load_head_data(args.scratch, args.globals, args.state.current_head);
+        }
+        __device__ static void work(consumer_work_args<layout> args) {
+            // warpgroup::copy(args.output.o[warpgroup::groupid()], args.input.x[warpgroup::groupid()]);
+            // X = F^T X
+            crt_fl<16, 64> mma_reg; // 64 registers
+            crt_bf<16, 64> accum, tmp; // 32 registers each
+            warpgroup::mm_AB(mma_reg.real, args.scratch.f.real, args.input.x[warpgroup::groupid()]);
+            warpgroup::mm_AB(mma_reg.imag, args.scratch.f.imag, args.input.x[warpgroup::groupid()]);
+            warpgroup::mma_async_wait();
+            copy(accum, mma_reg);
+
+            warpgroup::load(tmp, args.scratch.tw); // for twiddle first
+            mul(accum, accum, tmp);
+
+            group<NUM_CONSUMER_WARPS>::sync();
+            warpgroup::mm_AB(mma_reg, accum, args.scratch.f);
+            warpgroup::mma_async_wait();
+            copy(accum, mma_reg);
+
+            warpgroup::load(tmp, args.scratch.kf); // for filter second
+            mul(accum, accum, tmp);
+
+            warpgroup::mm_AB(mma_reg, accum, args.scratch.finv);
+            warpgroup::mma_async_wait();
+            copy(accum, mma_reg);
+
+            warpgroup::load(tmp, args.scratch.twinv_t); // twiddle inverse is pre-transposed
+            mul(accum, accum, tmp);
+
+            warpgroup::store(args.scratch.tmp[warpgroup::groupid()], accum); // must store for AtB
+            warpgroup::sync();
+
+            warpgroup::mm_AB(mma_reg, args.scratch.finv, args.scratch.tmp[warpgroup::groupid()]); // TODO: optimize
+            warpgroup::mma_async_wait();
+            
+            warpgroup::store(args.output.o[warpgroup::groupid()], mma_reg.real); // COMMENT ME OUT LATER
+            warpgroup::sync();
+
+            arrive(args.inputs_finished);
+            arrive(args.outputs_arrived);
+            int iters_per_head = (args.globals.x.batch + NUM_CONSUMER_WARPGROUPS-1) / NUM_CONSUMER_WARPGROUPS;
+            int next_head = ((args.iter+1) / iters_per_head)*132 + blockIdx.x;
+            if(next_head != args.state.current_head) {
+                load_head_data(args.scratch, args.globals, next_head);
+                args.state.current_head = next_head;
+            }
+        }
+    };
+};
 template<int _wg> struct fftconv_1024_layout { // 4096
     static constexpr int wg = _wg;
     using seq_tile      = st_bf<64, 64>;
@@ -257,7 +381,7 @@ struct fft_4096_template {
     };
 };
 
-// #ifdef TORCH_COMPILE_FFTCONV
+#ifdef TORCH_COMPILE_FFTCONV
 #include "common/pyutils/torch_helpers.cuh"
 #include <iostream>
 void dispatch_fft_conv( 
@@ -272,7 +396,8 @@ void dispatch_fft_conv(
     // if (N == 1024) {
         // using fftst = fft_1024_template;
     // } else {
-    using fftst = fft_4096_template;
+    // using fftst = fft_4096_template;
+    using fftst = fft_256_template;
     // }
     using globals       = typename fftst::layout::globals;
     using fft_layout    = typename fftst::layout::fft_layout;
@@ -415,4 +540,6 @@ torch::Tensor fftconv(
     CHECK_CUDA_ERROR(cudaGetLastError());
     return out;
 }
-// #endif
+#else
+#include "harness_async.impl"
+#endif
