ea2ed5acde04b922a1ddcfac70ebf42ad5eebfb1
benjaminfspector <benjaminfspector@gmail.com>
2024-10-17 16:19:27 -0700

    updating from tk2_bench

--- kernels/attn/h100/gentests.py
+++ kernels/attn/h100/gentests.py
@@ -16,7 +16,7 @@ D = int(sys.argv[2])
 H_QO = int(sys.argv[3])
 H_KV = int(sys.argv[4])
 
-causal = False
+causal = True
 
 torch.random.manual_seed(42)
 q = (torch.randn((B, H_QO, N, D), dtype=torch.bfloat16, device='cuda')).requires_grad_()
@@ -144,32 +144,32 @@ with open(filename, 'w') as f:
     vg_f = v_grad.to(torch.float32).flatten().detach().cpu().numpy()
     
     for i in trange(q.shape[0] * q.shape[1] * q.shape[2] * q.shape[3]):
-        f.write(repr(qf[i]))
+        f.write(repr(float(qf[i])))
         f.write(' ')
     for i in trange(k.shape[0] * k.shape[1] * k.shape[2] * k.shape[3]):
-        f.write(repr(kf[i]))
+        f.write(repr(float(kf[i])))
         f.write(' ')
     for i in trange(v.shape[0] * v.shape[1] * v.shape[2] * v.shape[3]):
-        f.write(repr(vf[i]))
+        f.write(repr(float(vf[i])))
         f.write(' ')
     for i in trange(o.shape[0] * o.shape[1] * o.shape[2] * o.shape[3]):
-        f.write(repr(of[i]))
+        f.write(repr(float(of[i])))
         f.write(' ')
     for i in trange(l_vec.shape[0] * l_vec.shape[1] * l_vec.shape[2]):
-        f.write(repr(l_vecf[i]))
+        f.write(repr(float(l_vecf[i])))
         f.write(' ')
     for i in trange(d_vec.shape[0] * d_vec.shape[1] * d_vec.shape[2]):
-        f.write(repr(d_vecf[i]))
+        f.write(repr(float(d_vecf[i])))
         f.write(' ')
     for i in trange(grad_output.shape[0] * grad_output.shape[1] * grad_output.shape[2] * grad_output.shape[3]):
-        f.write(repr(og_f[i]))
+        f.write(repr(float(og_f[i])))
         f.write(' ')
     for i in trange(q_grad.shape[0] * q_grad.shape[1] * q_grad.shape[2] * q_grad.shape[3]):
-        f.write(repr(qg_f[i]))
+        f.write(repr(float(qg_f[i])))
         f.write(' ')
     for i in trange(k_grad.shape[0] * k_grad.shape[1] * k_grad.shape[2] * k_grad.shape[3]):
-        f.write(repr(kg_f[i]))
+        f.write(repr(float(kg_f[i])))
         f.write(' ')
     for i in trange(v_grad.shape[0] * v_grad.shape[1] * v_grad.shape[2] * v_grad.shape[3]):
-        f.write(repr(vg_f[i]))
+        f.write(repr(float(vg_f[i])))
         f.write(' ')
