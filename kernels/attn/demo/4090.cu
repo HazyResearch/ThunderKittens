1bcecc3cb96d2a9f002b602027edca12b49b0bea
Aaryan0404 <aaryan0404@gmail.com>
2024-11-16 19:41:26 -0800

    m

--- kernels/attn/demo/4090.cu
+++ kernels/attn/demo/4090.cu
@@ -25,73 +25,80 @@ __global__ void attend_ker(const __grid_constant__ globals<D> g) {
     constexpr int LOAD_BLOCKS = NUM_WORKERS / load_group::GROUP_WARPS;
     const int batch = blockIdx.z, head  = blockIdx.y, q_seq = blockIdx.x * NUM_WORKERS + workerid;
 
-    extern __shared__ alignment_dummy __shm[]; // this is the CUDA shared memory
+    extern __shared__ alignment_dummy __shm[]; 
     shared_allocator al((int*)&__shm[0]);
-    // K and V live in shared memory. Here, we instantiate three tiles for a 3-stage pipeline.
+    
     shared_tile<D> (&k_smem)[LOAD_BLOCKS][PIPE_STAGES] = al.allocate<shared_tile<D>, LOAD_BLOCKS, PIPE_STAGES>();
     shared_tile<D> (&v_smem)[LOAD_BLOCKS][PIPE_STAGES] = al.allocate<shared_tile<D>, LOAD_BLOCKS, PIPE_STAGES>();
-    // We also reuse this memory to improve coalescing of DRAM reads and writes.
+    
     shared_tile<D> (&qo_smem)[NUM_WORKERS] = reinterpret_cast<shared_tile<D>(&)[NUM_WORKERS]>(k_smem);
-    // Initialize all of the register tiles.
-    qkvo_tile<D, bf16> q_reg, k_reg; // Q and K are both row layout, as we use mma_ABt.
-    qkvo_tile<D, bf16, col_l> v_reg; // V is column layout, as we use mma_AB.
-    qkvo_tile<D, float> o_reg; // Output tile.
-    attn_tile<D, float> att_block; // attention tile, in float. (We want to use float wherever possible.)
-    attn_tile<D, bf16> att_block_mma; // bf16 attention tile for the second mma_AB. We cast right before that op.
-    typename attn_tile<D, float>::col_vec max_vec_last, max_vec, norm_vec; // these are column vectors for the in-place softmax.
-    // each warp loads its own Q tile of 16x64
+    
+    qkvo_tile<D, bf16> q_reg, k_reg;
+    qkvo_tile<D, bf16, col_l> v_reg; 
+    qkvo_tile<D, float> o_reg; 
+    attn_tile<D, float> att_block;
+    attn_tile<D, bf16> att_block_mma;
+
+    typename attn_tile<D, float>::col_vec max_vec_last, max_vec, norm_vec;
+
     if (q_seq*ROWS<D> < g.Qg.rows) {
-        load(qo_smem[workerid], g.Qg, {batch, head, q_seq, 0});  // going through shared memory improves coalescing of dram reads.
+        load(qo_smem[workerid], g.Qg, {batch, head, q_seq, 0}); 
         __syncwarp();
         load(q_reg, qo_smem[workerid]);
     }
     __syncthreads();
-    // temperature adjustment. Pre-multiplying by lg2(e), too, so we can use exp2 later.
+
     if constexpr(D == 64) mul(q_reg, q_reg, __float2bfloat16(0.125f * 1.44269504089));
     else if constexpr(D == 128) mul(q_reg, q_reg, __float2bfloat16(0.08838834764f * 1.44269504089));
-    // initialize flash attention L, M, and O registers.
-    neg_infty(max_vec); // zero registers for the Q chunk
+
+    neg_infty(max_vec);
     zero(norm_vec);
     zero(o_reg);
-    // launch the load of the first k, v tiles
+
     int kv_blocks = g.Qg.rows / (LOAD_BLOCKS*ROWS<D>), tic = 0;
     load_group::load_async(k_smem[loadid][0], g.Kg, {batch, head, loadid, 0});
     load_group::load_async(v_smem[loadid][0], g.Vg, {batch, head, loadid, 0});
-    // iterate over k, v for these q's that have been loaded
+
     for(auto kv_idx = 0; kv_idx < kv_blocks; kv_idx++, tic=(tic+1)%PIPE_STAGES) {
         int next_load_idx = (kv_idx+1)*LOAD_BLOCKS + loadid;
+        
         if(next_load_idx*ROWS<D> < g.Kg.rows) {
             int next_tic = (tic+1)%PIPE_STAGES;
             load_group::load_async(k_smem[loadid][next_tic], g.Kg, {batch, head, next_load_idx, 0});
             load_group::load_async(v_smem[loadid][next_tic], g.Vg, {batch, head, next_load_idx, 0});
-            load_async_wait<2>(); // next k, v can stay in flight.
+            load_async_wait<2>();
         }
-        else load_async_wait(); // all must arrive
-        __syncthreads(); // Everyone's memory must be ready for the next stage.
-        // now each warp goes through all of the subtiles, loads them, and then does the flash attention internal alg.
+        else load_async_wait();
+        __syncthreads();
+
         #pragma unroll LOAD_BLOCKS
         for(int subtile = 0; subtile < LOAD_BLOCKS && (kv_idx*LOAD_BLOCKS + subtile) < g.Qg.rows; subtile++) {
-            load(k_reg, k_smem[subtile][tic]); // load k from shared into registers
-            zero(att_block); // zero 16x16 attention tile
-            mma_ABt(att_block, q_reg, k_reg, att_block); // Q@K.T
+            
+            load(k_reg, k_smem[subtile][tic]);
+            zero(att_block); 
+            mma_ABt(att_block, q_reg, k_reg, att_block); 
+            
             copy(max_vec_last,  max_vec);
-            row_max(max_vec, att_block, max_vec); // accumulate onto the max_vec
-            sub_row(att_block, att_block, max_vec); // subtract max from attention -- now all <=0
-            exp2(att_block, att_block); // exponentiate the block in-place.
-            sub(max_vec_last, max_vec_last, max_vec); // subtract new max from old max to find the new normalization.
-            exp2(max_vec_last, max_vec_last); // exponentiate this vector -- this is what we need to normalize by.
-            mul(norm_vec, norm_vec, max_vec_last); // and the norm vec is now normalized.
-            row_sum(norm_vec, att_block, norm_vec); // accumulate the new attention block onto the now-rescaled norm_vec
-            copy(att_block_mma, att_block); // convert to bf16 for mma_AB
-            load(v_reg, v_smem[subtile][tic]); // load v from shared into registers.
-            mul_row(o_reg, o_reg, max_vec_last); // normalize o_reg in advance of mma_AB'ing onto it
-            mma_AB(o_reg, att_block_mma, v_reg, o_reg); // mfma onto o_reg with the local attention@V matmul.
+            row_max(max_vec, att_block, max_vec); 
+            sub_row(att_block, att_block, max_vec); 
+            exp2(att_block, att_block);
+            sub(max_vec_last, max_vec_last, max_vec); 
+            exp2(max_vec_last, max_vec_last); 
+            mul(norm_vec, norm_vec, max_vec_last); 
+            row_sum(norm_vec, att_block, norm_vec); 
+            copy(att_block_mma, att_block); 
+            
+            load(v_reg, v_smem[subtile][tic]); 
+            mul_row(o_reg, o_reg, max_vec_last); 
+            mma_AB(o_reg, att_block_mma, v_reg, o_reg);
         }
     }
+
     div_row(o_reg, o_reg, norm_vec);
     __syncthreads();
-    if (q_seq*ROWS<D> < g.Qg.rows) { // write out o.
-        store(qo_smem[workerid], o_reg); // going through shared memory improves coalescing of dram writes.
+
+    if (q_seq*ROWS<D> < g.Qg.rows) {
+        store(qo_smem[workerid], o_reg);
         __syncwarp();
         store(g.Og, qo_smem[workerid], {batch, head, q_seq, 0});
     }
