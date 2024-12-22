133ec1cda346f0ad4e3bc5121f1098a8ace1761c
Aaryan0404 <aaryan0404@gmail.com>
2024-12-22 05:48:02 -0800

    cylon tk2

---
+++ kernels/cylon/cylon.cu
@@ -0,0 +1,296 @@
+#include "kittens.cuh"
+#include <cooperative_groups.h>
+#include <iostream>
+
+constexpr int NUM_CONSUMER_WARPGROUPS = 2; // hardcoded, don't touch
+constexpr int STATE_PER_SM = 2;            // how many maps get run on each SM. 
+                                           // hardcoded, don't touch
+
+constexpr int NUM_CONSUMER_WARPS = NUM_CONSUMER_WARPGROUPS * kittens::WARPGROUP_WARPS;
+constexpr int NUM_WARPS          = NUM_CONSUMER_WARPS + kittens::WARPGROUP_WARPS; 
+constexpr int NUM_THREADS        = NUM_WARPS * kittens::WARP_THREADS;
+
+using namespace kittens;
+namespace cg = cooperative_groups;
+
+struct fwd_globals {
+    using q_tile        = st_bf<64, 64>;
+    using k_tile        = st_bf<64, 64>;
+    using v_tile        = st_bf<64, 64>;
+    using o_tile        = st_bf<64, 64>;
+    using kv_state_tile = st_fl<64, 64>;
+    using q_map_tile    = st_bf<64, 64>;
+    using k_map_tile    = st_bf<64, 64>;
+
+    using q_gl        = gl<bf16, -1, -1, -1, -1, q_tile>;
+    using k_gl        = gl<bf16, -1, -1, -1, -1, k_tile>;
+    using v_gl        = gl<bf16, -1, -1, -1, -1, v_tile>;
+    using o_gl        = gl<bf16, -1, -1, -1, -1, o_tile>;
+    using kv_state_gl = gl<float, -1, -1, -1, -1, kv_state_tile>;
+    using q_map_gl    = gl<bf16, -1, -1, -1, -1, q_map_tile>;
+    using k_map_gl    = gl<bf16, -1, -1, -1, -1, k_map_tile>;
+
+    q_gl        q;
+    k_gl        k;
+    v_gl        v;
+    o_gl        o;
+    kv_state_gl kv_state;
+    q_map_gl    q_map;
+    k_map_gl    k_map;
+
+    const int N;
+}; 
+
+template<ducks::rt::all RT> __device__ static inline void featurize(RT &reg) {
+    relu(reg, reg);
+}
+
+__global__ __launch_bounds__(NUM_THREADS, 1)
+void cylon_forwards(fwd_globals g) 
+{
+    int laneid = kittens::laneid(); 
+    int warpid = kittens::warpid(); 
+    int warpgroupid = warpid/kittens::WARPGROUP_WARPS;
+
+    int tic = 0, toc = 1; // these are used to track the two-stage pipeline.
+    unsigned int batch_id = blockIdx.z; // which batch?
+    unsigned int head_id  = blockIdx.y; // which head?
+    unsigned int state_id = blockIdx.x; // which part of the KV state are we handling? (Important that this is on x.)
+    int n_chunks = g.N / 64; // this kernel handles 64 rows per chunk.
+
+    extern __shared__ int __shm[];
+    tma_swizzle_allocator alloc((int*)&__shm[0]);
+
+    using q_tile = st_bf<64, 64>;
+    using k_tile = st_bf<64, 64>;
+    using v_tile = st_bf<64, 64>;
+    using o_tile = st_bf<64, 64>;
+    using kv_state_tile = st_fl<64, 64>;
+    using q_map_tile = st_bf<64, 64>;
+    using k_map_tile = st_bf<64, 64>;
+
+    using tile_bf_64_64 = st_bf<64, 64>;
+    using tile_fl_64_64 = st_fl<64, 64>;
+
+    tile_bf_64_64 (&q_smem)[2]    = alloc.allocate<tile_bf_64_64, 2>();    // 64x64, tic-toc'd (16384)
+    tile_bf_64_64 (&k_smem)[2]    = alloc.allocate<tile_bf_64_64, 2>();    // 64x64, tic-toc'd (16384)
+    tile_bf_64_64 (&v_smem)[2][2] = alloc.allocate<tile_bf_64_64, 2, 2>(); // 64x128, but easier to work with when split up. (32768)
+    tile_bf_64_64 (&o_smem)[2]    = alloc.allocate<tile_bf_64_64, 2>();    // 64x128, but easier to work with when split up. (16384)
+
+    tile_bf_64_64 (&kv_scratch)[2][2] = alloc.allocate<tile_bf_64_64, 2, 2>(); // This is scratch for doing wgmma's (32768)
+
+    tile_bf_64_64 (&q_map)[2] = alloc.allocate<tile_bf_64_64, 2>(); // featurized q (16384)
+    tile_bf_64_64 (&k_map)[2] = alloc.allocate<tile_bf_64_64, 2>(); // featurized k (16384)
+
+    tile_fl_64_64 (&kv_state_smem)[2][2] = reinterpret_cast<tile_fl_64_64(&)[2][2]>(q_smem); // we can reuse old memory for the writeout at the end
+
+    // Initialize barriers
+    __shared__ kittens::semaphore inputs_arrived[2], inputs_finished[2], outputs_ready[2];
+    if (warpid == 0) {
+        init_semaphore(inputs_arrived[0], 0, 2); // needs to wait on just one memory transaction, plus confirmation that o is available for writing.
+        init_semaphore(inputs_arrived[1], 0, 2);
+        if(laneid == 0) { // o is available for writing on the initial load, so mark that appropriately
+            arrive(inputs_arrived[0]);
+        }
+        init_semaphore(inputs_finished[0], NUM_CONSUMER_WARPS, 0);
+        init_semaphore(inputs_finished[1], NUM_CONSUMER_WARPS, 0);
+        init_semaphore(outputs_ready[0],   NUM_CONSUMER_WARPGROUPS, 0);
+        init_semaphore(outputs_ready[1],   NUM_CONSUMER_WARPGROUPS, 0);
+    }
+    // Launch first load. No sync needed since thread 0 is doing these, too.
+    if(warpid == 0) {
+        tma::expect_bytes(inputs_arrived[0], sizeof(tile_bf_64_64) * 8); // register a transaction for q, k, v, and q_map, k_map
+
+        // int load_idx = ((batch_id * gridDim.y) + head_id) * n_chunks;
+        // tma::load_async(q_smem[tic],    g.q, inputs_arrived[0], load_idx); // launch the initial load
+        // tma::load_async(k_smem[tic],    g.k, inputs_arrived[0], load_idx); // launch the initial load
+        // tma::load_async(v_smem[tic][0], g.v, inputs_arrived[0], load_idx, 0); // launch the initial load
+        // tma::load_async(v_smem[tic][1], g.v, inputs_arrived[0], load_idx, 1); // launch the initial load
+        coord<q_tile> q_tile_idx = {batch_id, head_id, 0, 0};
+        tma::load_async(q_smem[tic], g.q, q_tile_idx, inputs_arrived[0]);
+        coord<k_tile> k_tile_idx = {batch_id, head_id, 0, 0};
+        tma::load_async(k_smem[tic], g.k, k_tile_idx, inputs_arrived[0]);
+        coord<v_tile> v_tile_idx = {batch_id, head_id, 0, 0};
+        tma::load_async(v_smem[tic][0], g.v, v_tile_idx, inputs_arrived[0]);
+                      v_tile_idx = {batch_id, head_id, 0, 1};
+        tma::load_async(v_smem[tic][1], g.v, v_tile_idx, inputs_arrived[0]);
+
+        // load q map, k map blocks
+        // int base_map_idx = (head_id * gridDim.x + state_id) * STATE_PER_SM; // gridDim.x is how many SMs are collaborating on each head
+        // tma::load_async(q_map[0], g.q_map, inputs_arrived[0], base_map_idx+0);
+        // tma::load_async(q_map[1], g.q_map, inputs_arrived[0], base_map_idx+1);
+        // tma::load_async(k_map[0], g.k_map, inputs_arrived[0], base_map_idx+0);
+        // tma::load_async(k_map[1], g.k_map, inputs_arrived[0], base_map_idx+1);
+        coord<q_map_tile> q_map_tile_idx = {head_id, state_id, 0, 0};
+        tma::load_async(q_map[0], g.q_map, q_map_tile_idx, inputs_arrived[0]);
+                          q_map_tile_idx = {head_id, state_id, 0, 1};
+        tma::load_async(q_map[1], g.q_map, q_map_tile_idx, inputs_arrived[0]);
+        coord<k_map_tile> k_map_tile_idx = {head_id, state_id, 0, 0};
+        tma::load_async(k_map[0], g.k_map, k_map_tile_idx, inputs_arrived[0]);
+                          k_map_tile_idx = {head_id, state_id, 0, 1};
+        tma::load_async(k_map[1], g.k_map, k_map_tile_idx, inputs_arrived[0]);
+    }
+
+    __syncthreads(); // all warps must arrive here, confirming barrier initialization is visible to all threads.
+
+    if(warpgroupid == NUM_CONSUMER_WARPGROUPS) { // last warpgroup is a producer
+        warpgroup::decrease_registers<24>();
+   
+        if(warpid == NUM_CONSUMER_WARPS) { // just need a single warp to handle input loads
+            for (int chunk_idx = 0; chunk_idx < n_chunks-1; chunk_idx++, tic=tic^1, toc=toc^1) {
+                tma::expect_bytes(inputs_arrived[toc], sizeof(tile_bf_64_64) * 4); // register that another block is coming in
+                
+                // int next_load_idx = ((batch_id * gridDim.y) + head_id) * n_chunks + chunk_idx + 1;
+                // tma::load_async(q_smem[toc],    g.q, inputs_arrived[toc], next_load_idx); // load that block
+                // tma::load_async(k_smem[toc],    g.k, inputs_arrived[toc], next_load_idx); // load that block
+                // tma::load_async(v_smem[toc][0], g.v, inputs_arrived[toc], next_load_idx, 0); // load that block
+                // tma::load_async(v_smem[toc][1], g.v, inputs_arrived[toc], next_load_idx, 1); // load that block
+
+                coord<q_tile> q_tile_idx = {batch_id, head_id, chunk_idx + 1, 0};
+                tma::load_async(q_smem[toc], g.q, q_tile_idx, inputs_arrived[toc]);
+                coord<k_tile> k_tile_idx = {batch_id, head_id, chunk_idx + 1, 0};
+                tma::load_async(k_smem[toc], g.k, k_tile_idx, inputs_arrived[toc]);
+                coord<v_tile> v_tile_idx = {batch_id, head_id, chunk_idx + 1, 0};
+                tma::load_async(v_smem[toc][0], g.v, v_tile_idx, inputs_arrived[toc]);
+                              v_tile_idx = {batch_id, head_id, chunk_idx + 1, 1};
+                tma::load_async(v_smem[toc][1], g.v, v_tile_idx, inputs_arrived[toc]);
+
+                wait(inputs_finished[tic], (chunk_idx/2)%2); // phase changes at half the rate of the tic/toc
+            }
+        }
+        else if(warpid == NUM_CONSUMER_WARPS + 1) { // responsible for storing outputs
+            for (int chunk_idx = 0; chunk_idx < n_chunks; chunk_idx++, tic=tic^1, toc=toc^1) {
+                wait(outputs_ready[tic], (chunk_idx/2)%2); // phase changes at half the rate of the tic/toc
+                // int store_idx = ((batch_id * gridDim.y) + head_id) * n_chunks + chunk_idx;
+                // tma::store_add_async(g.o, o_smem[0], store_idx, 0); // store
+                // tma::store_add_async(g.o, o_smem[1], store_idx, 1);
+                coord<o_tile> o_tile_idx = {batch_id, head_id, chunk_idx, 0};
+                tma::store_add_async(g.o, o_smem[0], o_tile_idx);
+                              o_tile_idx = {batch_id, head_id, chunk_idx, 1};
+                tma::store_add_async(g.o, o_smem[1], o_tile_idx);
+                
+                tma::store_commit_group();
+                tma::store_async_read_wait();
+                if(laneid == 0) arrive(inputs_arrived[toc]); // tell the consumers they can write to o again
+            }
+        }
+    }
+    else { // other warpgroups are consumers
+        warpgroup::increase_registers<240>(); // 240 registers, no spills!
+
+        rt_fl<16, 64> kv_state[2];
+        zero(kv_state[0]);
+        zero(kv_state[1]);
+
+        for (int chunk_idx = 0; chunk_idx < n_chunks; chunk_idx++, tic^=1, toc^=1) {
+            
+            wait(inputs_arrived[tic], (chunk_idx/2)%2); // wait for memory to arrive
+
+            rt_fl<16, 64> o_reg, qf_reg, kf_reg;
+            rt_bf<16, 64> qf_reg_bf, kf_reg_bf;
+            
+            // First thing we need to do is do [ReLU(q_map @ q)] @ kv_state
+
+            // warpgroup::mma_fence(qf_reg);
+            warpgroup::mm_AB(qf_reg, q_smem[tic], q_map[0]);
+            warpgroup::mma_commit_group();
+            // INJECTION: warpgroup::mma_async_wait();
+            warpgroup::store(kv_scratch[warpgroupid][0], kv_state[0]); // need to store k, v back to smem for wgmma
+            warpgroup::mma_async_wait();
+
+            featurize(qf_reg);
+            copy(qf_reg_bf, qf_reg); // now q has been featurized the first way
+
+            // warpgroup::sync(); // shared memory writes need to have finished by this point.
+            group<4>::sync(warpgroup::groupid()+4);
+            // warpgroup::mma_fence(o_reg); // launch first kv_state matmul
+            warpgroup::mm_AB(o_reg, qf_reg_bf, kv_scratch[warpgroupid][0]); // overwrite o_reg
+            warpgroup::mma_commit_group();
+            warpgroup::mma_async_wait();
+
+            // warpgroup::mma_fence(qf_reg); // launch second featurization
+            warpgroup::mm_AB(qf_reg, q_smem[tic], q_map[1]);
+            warpgroup::mma_commit_group();
+            // INJECTION: warpgroup::mma_async_wait();
+            warpgroup::store(kv_scratch[warpgroupid][1], kv_state[1]); // need to store k, v back to smem for wgmma
+            warpgroup::mma_async_wait();
+
+            featurize(qf_reg);
+            copy(qf_reg_bf, qf_reg); // now q has been featurized the second way
+
+            // warpgroup::sync(); // shared memory writes need to have finished by this point.
+            group<4>::sync(warpgroup::groupid()+4);
+            // warpgroup::mma_fence(o_reg); // launch second kv_state matmul
+            warpgroup::mma_AB(o_reg, qf_reg_bf, kv_scratch[warpgroupid][1]); // accumulate into o_reg
+            warpgroup::mma_commit_group();
+            warpgroup::mma_async_wait(); // at this point all compute on o is done.
+
+            // NEXT: we need to update the state. We can immediately launch a k_map matmul from shared memory
+
+            // warpgroup::mma_fence(kf_reg);
+            warpgroup::mm_AtBt(kf_reg, k_map[0], k_smem[tic]); // note we want to transpose k here on its way into registers
+            warpgroup::mma_commit_group();
+            // INJECTION: warpgroup::mma_async_wait();
+            
+            // We've now done all the work on o that's required -- send it up to shared memory and let the producer run a cp.reduce.async.bulk.
+            warpgroup::store(o_smem[warpgroupid], o_reg); // bf16
+
+            warpgroup::mma_async_wait();
+
+            featurize(kf_reg);
+            copy(kf_reg_bf, kf_reg); // now k has been featurized with the first map
+
+            // warpgroup::mma_fence(kv_state[0]);
+            warpgroup::mma_AB(kv_state[0], kf_reg_bf, v_smem[tic][warpgroupid]); // not pre-transposed so AtB
+            warpgroup::mma_commit_group();
+
+            // warpgroup::sync(); // o memory arrived in smem!
+            group<4>::sync(warpgroup::groupid()+4);
+            if(warpgroup::laneid() == 0) arrive(outputs_ready[tic]); // we've now told the producer it can send o along once it's ready.
+
+            warpgroup::mma_async_wait();
+
+            // warpgroup::mma_fence(kf_reg); // launch second featurization
+            warpgroup::mm_AtBt(kf_reg, k_map[1], k_smem[tic]); // note we want to transpose k here on its way into registers
+            warpgroup::mma_commit_group();
+            warpgroup::mma_async_wait();
+
+            featurize(kf_reg);
+            copy(kf_reg_bf, kf_reg); // now k has been featurized the second way
+
+            // warpgroup::mma_fence(kv_state[1]);
+            warpgroup::mma_AB(kv_state[1], kf_reg_bf, v_smem[tic][warpgroupid]); // pre-transposed so AB
+            warpgroup::mma_commit_group();
+            warpgroup::mma_async_wait();
+
+            // Finished with inputs
+            if(laneid == 0) arrive(inputs_finished[tic]); // we can now start loading the next q, k, v
+        }
+        kittens::group<8>::sync(10); // Everyone must be done with everything before we do this! Otherwise we will overwrite Q, K, V.
+        warpgroup::store(kv_state_smem[0][warpgroupid], kv_state[0]);
+        // warpgroup::sync();
+        group<4>::sync(warpgroup::groupid()+4);
+        if(warpgroup::warpid() == 0) {
+            // int base_kv_idx = ((((batch_id * gridDim.y) + head_id) * gridDim.x) + state_id) * STATE_PER_SM;
+            // tma::store_async(g.kv_state, kv_state_smem[0][warpgroupid], base_kv_idx+0, warpgroupid);
+            coord<kv_state_tile> kv_state_tile_idx = {batch_id, head_id, (state_id * STATE_PER_SM), warpgroupid}; 
+            tma::store_async(g.kv_state, kv_state_smem[0][warpgroupid], kv_state_tile_idx);
+            tma::store_commit_group();
+        }
+        warpgroup::store(kv_state_smem[1][warpgroupid], kv_state[1]);
+        // warpgroup::sync();
+        group<4>::sync(warpgroup::groupid()+4);
+        if(warpgroup::warpid() == 0) {
+            // int base_kv_idx = ((((batch_id * gridDim.y) + head_id) * gridDim.x) + state_id) * STATE_PER_SM;
+            // tma::store_async(g.kv_state, kv_state_smem[1][warpgroupid], base_kv_idx+1, warpgroupid);
+            coord<kv_state_tile> kv_state_tile_idx = {batch_id, head_id, (state_id * STATE_PER_SM) + 1, warpgroupid}; 
+            tma::store_async(g.kv_state, kv_state_smem[1][warpgroupid], kv_state_tile_idx); 
+            tma::store_commit_group();
+        }
+        tma::store_async_read_wait();
+        // warpgroup::sync();
+        group<4>::sync(warpgroup::groupid()+4);
+    }
+}
+
+#include "harness.impl"
---
+++ kernels/cylon/gentests.py
@@ -0,0 +1,185 @@
+import torch
+import numpy as np
+
+torch.manual_seed(42)
+
+# Constants
+B = 1     # Batch size
+H = 1     # Heads, which we're currently ignoring in this Python code.
+N = 8192  # Sequence length
+
+D_QK = 64   # Query/Key dimension
+D_VO = 128  # Value/Output dimension
+
+COLLABORATIVE_SMS = 8  # Number of collaborative SMs
+EXPANSION_FACTOR  = COLLABORATIVE_SMS*2
+
+def create_terraced_lower_triangular(size, block_size=64):
+    # Create a lower triangular matrix of ones
+    matrix = torch.tril(torch.ones(size, size, device='cuda:0'))
+    
+    # Create a mask for the block diagonal
+    mask = torch.zeros(size, size, dtype=torch.bool, device='cuda:0')
+    for i in range(0, size, block_size):
+        end = min(i + block_size, size)
+        mask[i:end, i:end] = True
+    
+    # Apply the mask to cut out the block diagonal
+    matrix[mask] = 0
+    
+    return matrix
+
+def initialize_tensors():
+    # Initialize Q, K, V tensors
+    q = torch.randn(B, N, D_QK, dtype=torch.bfloat16, device='cuda:0') / D_QK
+    k = torch.randn(B, N, D_QK, dtype=torch.bfloat16, device='cuda:0') / D_QK
+    v = torch.randn(B, N, D_VO, dtype=torch.bfloat16, device='cuda:0')
+
+    # Initialize Q_map and K_map tensors
+    q_map = torch.randn(EXPANSION_FACTOR, D_QK, D_QK, dtype=torch.bfloat16, device='cuda:0') / D_QK
+    k_map = torch.randn(EXPANSION_FACTOR, D_QK, D_QK, dtype=torch.bfloat16, device='cuda:0') / D_QK
+
+    return q, k, v, q_map, k_map
+
+def compute_output(q, k, v, q_map, k_map, mask):
+    q_heads = torch.relu(torch.einsum('edk,bnd->benk', q_map, q)).to(torch.bfloat16)
+    k_heads = torch.relu(torch.einsum('edk,bnd->benk', k_map, k)).to(torch.bfloat16)
+    attn = torch.einsum('benk,bemk->benm', q_heads, k_heads) * mask
+    output = torch.einsum('benm,bmv->benv', attn.to(torch.bfloat16), v).to(torch.bfloat16)
+    output = torch.sum(output, dim=1) # sum over projections
+    kv_state = torch.einsum('benk,bnv->bekv', k_heads, v)
+    return output.to(torch.bfloat16), kv_state.to(torch.bfloat16)
+
+def save_tensors_to_file(tensor, filename, width=64):
+    np_array = tensor.detach().to(torch.float32).cpu().numpy().reshape((-1,width))
+    rep = '\n'.join([' '.join([f'{x:0.6f}' for x in row]) for row in np_array])
+    print(f'Writing shape {np_array.shape} to {filename}')
+    with open(filename, 'w') as f:
+        f.write(rep)
+
+def terraced_linear_attention_backward(kv_state, grad_output, q, k, v, q_map, k_map):
+    q_grad, k_grad, v_grad = torch.zeros_like(q).to(torch.float32), torch.zeros_like(k).to(torch.float32), torch.zeros_like(v).to(torch.float32)
+    q_map_grad, k_map_grad = torch.zeros_like(q_map).to(torch.float32), torch.zeros_like(k_map).to(torch.float32)
+
+    # We need to iterate backwards through the blocks in chunks of 64
+    for b in range(B):
+        for s in range(EXPANSION_FACTOR):
+            kv = -kv_state[b, s, :, :].to(torch.float32) # negate at first, so that we can do mma's onto it with + later. And we'll just invert everything at the end.
+            kv_grad = torch.zeros_like(kv).to(torch.float32)
+            for _id in range(N // 64):
+                chunk_id = (N//64)-_id-1
+                qm, km = q_map[s].to(torch.float32), k_map[s].to(torch.float32)
+                q_chunk = q[b, chunk_id*64:(chunk_id+1)*64].to(torch.float32)
+                k_chunk = k[b, chunk_id*64:(chunk_id+1)*64].to(torch.float32)
+                v_chunk = v[b, chunk_id*64:(chunk_id+1)*64].to(torch.float32)
+                o_grad_chunk = grad_output[b, chunk_id*64:(chunk_id+1)*64]
+
+                # first we update K, V grads based on the current kv state and gradients
+                k_head_prerelu = torch.einsum('nd,dk->nk', k_chunk, km) # CHECK
+                k_head_chunk_grad = torch.einsum('nv,kv->nk', v_chunk, kv_grad) * (k_head_prerelu>0)
+                k_grad[b, chunk_id*64:(chunk_id+1)*64] += torch.einsum('nk,dk->nd', k_head_chunk_grad, km)
+                k_map_grad[s] += torch.einsum('nd,nk->dk', k_chunk, k_head_chunk_grad)
+                v_grad[b, chunk_id*64:(chunk_id+1)*64] += torch.einsum('nk,kv->nv', torch.relu(k_head_prerelu), kv_grad)
+
+                # second we need to subtract (add, since negated) the KV contribution from that local state.
+                kv += torch.einsum('nk,nv->kv', torch.relu(k_head_prerelu), v_chunk) # we now have the kv state as it was before the last iter forwards
+
+                # third, we need to compute q_head pre-relu and its corresponding gradients
+                q_head_prerelu = torch.einsum('nd,dk->nk', q_chunk, qm)
+                q_head_chunk_grad = -torch.einsum('nv,kv->nk', o_grad_chunk, kv) * (q_head_prerelu>0) # incorporate grad of relu
+                q_grad[b, chunk_id*64:(chunk_id+1)*64] += torch.einsum('nk,dk->nd', q_head_chunk_grad, qm)
+                q_map_grad[s] += torch.einsum('dn,nk->dk', q_chunk.T, q_head_chunk_grad)
+
+                # finally we accumulate the gradient on the current kv state
+                kv_grad += torch.einsum('nk,nv->kv', torch.relu(q_head_prerelu), o_grad_chunk)
+
+    # Todo
+    return q_grad, k_grad, v_grad, q_map_grad, k_map_grad
+
+def main():
+    # Initialize mask
+    mask = create_terraced_lower_triangular(N, 64)
+    print('Generated mask')
+
+    # Initialize tensors
+    q, k, v, q_map, k_map = initialize_tensors()
+    q.requires_grad_(True)
+    k.requires_grad_(True)
+    v.requires_grad_(True)
+    q_map.requires_grad_(True)
+    k_map.requires_grad_(True)
+    print('Initialized tensors')
+
+    # Compute output (you'll implement this function)
+    output, kv_state = compute_output(q, k, v, q_map, k_map, mask)
+    print('Computed output')
+
+    # Create a random gradient for the output
+    o_grad = torch.randn_like(output).to(torch.float32)
+    print('Generated random gradient for output')
+
+    # Compute backward pass
+    output.backward(o_grad)
+    print('Computed backward pass')
+
+    # Print gradients
+    print('Gradient shapes:')
+    print('q_grad:', q.grad.shape)
+    print('k_grad:', k.grad.shape)
+    print('v_grad:', v.grad.shape)
+    print('q_map_grad:', q_map.grad.shape)
+    print('k_map_grad:', k_map.grad.shape)
+
+    # Compute grad the other way, with the same output
+    grad_q, grad_k, grad_v, grad_q_map, grad_k_map = terraced_linear_attention_backward(kv_state, o_grad, q, k, v, q_map, k_map)
+    print('Computed backward pass')
+
+    # Print some gradient statistics
+    def print_grad_stats(name, grad, gref):
+        print(f'{name} REF - min: {gref.min().item():.6f}, max: {gref.max().item():.6f}, mean: {gref.mean().item():.6f}, abs mean: {gref.abs().mean().item():.6f}, std: {gref.std().item():.6f}')
+        print(f'{name} NEW - min: {grad.min().item():.6f}, max: {grad.max().item():.6f}, mean: {grad.mean().item():.6f}, abs mean: {grad.abs().mean().item():.6f}, std: {grad.std().item():.6f}')
+        print(f'{name} avg % error: {100*((grad-gref).abs().mean()/gref.abs().mean()).item():.6f}%')
+
+    print('\nGradient statistics:')
+    print_grad_stats('grad_q', grad_q, q.grad)
+    print_grad_stats('grad_k', grad_k, k.grad)
+    print_grad_stats('grad_v', grad_v, v.grad)
+    print_grad_stats('grad_q_map', grad_q_map, q_map.grad)
+    print_grad_stats('grad_k_map', grad_k_map, k_map.grad)
+
+    print(grad_k[0,0,:10])
+    print(k.grad[0,0,:10])
+
+    if True:
+        # Save tensors to files
+        save_tensors_to_file(q, 'inputs/q.txt', D_QK)
+        save_tensors_to_file(k, 'inputs/k.txt', D_QK)
+        save_tensors_to_file(v, 'inputs/v.txt', D_VO)
+        save_tensors_to_file(q_map, 'inputs/q_map.txt', D_QK)
+        save_tensors_to_file(k_map, 'inputs/k_map.txt', D_QK)
+        save_tensors_to_file(o_grad, 'inputs/o_grad.txt', D_VO)
+        save_tensors_to_file(output, 'reference/output.txt', D_VO)
+        save_tensors_to_file(kv_state, 'reference/kv_state.txt', D_QK*D_VO)
+        save_tensors_to_file(q.grad, 'reference/q_grad.txt', D_QK)
+        save_tensors_to_file(v.grad, 'reference/v_grad.txt', D_VO)
+        save_tensors_to_file(k.grad, 'reference/k_grad.txt', D_QK)
+        save_tensors_to_file(q_map.grad, 'reference/q_map_grad.txt', D_QK)
+        save_tensors_to_file(k_map.grad, 'reference/k_map_grad.txt', D_QK)
+
+        print("Tensors have been initialized and saved to files.")
+        print("Output shape:", output.shape)
+        print("KV state shape:", kv_state.shape)
+        
+        # print all tensor shapes
+        print("Tensor shapes:")
+        print("q:", q.shape)
+        print("k:", k.shape)
+        print("v:", v.shape)
+        print("q_map:", q_map.shape)
+        print("k_map:", k_map.shape)
+        print("o_grad:", o_grad.shape)
+        print("output:", output.shape)
+        print("kv_state:", kv_state.shape)
+
+if __name__ == "__main__":
+    main()
