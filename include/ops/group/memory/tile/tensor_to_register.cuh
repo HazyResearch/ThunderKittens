52992d3b9482f96e28cbe8d1a225faf18c95b1ab
Aaryan0404 <aaryan0404@gmail.com>
2025-01-24 09:21:54 +0000

    group

---
+++ include/ops/group/memory/tile/tensor_to_register.cuh
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * @brief Group (collaborative warp) ops for loading tensor tiles into register tiles.
+ */
+
+template<ducks::rt::all RT, ducks::tmem::all TM>
+__device__ static inline void load(RT &dst, const TM &src) {
+    static_assert(N_WARPS==2 || N_WARPS%4==0);
+    constexpr int warp_rows = TM::height/N_WARPS;
+    static_assert(TM::cols==RT::cols);
+    static_assert(warp_rows==RT::rows);
+    auto src_subtile = src.subtile<tmem<typename TM::dtype, warp_rows, TM::cols>>(warp_rows*(warpid()/4+(warpid()%4)*(N_WARPS/4)), 0);
+    kittens::load(dst, src_subtile);
+}
+
+template<ducks::rt::all RT, ducks::tmem::all TM>
+__device__ static inline void store(TM &dst, const RT &src) {
+    static_assert(N_WARPS==2 || N_WARPS%4==0);
+    constexpr int warp_rows = TM::height/N_WARPS;
+    static_assert(TM::cols==RT::cols);
+    static_assert(warp_rows==RT::rows);
+    auto dst_subtile = dst.subtile<tmem<typename TM::dtype, warp_rows, TM::cols>>(warp_rows*(warpid()/4+(warpid()%4)*(N_WARPS/4)), 0);
+    kittens::store(dst_subtile, src);
+}
