5c6d3818ee04f3fa616ab31c50ca15e4d9c0a580
Sylvie Liberman <sylv@research-common-25.cloud.together.ai>
2025-02-10 11:18:12 -0800

    setup

--- config.py
+++ config.py
@@ -58,12 +58,13 @@ sources = {
             'h100': 'kernels/matmul/FP8/matmul.cu'
         }
     },
+    "test": {"source_files": {"h100": "kernels/test/matmul.cu"}},
 }
 
 ### WHICH KERNELS DO WE WANT TO BUILD?
 # (oftentimes during development work you don't need to redefine them all.)
 # kernels = ['attn', 'mamba2', 'hedgehog', 'fftconv', 'fused_rotary', 'based', 'fused_layernorm']
-kernels = ['fp8_gemm']
+kernels = ['test']
 
 ### WHICH GPU TARGET DO WE WANT TO BUILD FOR?
 target = 'h100'
