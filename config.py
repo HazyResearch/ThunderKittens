6cac91bdafc8122374b1e9607f68ae77c3184732
technillogue <technillogue@gmail.com>
2025-02-20 18:19:47 -0500

    rename

--- config.py
+++ config.py
@@ -58,13 +58,13 @@ sources = {
             'h100': 'kernels/matmul/FP8/matmul.cu'
         }
     },
-    "test": {"source_files": {"h100": "kernels/test/matmul.cu"}},
+    "bmm": {"source_files": {"h100": "kernels/bmm/matmul.cu"}},
 }
 
 ### WHICH KERNELS DO WE WANT TO BUILD?
 # (oftentimes during development work you don't need to redefine them all.)
 # kernels = ['attn', 'mamba2', 'hedgehog', 'fftconv', 'fused_rotary', 'based', 'fused_layernorm']
-kernels = ['test']
+kernels = ['bmm']
 
 ### WHICH GPU TARGET DO WE WANT TO BUILD FOR?
 target = 'h100'
