ae59907ee554979a046b3d72efa698ff27929403
Benjamin Spector <benjaminfspector@gmail.com>
2025-03-12 07:55:24 +0000

    added some timing infra

--- kernels/attn/b200/test_attn_pytorch.py
+++ kernels/attn/b200/test_attn_pytorch.py
@@ -1,5 +1,6 @@
 import torch
 import b200_attn
+import time
 
 def ref_attn(q, k, v):
     B, H, N, D = q.shape
@@ -9,8 +10,8 @@ def ref_attn(q, k, v):
     o = torch.matmul(a, v)
     return o
 
-B = 1
-H = 1
+B = 16
+H = 16
 N = 8192
 D = 128
 
@@ -20,12 +21,24 @@ v = torch.randn((B, H, N, D), dtype=torch.bfloat16, device='cuda')
 l = torch.empty((B, H, 1, N), dtype=torch.float, device='cuda')
 o = torch.empty((B, H, N, D), dtype=torch.bfloat16, device='cuda')
 
-b200_attn.fwd_attend_ker_128_noncausal(q, k, v, l, o)
+stream = torch.cuda.current_stream()
 
-breakpoint()
+torch.cuda.synchronize()
+t0 = time.time()
+for _ in range(100):
+    b200_attn.fwd_attend_ker_128_noncausal(q, k, v, l, o)
+torch.cuda.synchronize()
+t1 = time.time()
+flops = 4 * B * H * N * N * D
+avg_us = (t1 - t0)/100*1e6
+print(f'Time taken: {avg_us} us/iter')
+print(f'Total FLOPs: {flops}')
+print(f'TFLOPS: {(flops / (avg_us*1e-6))*10**-12} TFLOPS')
+
 print(o.abs().max())
 
 ref = ref_attn(q, k, v)
 print(ref.abs().max())
 
 print((o - ref).abs().max())
+print((o - ref).abs().mean())
