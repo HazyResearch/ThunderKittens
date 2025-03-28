e8930151c84bfa04a73e8bfff7ee1bb404e0e05f
StuartSul <ssstuartss@gmail.com>
2025-03-28 21:25:13 +0000

    add axes looper

--- tests/unit/warp/memory/tile/pgl_to_register.cu
+++ tests/unit/warp/memory/tile/pgl_to_register.cu
@@ -2,18 +2,18 @@
 
 #ifdef TEST_WARP_MEMORY_TILE_PGL_TO_REGISTER
 
-template<typename Ker, int H, int W, int NW, kittens::ducks::pgl::all PGL>
+template<typename Ker, int H, int W, int NW, kittens::ducks::pgl::all PGL, typename... args>
 static __global__ void p2r_global_wrapper_2d(const __grid_constant__ PGL input, const __grid_constant__ PGL output, const __grid_constant__ int dev_idx) {
-    Ker::template device_func<H, W, NW, PGL>(input, output, dev_idx);
+    Ker::template device_func<H, W, NW, PGL, args...>(input, output, dev_idx);
 }
 
-template<typename test, int NUM_DEVICES, int H, int W, int NUM_WORKERS, typename... args>
+template<typename test, int NUM_DEVICES, int H, int W, int NUM_WORKERS, typename axis, typename... args>
 struct p2r_test_wrapper_2d {
     constexpr static int B = 3, D = 1, R = 4, C = 5; // arbitrary mix of prime/composite numbers
     constexpr static int SIZE = H*W*256 * B * D * R * C;
 
     using dtype = gmem_dtype<test>; // defaults to bf16 in global memory if the test doesn't specify.
-    using PGL = kittens::pgl<kittens::gl<dtype, -1, D, -1, 16*C*W>, NUM_DEVICES, true>;
+    using PGL = kittens::pgl<kittens::gl<dtype, -1, -1, -1, 16*C*W>, NUM_DEVICES, true>;
 
     static void run(test_data& results) {
         int device_count;
@@ -23,7 +23,8 @@ struct p2r_test_wrapper_2d {
             return;
         }
         test_info this_result;
-        this_result.label = generate_test_name<H, W, NUM_WORKERS>(test::test_identifier);
+        this_result.label = generate_test_name<H, W, NUM_WORKERS, axis, args...>(test::test_identifier);
+        static_assert(axis::value==0 || axis::value==1 || axis::value==2, "Axis must be 0, 1, or 2.");
         if constexpr (test::template valid<H, W, NUM_WORKERS>::value) {
             // initialize
             dtype *d_i_arr[NUM_DEVICES];
@@ -34,22 +35,22 @@ struct p2r_test_wrapper_2d {
             for (int dev_idx = 0; dev_idx < NUM_DEVICES; ++dev_idx) device_ids[dev_idx] = dev_idx;
             initialize<NUM_DEVICES>(device_ids, d_i_arr, d_o_arr, i_ref, o_ref);
             // make parralel global layouts
-            PGL input(device_ids, d_i_arr, B, nullptr, 16*R*H, nullptr);
-            PGL output(device_ids, d_o_arr, B, nullptr, 16*R*H, nullptr);
+            PGL input(device_ids, d_i_arr, (axis::value==0?H*16:1)*B, (axis::value==1?H*16:1)*D, (axis::value==2?H*16:1)*R, nullptr);
+            PGL output(device_ids, d_o_arr, (axis::value==0?H*16:1)*B, (axis::value==1?H*16:1)*D, (axis::value==2?H*16:1)*R, nullptr);
             // run kernel
             for (int dev_idx = 0; dev_idx < NUM_DEVICES; ++dev_idx) {
                 cudaSetDevice(dev_idx);
                 cudaFuncSetAttribute(
-                    p2r_global_wrapper_2d<test, H, W, NUM_WORKERS, PGL>,
+                    p2r_global_wrapper_2d<test, H, W, NUM_WORKERS, PGL, axis, args...>,
                     cudaFuncAttributeMaxDynamicSharedMemorySize,
                     kittens::MAX_SHARED_MEMORY
                 );
-                p2r_global_wrapper_2d<test, H, W, NUM_WORKERS, PGL><<<1, NUM_WORKERS*32>>>(input, output, dev_idx);
+                p2r_global_wrapper_2d<test, H, W, NUM_WORKERS, PGL, axis, args...><<<1, NUM_WORKERS*32>>>(input, output, dev_idx);
                 cudaDeviceSynchronize();
                 CudaCheckError();
             }
             // fill in correct results on cpu
-            test::template host_func<H, W, NUM_WORKERS, PGL>(i_ref, o_ref);
+            test::template host_func<H, W, NUM_WORKERS, PGL, axis>(i_ref, o_ref);
             // check and cleanup
             this_result.result = validate<NUM_DEVICES, PGL, dtype>(input, output, i_ref, o_ref, this_result.label, W * 16);
         }
@@ -71,7 +72,7 @@ struct p2r_all_reduce_test {
     static inline const std::string test_identifier = std::is_same_v<T, kittens::bf16> ? "p2r_all_reduce=bf16,op=" + op_identifier :
                                                       std::is_same_v<T, kittens::half> ? "p2r_all_reduce=half,op=" + op_identifier :
                                                                                          "p2r_all_reduce=float,op=" + op_identifier;
-    template<int H, int W, int NW, kittens::ducks::pgl::all PGL>
+    template<int H, int W, int NW, kittens::ducks::pgl::all PGL, typename axis>
     __host__ static void host_func(const std::vector<std::vector<float>> &i_ref, std::vector<std::vector<float>> &o_ref) {
         // each vector represents a GPU device holding the data
         for (int dev_idx = 0; dev_idx < i_ref.size(); ++dev_idx) {
@@ -89,21 +90,25 @@ struct p2r_all_reduce_test {
             }
         }
     }
-    template<int H, int W, int NW, kittens::ducks::pgl::all PGL>
+    template<int H, int W, int NW, kittens::ducks::pgl::all PGL, typename axis>
     __device__ static void device_func(const PGL &input, const PGL &output, const int dev_idx) {
-        kittens::rt<dtype,  16*H, 16*W, kittens::ducks::rt_layout::row> reg_tile; // currently only row-major layout is supported
-        for(int i = 0; i < input[dev_idx].batch(); i++) {
-            for(int j = 0; j < input[dev_idx].depth(); j++) {
-                for(int k = 0; k < input[dev_idx].rows()/reg_tile.rows; k++) {
+        using RT = kittens::rt<dtype,  16*H, 16*W, kittens::ducks::rt_layout::row>;
+        RT reg_tile; // currently only row-major layout is supported
+        int num_batches = axis::value==0 ? ((int)input[dev_idx].batch()/reg_tile.rows) : (int)input[dev_idx].batch();
+        int num_depths = axis::value==1 ? ((int)input[dev_idx].depth()/reg_tile.rows) : (int)input[dev_idx].depth();
+        int num_rows = axis::value==2 ? ((int)input[dev_idx].rows()/reg_tile.rows) : (int)input[dev_idx].rows();
+        for(int i = 0; i < num_batches; i++) {
+            for(int j = 0; j < num_depths; j++) {
+                for(int k = 0; k < num_rows; k++) {
                     for(int l = 0; l < input[dev_idx].cols()/reg_tile.cols; l++) {
                         if constexpr (op == kittens::ReduceOp::ADD) {
-                            kittens::all_reduce_add(reg_tile, input, dev_idx, {i, j, k, l});
+                            kittens::all_reduce_add<axis::value, RT, PGL, kittens::coord<RT>>(reg_tile, input, dev_idx, {i, j, k, l});
                         } else if constexpr (op == kittens::ReduceOp::MIN) {
-                            kittens::all_reduce_min(reg_tile, input, dev_idx, {i, j, k, l});
+                            kittens::all_reduce_min<axis::value, RT, PGL, kittens::coord<RT>>(reg_tile, input, dev_idx, {i, j, k, l});
                         } else if constexpr (op == kittens::ReduceOp::MAX) {
-                            kittens::all_reduce_max(reg_tile, input, dev_idx, {i, j, k, l});
+                            kittens::all_reduce_max<axis::value, RT, PGL, kittens::coord<RT>>(reg_tile, input, dev_idx, {i, j, k, l});
                         }
-                        kittens::store(output[dev_idx], reg_tile, {i, j, k, l});
+                        kittens::store<axis::value, RT, PGL::_GL, kittens::coord<RT>>(output[dev_idx], reg_tile, {i, j, k, l});
                     }
                 }
             }
@@ -117,6 +122,19 @@ using p2r_sweep_size_2d = mg_loop_h<p2r_test_wrapper_2d, test, NUM_DEVICES, MAX_
 template<typename test, int NUM_DEVICES, int MAX_H=8, int MAX_W=8, typename... args> 
 using p2r_sweep_size_2d_warp = p2r_sweep_size_2d<test, NUM_DEVICES, MAX_H, MAX_W, 1, args...>;
 
+template<typename test, int NUM_DEVICES, int MAX_H=8, int MAX_W=8, typename... args>
+struct p2r_sweep_size_2d_warp_axes {
+    using I0_t = std::integral_constant<int, 0>;
+    using I1_t = std::integral_constant<int, 1>;
+    using I2_t = std::integral_constant<int, 2>;
+
+    static void run(test_data &results) {
+        p2r_sweep_size_2d_warp<test, NUM_DEVICES, MAX_H, MAX_W, I2_t>::run(results);
+        p2r_sweep_size_2d_warp<test, NUM_DEVICES, MAX_H, MAX_W, I1_t>::run(results);
+        p2r_sweep_size_2d_warp<test, NUM_DEVICES, MAX_H, MAX_W, I0_t>::run(results);
+    }
+};
+
 void warp::memory::tile::pgl_to_register::tests(test_data &results) {
     std::cout << "\n ----- Starting ops/warp/memory/tile/pgl_to_register tests! -----\n" << std::endl;
     constexpr int NUM_DEVICES = 8;
@@ -125,7 +143,8 @@ void warp::memory::tile::pgl_to_register::tests(test_data &results) {
                          INTENSITY_3 ? 8  :
                          INTENSITY_4 ? 16 : -1;
 
-    p2r_sweep_size_2d_warp<p2r_all_reduce_test<float, kittens::ReduceOp::ADD>, NUM_DEVICES, SIZE, SIZE>::run(results);
+    p2r_sweep_size_2d_warp_axes<p2r_all_reduce_test<float, kittens::ReduceOp::ADD>, NUM_DEVICES, SIZE, SIZE>::run(results);
+    p2r_sweep_size_2d_warp_axes<p2r_all_reduce_test<kittens::bf16, kittens::ReduceOp::ADD>, NUM_DEVICES, SIZE, SIZE>::run(results);
 }
 
 #endif
