dfc5f554e1c1758fbfb57a3f45eb687e7ff94be0
StuartSul <ssstuartss@gmail.com>
2025-04-06 09:22:28 +0000

    wip

--- include/ops/gang/gang.cuh
+++ include/ops/gang/gang.cuh
@@ -18,18 +18,19 @@ template <int NUM_DEVICES>
 struct gang {
 
 /**
- * @brief Synchronize all GPUs in a gang at a specific sync point. 
- *        This is a block-level sync (i.e., only thread blocks with
- *        the same block ID across all GPUs will be synchronized).
- *        Be warned that if the number of thread blocks is too large 
- *        (more than 2x the HW maximum), there might be a deadlock if
- *        two GPU devices schedule completely different set of blocks.
+ * @brief Synchronizes all GPUs in a gang at a specific synchronization point.
+ *
+ * This is a block-level synchronization, meaning that only thread blocks
+ * with the same block ID across all GPUs will be synchronized. 
+ *
+ * Caution: If the total number of active thread blocks on a GPU device 
+ * exceeds approximately twice the hardware limit, a deadlock may occur.
  */
 template <ducks::sync_manager::all SyncManager>
-__device__ static inline void sync(const SyncManager &sm, const int sync_id, const int dev_idx) {
+__device__ static inline void block_sync(const SyncManager &sm, const int sync_id, const int dev_idx) {
     #if defined(__CUDA_ARCH__)
         static_assert(__CUDA_ARCH__ >= 900, 
-            "Using gang::sync() requires CUDA compute capability >= 9.0 (Hopper or newer)");
+            "Using gang::block_sync() requires CUDA compute capability >= 9.0 (Hopper or newer)");
     #endif
     static_assert(NUM_DEVICES <= SyncManager::SYNC_SPACE_T::num_devices, 
         "Number of devices in the gang cannot be greater than that in the sync manager");
@@ -61,7 +62,62 @@ __device__ static inline void sync(const SyncManager &sm, const int sync_id, con
     }
 
     // Must block all threads until thread 0 completes the sync
-    // Very certain that the two syncthreads are both needed
+    __syncthreads();
+}
+
+/**
+ * @brief Synchronizes all GPUs in a gang at a specific synchronization point.
+ *
+ * This is a grid-level synchronization, meaning that all thread blocks across
+ * all GPUs will be synchronized.
+ *
+ * Caution: If not all thread blocks are active on a GPU device, a deadlock will occur.
+ */
+template <ducks::sync_manager::all SyncManager>
+__device__ static inline void grid_sync(const SyncManager &sm, const int sync_id, const int dev_idx) {
+    #if defined(__CUDA_ARCH__)
+        static_assert(__CUDA_ARCH__ >= 900, 
+            "Using gang::grid_sync() requires CUDA compute capability >= 9.0 (Hopper or newer)");
+    #endif
+
+    if (dev_idx >= NUM_DEVICES) return;
+
+    __threadfence_system();
+    __syncthreads();
+
+    // All the code from here is simply asking every thread/block on all devices "did you arrive here yet?"
+    if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) {
+        sync_point sp = sm.get_sync_point(sync_id, dev_idx);
+        cuda::atomic_ref<typename SyncManager::SYNC_SPACE_DTYPE, cuda::thread_scope_device> uc0(sp.uc[0]);
+        cuda::atomic_ref<typename SyncManager::SYNC_SPACE_DTYPE, cuda::thread_scope_system> uc1(sp.uc[1]);
+
+        int block_idx = blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y;
+        if (block_idx == 0) { // Block 0 is the leader block
+            size_t nblocks = gridDim.x * gridDim.y * gridDim.z;
+            while (uc0.load(cuda::memory_order_acquire) < nblocks - 1); // Wait for all non-leader blocks to check in
+
+            // At this point, all threads across all blocks on the current device have now reached 
+            // gang::grid_sync() and are waiting.
+
+            // Now, sync all leader blocks across all devices
+            asm volatile ("{multimem.red.release.sys.global.add.u32 [%0], %1;}" 
+                :: "l"(&sp.mc[1]), "n"(1) : "memory");
+            asm volatile ("{fence.proxy.alias;}" ::: "memory"); // nvidia says this is needed
+            while (uc1.load(cuda::memory_order_acquire) < NUM_DEVICES);
+
+            // At this point, all threads across all blocks on all devices have now reached
+            // gang::grid_sync() and are waiting.
+
+            // Release all blocks
+            uc1.store(0, cuda::memory_order_release); // Do this before releasing non-leader blocks
+            uc0.store(0, cuda::memory_order_release); // Release non-leader blocks
+        } else {
+            uc0++; // "check-in"
+            while (uc0.load(cuda::memory_order_acquire) > 0);
+        }
+    }
+
+    // Make all threads wait for thread 0
     __syncthreads();
 }
 
--- include/types/device/sync_manager.cuh
+++ include/types/device/sync_manager.cuh
@@ -24,22 +24,39 @@ template<typename T> concept all = requires {
 } // namespace ducks
 
 template <typename SYNC_SPACE_DTYPE = int>
-struct sync_point {
+struct block_sync_point {
     SYNC_SPACE_DTYPE *mc;
     SYNC_SPACE_DTYPE *uc;
+};
+
+template <typename SYNC_SPACE_DTYPE = int>
+struct grid_sync_point {
+    SYNC_SPACE_DTYPE *gang_mc;
+    SYNC_SPACE_DTYPE *gang_uc;
+    SYNC_SPACE_DTYPE *grid_uc;
 };
 
 /**
  * @brief The sync_manager struct manages multicast memory spaces across 
  * multiple GPUs for synchronization. It is just a named PGL under the hood.
+ * 
+ * If you want to call gang::grid_sync(...), MAX_BLOCKS must be -1.
+ * If you want to call gang::block_sync(...), set MAX_BLOCKS to the 
+ * maximum number of blocks you expect to use on any device.
  */
-template <int NUM_DEVICES, int MAX_BLOCKS = 256, int MAX_SYNC_POINTS = 4>
+template <int NUM_DEVICES, int MAX_BLOCKS = -1, int MAX_SYNC_POINTS = 4>
 struct sync_manager {
     using identifier = ducks::sync_manager::identifier;
 
-    static constexpr int max_blocks = MAX_BLOCKS;
+    static_assert(NUM_DEVICES > 0, "NUM_DEVICES must be greater than 0");
+    static_assert(MAX_BLOCKS == -1 || MAX_BLOCKS > 0, "MAX_BLOCKS must be greater than 0 or -1");
+    static_assert(MAX_SYNC_POINTS > 0, "MAX_SYNC_POINTS must be greater than 0");
+
+    static constexpr bool is_grid_sync = MAX_BLOCKS == -1;
+    static constexpr int sync_point_size = is_grid_sync ? 2 : MAX_BLOCKS;
     static constexpr int max_sync_points = MAX_SYNC_POINTS;
-    static constexpr int sync_space_size = max_blocks * max_sync_points;
+    static constexpr int sync_space_size = sync_point_size * max_sync_points;
+
     using SYNC_SPACE_DTYPE = int;
     using SYNC_SPACE_T = pgl<gl<SYNC_SPACE_DTYPE, 1, 1, 1, sync_space_size>, NUM_DEVICES, true>;
     SYNC_SPACE_T sync_space;
@@ -69,10 +86,28 @@ struct sync_manager {
         pglFree(sync_space);
     }
 
-    __device__ inline sync_point<SYNC_SPACE_DTYPE> get_sync_point(const int sync_id, const int dev_idx, const int block_idx) const {
-        return sync_point{
-            sync_space.mc_vas[dev_idx] + sync_id * max_blocks + block_idx,
-            sync_space[dev_idx].raw_ptr + sync_id * max_blocks + block_idx,
+    /*
+     * Get the sync point for a specific sync ID, device index, and block index.
+     * This is used for block-level synchronization. (gang::block_sync)
+     */
+    __device__ inline block_sync_point<SYNC_SPACE_DTYPE> get_sync_point(const int sync_id, const int dev_idx, const int block_idx) const {
+        static_assert(!is_grid_sync, "You cannot specify block_idx for grid-level sync");
+        return block_sync_point{
+            sync_space.mc_vas[dev_idx] + sync_id * sync_point_size + block_idx,
+            sync_space[dev_idx].raw_ptr + sync_id * sync_point_size + block_idx,
+        };
+    }
+
+    /*
+     * Get the sync point for a specific sync ID and device index.
+     * This is used for grid-level synchronization. (gang::grid_sync)
+     */
+    __device__ inline grid_sync_point<SYNC_SPACE_DTYPE> get_sync_point(const int sync_id, const int dev_idx) const {
+        static_assert(is_grid_sync, "You need to specify block_idx for block-level sync");
+        return grid_sync_point{
+            sync_space.mc_vas[dev_idx] + sync_id * sync_point_size,
+            sync_space[dev_idx].raw_ptr + sync_id * sync_point_size,
+            sync_space[dev_idx].raw_ptr + sync_id * sync_point_size + 1,
         };
     }
 };
