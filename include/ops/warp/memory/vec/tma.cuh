aaf9ea1e1b7d4431c7704a22faa313697203a6b1
StuartSul <ssstuartss@gmail.com>
2025-04-06 02:43:26 +0000

    add vec tma ops

--- include/ops/warp/memory/vec/tma.cuh
+++ include/ops/warp/memory/vec/tma.cuh
@@ -101,6 +101,42 @@ template<ducks::sv::all SV, ducks::gl::all GL, ducks::coord::vec COORD=coord<SV>
 __device__ static inline void store_async(const GL &dst, const SV &src, const COORD &idx) {
     store_async<cache_policy::NORMAL>(dst, src, idx);
 }
+template<cache_policy policy, ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
+    coord<> unit_coord = idx.template unit_coord<-1, 3>();
+    uint64_t tma_ptr  = reinterpret_cast<uint64_t>(dst.template get_tma<SV, -1>(dev_idx));
+    uint32_t src_ptr  = static_cast<uint32_t>(__cvta_generic_to_shared(&src));
+    for(int i = ::kittens::laneid(); i < detail::sv_tma_dim2<SV>; i += WARP_THREADS) {
+        coord<> tma_coord = unit_coord;
+        tma_coord.c += i * detail::sv_tma_dim1<SV>;
+        uint32_t src_i_ptr = src_ptr + i*detail::sv_tma_dim1<SV>*sizeof(typename SV::dtype);
+        
+        asm volatile("fence.proxy.async.shared::cta;\n" ::: "memory");
+        if constexpr (policy == cache_policy::NORMAL) {
+            asm volatile (
+                "cp.async.bulk.tensor.4d.global.shared::cta.tile.bulk_group"
+                " [%0, {%2, %3, %4, %5}], [%1];"
+                :
+                : "l"(tma_ptr), "r"(src_i_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b)
+                : "memory"
+            );
+        }
+        else {
+            asm volatile (
+                "cp.async.bulk.tensor.4d.global.shared::cta.tile.bulk_group.L2::cache_hint"
+                " [%0, {%2, %3, %4, %5}], [%1], %6;"
+                :
+                : "l"(tma_ptr), "r"(src_i_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b), "l"(make_cache_policy<policy>())
+                : "memory"
+            );
+        }
+    }
+    store_commit_group();
+}
+template<ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
+    store_async<cache_policy::NORMAL>(dst, src, idx);
+}
 
 /**
 * @brief Asynchronously performs an add reduction and stores the result into global memory.
@@ -146,6 +182,41 @@ __device__ static inline void store_add_async(const GL &dst, const SV &src, cons
 }
 template<ducks::sv::all SV, ducks::gl::all GL, ducks::coord::vec COORD=coord<SV>>
 __device__ static inline void store_add_async(const GL &dst, const SV &src, const COORD &idx) {
+    store_add_async<cache_policy::NORMAL>(dst, src, idx);
+}template<cache_policy policy, ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_add_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
+    coord<> unit_coord = idx.template unit_coord<-1, 3>();
+    uint64_t tma_ptr  = reinterpret_cast<uint64_t>(dst.template get_tma<SV, -1>(dev_idx));
+    uint32_t src_ptr  = static_cast<uint32_t>(__cvta_generic_to_shared(&src));
+    for(int i = ::kittens::laneid(); i < detail::sv_tma_dim2<SV>; i += WARP_THREADS) {
+        coord<> tma_coord = unit_coord;
+        tma_coord.c += i * detail::sv_tma_dim1<SV>;
+        uint32_t src_tma_ptr = tma_ptr + i*detail::sv_tma_dim1<SV>*sizeof(typename SV::dtype);
+        
+        asm volatile("fence.proxy.async.shared::cta;\n" ::: "memory");
+        if constexpr (policy == cache_policy::NORMAL) {
+            asm volatile (
+                "cp.reduce.async.bulk.tensor.4d.global.shared::cta.add.tile.bulk_group"
+                " [%0, {%2, %3, %4, %5}], [%1];"
+                :
+                : "l"(tma_ptr), "r"(src_tma_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b)
+                : "memory"
+            );
+        }
+        else {
+            asm volatile (
+                "cp.reduce.async.bulk.tensor.4d.global.shared::cta.add.tile.bulk_group.L2::cache_hint"
+                " [%0, {%2, %3, %4, %5}], [%1], %6;"
+                :
+                : "l"(tma_ptr), "r"(src_tma_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b), "l"(make_cache_policy<policy>())
+                : "memory"
+            );
+        }
+    }
+    store_commit_group();
+}
+template<ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_add_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
     store_add_async<cache_policy::NORMAL>(dst, src, idx);
 }
 
@@ -196,6 +267,43 @@ template<ducks::sv::all SV, ducks::gl::all GL, ducks::coord::vec COORD=coord<SV>
 __device__ static inline void store_min_async(const GL &dst, const SV &src, const COORD &idx) {
     store_min_async<cache_policy::NORMAL>(dst, src, idx);
 }
+template<cache_policy policy, ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_min_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
+    static_assert(!std::is_same_v<typename SV::dtype, float>, "TMA does not support async min/max reductions for fp32 types.");
+    coord<> unit_coord = idx.template unit_coord<-1, 3>();
+    uint64_t tma_ptr  = reinterpret_cast<uint64_t>(dst.template get_tma<SV, -1>(dev_idx));
+    uint32_t src_ptr  = static_cast<uint32_t>(__cvta_generic_to_shared(&src));
+    for(int i = ::kittens::laneid(); i < detail::sv_tma_dim2<SV>; i += WARP_THREADS) {
+        coord<> tma_coord = unit_coord;
+        tma_coord.c += i * detail::sv_tma_dim1<SV>;
+        uint32_t src_i_ptr = src_ptr + i*detail::sv_tma_dim1<SV>*sizeof(typename SV::dtype);
+        
+        asm volatile("fence.proxy.async.shared::cta;\n" ::: "memory");
+        if constexpr (policy == cache_policy::NORMAL) {
+            asm volatile (
+                "cp.reduce.async.bulk.tensor.4d.global.shared::cta.min.tile.bulk_group"
+                " [%0, {%2, %3, %4, %5}], [%1];"
+                :
+                : "l"(tma_ptr), "r"(src_i_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b)
+                : "memory"
+            );
+        }
+        else {
+            asm volatile (
+                "cp.reduce.async.bulk.tensor.4d.global.shared::cta.min.tile.bulk_group.L2::cache_hint"
+                " [%0, {%2, %3, %4, %5}], [%1], %6;"
+                :
+                : "l"(tma_ptr), "r"(src_i_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b), "l"(make_cache_policy<policy>())
+                : "memory"
+            );
+        }
+    }
+    store_commit_group();
+}
+template<ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_min_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
+    store_min_async<cache_policy::NORMAL>(dst, src, idx);
+}
 
 /**
 * @brief Asynchronously performs an max reduction and stores the result into global memory.
@@ -242,6 +350,43 @@ __device__ static inline void store_max_async(const GL &dst, const SV &src, cons
 }
 template<ducks::sv::all SV, ducks::gl::all GL, ducks::coord::vec COORD=coord<SV>>
 __device__ static inline void store_max_async(const GL &dst, const SV &src, const COORD &idx) {
+    store_max_async<cache_policy::NORMAL>(dst, src, idx);
+}
+template<cache_policy policy, ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_max_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
+    static_assert(!std::is_same_v<typename SV::dtype, float>, "TMA does not support async min/max reductions for fp32 types.");
+    coord<> unit_coord = idx.template unit_coord<-1, 3>();
+    uint64_t tma_ptr  = reinterpret_cast<uint64_t>(dst.template get_tma<SV, -1>(dev_idx));
+    uint32_t src_ptr  = static_cast<uint32_t>(__cvta_generic_to_shared(&src));
+    for(int i = ::kittens::laneid(); i < detail::sv_tma_dim2<SV>; i += WARP_THREADS) {
+        coord<> tma_coord = unit_coord;
+        tma_coord.c += i * detail::sv_tma_dim1<SV>;
+        uint32_t src_i_ptr = src_ptr + i*detail::sv_tma_dim1<SV>*sizeof(typename SV::dtype);
+        
+        asm volatile("fence.proxy.async.shared::cta;\n" ::: "memory");
+        if constexpr (policy == cache_policy::NORMAL) {
+            asm volatile (
+                "cp.reduce.async.bulk.tensor.4d.global.shared::cta.max.tile.bulk_group"
+                " [%0, {%2, %3, %4, %5}], [%1];"
+                :
+                : "l"(tma_ptr), "r"(src_i_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b)
+                : "memory"
+            );
+        }
+        else {
+            asm volatile (
+                "cp.reduce.async.bulk.tensor.4d.global.shared::cta.max.tile.bulk_group.L2::cache_hint"
+                " [%0, {%2, %3, %4, %5}], [%1], %6;"
+                :
+                : "l"(tma_ptr), "r"(src_i_ptr), "r"(tma_coord.c), "r"(tma_coord.r), "r"(tma_coord.d), "r"(tma_coord.b), "l"(make_cache_policy<policy>())
+                : "memory"
+            );
+        }
+    }
+    store_commit_group();
+}
+template<ducks::sv::all SV, ducks::pgl::all PGL, ducks::coord::vec COORD=coord<SV>>
+__device__ static inline void store_max_async(const PGL &dst, const SV &src, const COORD &idx, const int dev_idx) {
     store_max_async<cache_policy::NORMAL>(dst, src, idx);
 }
 
