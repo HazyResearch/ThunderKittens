91547f014ff0db051c2c3221fb8ba380b7c84504
StuartSul <ssstuartss@gmail.com>
2025-04-08 01:15:04 +0000

    versatile sync manager

--- include/types/device/sync_manager.cuh
+++ include/types/device/sync_manager.cuh
@@ -23,47 +23,34 @@ template<typename T> concept all = requires {
 } // namespace sync_manager
 } // namespace ducks
 
-enum sync_level {
-    GRID = 0,
-    BLOCK = 1
-};
-
-template <typename SYNC_SPACE_DTYPE, sync_level LEVEL>
+template <typename SYNC_SPACE_DTYPE>
 struct sync_point {
-    static_assert(std::is_same_v<SYNC_SPACE_DTYPE, std::true_type>, "Invalid sync level.");
-};
-template <typename SYNC_SPACE_DTYPE>
-struct sync_point<SYNC_SPACE_DTYPE, sync_level::BLOCK> {
-    SYNC_SPACE_DTYPE *gang_mc;
-    SYNC_SPACE_DTYPE *gang_uc;
-};
-template <typename SYNC_SPACE_DTYPE>
-struct sync_point<SYNC_SPACE_DTYPE, sync_level::GRID> {
-    SYNC_SPACE_DTYPE *gang_mc;
-    SYNC_SPACE_DTYPE *gang_uc;
-    SYNC_SPACE_DTYPE *grid_uc;
+    SYNC_SPACE_DTYPE *sys_mc;
+    SYNC_SPACE_DTYPE *sys_uc;
+    SYNC_SPACE_DTYPE *dev_uc = nullptr;
 };
 
 /**
  * @brief The sync_manager struct manages multicast memory spaces across 
  * multiple GPUs for synchronization. It is just a named PGL under the hood.
  * 
- * If sync_level is BLOCK, you must set MAX_BLOCKS.
- * If sync_level is GRID, MAX_BLOCKS is ignored.
+ * This struct is meant to be passed to functions under the gang::sync namespace.
+ * You can pass it to gang::sync::all(...) by default
+ * You can pass it to gang::sync::blockwise(...) if MAX_BLOCKS > 0
+ * You can pass it to gang::sync::blockgroup(...) if MAX_SYNC_POINTS > 0
  */
-template <int NUM_DEVICES, sync_level LEVEL, int MAX_SYNC_POINTS = 4, int MAX_BLOCKS = -1>
+template <int NUM_DEVICES, int MAX_BLOCKS = 256, int MAX_SYNC_POINTS = 16>
 struct sync_manager {
-    using identifier = ducks::sync_manager::identifier;
-
     static_assert(NUM_DEVICES > 0, "NUM_DEVICES must be greater than 0");
-    static_assert(LEVEL == sync_level::BLOCK || LEVEL == sync_level::GRID, "LEVEL must be either BLOCK or GRID");
-    static_assert(LEVEL == sync_level::GRID || MAX_BLOCKS > 0, "MAX_BLOCKS must be greater than 0 for BLOCK level sync");
-    static_assert(MAX_SYNC_POINTS > 0, "MAX_SYNC_POINTS must be greater than 0");
 
-    static constexpr sync_level level = LEVEL;
-    static constexpr int sync_point_size = level == sync_level::GRID ? 2 : MAX_BLOCKS;
+    using identifier = ducks::sync_manager::identifier;
+    static constexpr int num_devices = NUM_DEVICES;
+    static constexpr int all_sync_point_size = 2;
+    static constexpr int max_blocks = MAX_BLOCKS;
     static constexpr int max_sync_points = MAX_SYNC_POINTS;
-    static constexpr int sync_space_size = sync_point_size * max_sync_points;
+    static constexpr int sync_space_size = all_sync_point_size /* sync::all */ + 
+                                           MAX_BLOCKS /* sync::blockwise */ + 
+                                           MAX_SYNC_POINTS /* sync::blockgroup */;
 
     using SYNC_SPACE_DTYPE = int;
     using SYNC_SPACE_T = pgl<gl<SYNC_SPACE_DTYPE, 1, 1, 1, sync_space_size>, NUM_DEVICES, true>;
@@ -94,25 +81,28 @@ struct sync_manager {
         pglFree(sync_space);
     }
 
-    /*
-     * Get the sync point for a specific sync ID.
-     * block_idx is only used for block level sync, ignored for grid-level sync.
-     */
-    __device__ inline sync_point<SYNC_SPACE_DTYPE, LEVEL> get_sync_point(const int sync_id, const int dev_idx, const int block_idx = -1) const {
-        if constexpr (LEVEL == sync_level::GRID) {
-            return sync_point<SYNC_SPACE_DTYPE, LEVEL>{
-                sync_space.mc_vas[dev_idx] + sync_id * sync_point_size,
-                sync_space[dev_idx].raw_ptr + sync_id * sync_point_size,
-                sync_space[dev_idx].raw_ptr + sync_id * sync_point_size + 1,
-            };
-        } else if constexpr (LEVEL == sync_level::BLOCK) {
-            return sync_point<SYNC_SPACE_DTYPE, LEVEL>{
-                sync_space.mc_vas[dev_idx] + sync_id * sync_point_size + block_idx,
-                sync_space[dev_idx].raw_ptr + sync_id * sync_point_size + block_idx,
-            };
-        } else {
-            return sync_point<SYNC_SPACE_DTYPE, LEVEL>{}; // this will raise a compile-time error
-        }
+    __device__ inline auto get_all_sync_point(const int dev_idx) const {
+        return sync_point<SYNC_SPACE_DTYPE>{
+            sync_space.mc_vas[dev_idx],
+            sync_space[dev_idx].raw_ptr,
+            sync_space[dev_idx].raw_ptr + 1
+        };
+    }
+
+    __device__ inline auto get_blockwise_sync_point(const int dev_idx, const int block_idx) const {
+        return sync_point<SYNC_SPACE_DTYPE>{
+            sync_space.mc_vas[dev_idx] + all_sync_point_size + block_idx,
+            sync_space[dev_idx].raw_ptr + all_sync_point_size + block_idx,
+            nullptr // unused
+        };
+    }
+    
+    __device__ inline auto get_blockgroup_sync_point(const int dev_idx, const int sync_id) const {
+        return sync_point<SYNC_SPACE_DTYPE>{
+            sync_space.mc_vas[dev_idx] + all_sync_point_size + max_blocks + sync_id,
+            sync_space[dev_idx].raw_ptr + all_sync_point_size + max_blocks + sync_id,
+            nullptr // unused
+        };
     }
 };
 
