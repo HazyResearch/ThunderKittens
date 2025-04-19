52f1813ebde98bb513232d0ff436057c13ea6bec
Stuart Sul <ssstuartss@gmail.com>
2025-04-19 01:14:16 +0000

    it works!

--- tests/vm/rope_gqa_partial/rope_gqa_partial.cu
+++ tests/vm/rope_gqa_partial/rope_gqa_partial.cu
@@ -16,16 +16,26 @@ using namespace kittens::prototype::vm;
 */
 
 constexpr int NUM_BLOCKS = 148;
-constexpr int ROPE_GQA_PARTIAL_OPCODE = 2;
+constexpr int ROPE_GQA_PARTIAL_OPCODE = 1;
 
 using config = default_config;
 struct globals {
+    using q_rt = rt_bf<32, 64>;
+    using kv_rt = rt_bf<32, 64>;
+    using o_rt = rt_fl<32, 32>;
+    using q_st = st_bf<32, 64>; // <= 16KB
+    using kv_st = st_bf<32, 64>;
+    using o_st = st_bf<32, 32>;
+    using q_layout = gl<bf16, 1, -1, -1, -1, q_st>; // assume single batch
+    using kv_layout = gl<bf16, 1, -1, -1, -1, kv_st>;
+    using o_layout = gl<bf16, 1, -1, -1, -1, o_st>; // assume single batch
     using instruction_layout = ::kittens::prototype::vm::instruction_layout<config>;
     using timing_layout = ::kittens::prototype::vm::timing_layout<config>;
-    using bf16_matrix = gl<bf16, 1, -1, -1, -1, st_bf<128, 128>>; // assume single batch
     instruction_layout instructions;
     timing_layout timings;
-    bf16_matrix Q, K_c, V_c, O;
+    q_layout Q;
+    kv_layout K_c, V_c;
+    o_layout O;
     dim3 grid() { return dim3(NUM_BLOCKS); }
     dim3 block() { return dim3(config::NUM_THREADS); }
     int dynamic_shared_memory() { return config::DYNAMIC_SHARED_MEMORY; }
@@ -35,54 +45,65 @@ template<typename config=config> struct rope_gqa_partial_op {
     static constexpr int opcode = ROPE_GQA_PARTIAL_OPCODE;
     static constexpr int PIPELINE_STAGES = 3;
 
-    static __device__ inline parsed_instruction parse_instruction(const globals &g, state<config> &s) {
-        return parsed_instruction{s.instruction()[1], s.instruction()[2], s.instruction()[3]};
+    __device__ static inline semaphore &inputs_arrived(state<config> &s) {
+        return s.semaphores()[0];
+    }
+    __device__ static inline semaphore &outputs_arrived(state<config> &s) {
+        return s.semaphores()[1];
     }
 
-    __device__ static inline semaphore &inputs_arrived(state<config> &s, int id) {
-        return s.semaphores()[id];
-    }
-    __device__ static inline semaphore &inputs_finished(state<config> &s, int id) {
-        return s.semaphores()[id+PIPELINE_STAGES];
-    }
-    __device__ static inline semaphore &outputs_arrived(state<config> &s, int id) {
-        return s.semaphores()[id+PIPELINE_STAGES*2];
-    }
-    __device__ static inline semaphore &outputs_shared(state<config> &s, int id) {
-        return s.semaphores()[id+PIPELINE_STAGES*2+4];
-    }
-    __device__ static inline int get_a_page(state<config> &s, int stage, int offset) {
-        return s.pid(stage*4 + offset);
-    }
-    __device__ static inline int get_b_page(state<config> &s, int stage, int offset) {
-        return s.pid(stage*4 + offset + 2);
-    }
-
-    __device__ static inline void debug(const char* msg) {
-        printf("Warp ID %d, Lane ID %d: %s\n", warpid(), laneid(), msg);
-    }
-    
     struct controller {
         static __device__ int release_lid(const globals &g, typename config::instruction_t &instruction, int &query) {
+            if (!blockIdx.x && !laneid()) {
+                printf("NUM_PAGES: %d\n", config::NUM_PAGES);
+            }
+
             int ret_order[13] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
             return ret_order[query];
         }
         static __device__ int init_semaphores(const globals &g, state<config> &s) {
-            // for(int i = 0; i < PIPELINE_STAGES; i++) {
-            //     init_semaphore(inputs_arrived(s, i), 1); // Inputs arrived.
-            //     init_semaphore(inputs_finished(s, i), 4); // Inputs finished.
-            // }
-            // for(int i = 0; i < 4; i++) {
-            //     init_semaphore(outputs_arrived(s, i), 1); // outputs arrived.
-            //     init_semaphore(outputs_shared(s, i), 1); // outputs shared.
-            // }
-            return 2*PIPELINE_STAGES + 8;
+            init_semaphore(inputs_arrived(s), 0, 1);
+            init_semaphore(outputs_arrived(s), 0, 1);
+            return 2;
         }
     };
     struct loader {
         static __device__ void run(const globals &g, state<config> &s) {
-            // debug("Loader run");
             // parsed_instruction inst = parse_instruction(g, s);
+
+            /* Initial of of Qi, Kj, Vj */
+            if (laneid() == 0) {
+                int Q_page_pid = s.pid(0);
+                int K_page_pid = s.pid(1);
+                int V_page_pid = s.pid(2);
+                s.wait_page_ready(Q_page_pid);
+                s.wait_page_ready(K_page_pid);
+                s.wait_page_ready(V_page_pid);
+                globals::q_st  &Q = *reinterpret_cast<globals::q_st*>(s.pages[Q_page_pid].data); // exactly 1 page (16KB)
+                globals::kv_st &K = *reinterpret_cast<globals::kv_st*>(s.pages[K_page_pid].data);
+                globals::kv_st &V = *reinterpret_cast<globals::kv_st*>(s.pages[V_page_pid].data);
+                tma::expect_bytes(inputs_arrived(s), sizeof(Q) + sizeof(K) + sizeof(V));
+                tma::load_async<dim::ROW, cache_policy::EVICT_FIRST>(Q, g.Q, {0, 0, 0, 0}, inputs_arrived(s));
+                tma::load_async<dim::ROW, cache_policy::EVICT_FIRST>(K, g.K_c, {0, 0, 0, 0}, inputs_arrived(s));
+                tma::load_async<dim::ROW, cache_policy::EVICT_FIRST>(V, g.V_c, {0, 0, 0, 0}, inputs_arrived(s));
+            }
+            // warp::sync();
+
+
+
+
+            // if(args.iter == 1) group<12>::sync(11); // wait for the consumer to finish its setup, before we do the second load.
+            // if(warpgroup::warpid() == 0) {
+            //     int pos = args.common.start_pos + NUM_ROWS*args.iter;
+            //     int within_page_idx = (pos % PAGE_SIZE) / NUM_ROWS;
+            //     int next_page_id = args.globals.Table[coord<>{args.common.q_batch_idx, pos/PAGE_SIZE}];
+            //     // next page we need to load?
+            //     tma::expect(args.inputs_arrived, args.input.kcache, args.input.vcache);
+            //     tma::load_async<dim::ROW, cache_policy::EVICT_FIRST>(args.input.kcache, args.globals.K_cache, {0, next_page_id, within_page_idx, 0}, args.inputs_arrived);
+            //     tma::load_async<dim::ROW, cache_policy::EVICT_FIRST>(args.input.vcache, args.globals.V_cache, {0, next_page_id, within_page_idx, 0}, args.inputs_arrived);
+            //     if(laneid() == 0) arrive(args.inputs_arrived, 3);
+            // }
+            // warpgroup::sync(5);
 
             // uint32_t semaphore_bitfield = 0xFFFF0000; // ***_finished phase bits start as 1s, ***_arrived phase bits start as 0s
 
@@ -113,18 +134,112 @@ template<typename config=config> struct rope_gqa_partial_op {
             // warp::sync();
         }
     };
-    struct launcher { // launches mma's
-        static __device__ void run(const globals &g, state<config> &s) {
+    struct launcher {
+        static __device__ void run(const globals &g, state<config> &s) { 
+            if (!laneid()) {
+                const char *msg = "launcher";
+                printf("Block %d, Warp ID %d, Lane ID %d, opcode: %d: %s\n", blockIdx.x, warpid(), laneid(), s.instruction()[0], msg);
+
+                col_vec<rt_fl<16, 32>> max_vec, norm_vec;
+                printf("%d %d\n", max_vec.outer_dim, max_vec.inner_dim);
+
+            }
+
         }
     };
     struct consumer {
         static __device__ void run(const globals &g, state<config> &s) {
+            if (warpid() == 0) {
+                // Wait for the inputs to be ready
+                wait(inputs_arrived(s), 0);
+    
+                // Move from smem to registers
+                int Q_page_pid = s.pid(0);
+                int K_page_pid = s.pid(1);
+                int V_page_pid = s.pid(2);
+                int O_page_pid = s.pid(3);
+                s.wait_page_ready(O_page_pid);
+                globals::q_st  &Q_smem = *reinterpret_cast<globals::q_st*>(s.pages[Q_page_pid].data);
+                globals::kv_st &K_smem = *reinterpret_cast<globals::kv_st*>(s.pages[K_page_pid].data);
+                globals::kv_st &V_smem = *reinterpret_cast<globals::kv_st*>(s.pages[V_page_pid].data);
+                globals::o_st  &O_smem = *reinterpret_cast<globals::o_st*>(s.pages[O_page_pid].data);
+                globals::q_rt Q_reg;
+                globals::kv_rt K_reg, V_reg;
+                globals::o_rt att_block;
+                warp::zero(att_block);
+
+                warp::load(Q_reg, Q_smem);
+                warp::load(K_reg, K_smem);
+                warp::load(V_reg, V_smem);
+
+                warp::mma_ABt(att_block, Q_reg, K_reg, att_block); // Q@K.T
+                warp::store(O_smem, att_block);
+                warp::sync();
+                warp::arrive(outputs_arrived(s));
+
+                warp::arrive(s.page_finished[Q_page_pid], config::NUM_CONSUMER_WARPS);
+                warp::arrive(s.page_finished[K_page_pid], config::NUM_CONSUMER_WARPS);
+                warp::arrive(s.page_finished[V_page_pid], config::NUM_CONSUMER_WARPS);
+
+                // if (laneid() == 0) {
+                //     printf("K batch: %d\n", g.K_c.batch());
+                //     printf("K head: %d\n", g.K_c.depth());
+                //     printf("K row: %d\n", g.K_c.rows());
+                //     printf("K col: %d\n", g.K_c.cols());
+
+                //     float Q_smem_sum = 0;
+                //     float Q_g_sum = 0;
+                //     float Q_reg_sum = 0;
+                //     for (int i = 0; i < 64 * 32; i++) {
+                //         Q_g_sum += (float)g.Q.raw_ptr[i];
+                //         Q_smem_sum += (float)Q_smem.data[i];
+                //     }
+                //     for (int i = 0; i < Q_reg.height; ++i) {
+                //         for (int j = 0; j < Q_reg.width; ++j) {
+                //             for (int k = 0; k < 128; ++k) {
+                //                 Q_reg_sum += (float)*reinterpret_cast<bf16*>(&Q_reg.tiles[i][j].data[k]);
+                //                 Q_reg_sum += (float)*(reinterpret_cast<bf16*>(&Q_reg.tiles[i][j].data[k]) + 1);
+                //             }
+                //         }
+                //     }
+                //     printf("Q_g_sum: %f\n", (float)(Q_g_sum));
+                //     printf("Q_smem_sum: %f\n", (float)(Q_smem_sum));
+                //     printf("Q_reg_sum: %f\n", (float)(Q_reg_sum));
+                    
+                //     float K_smem_sum = 0;
+                //     float K_g_sum = 0;
+                //     for (int i = 0; i < 64 * 32; i++) {
+                //         K_g_sum += (float)g.K_c.raw_ptr[i];
+                //         K_smem_sum += (float)K_smem.data[i];
+                //     }
+                //     printf("K_g_sum: %f\n", (float)(K_g_sum));
+                //     printf("K_smem_sum: %f\n", (float)(K_smem_sum));
+    
+                //     float V_smem_sum = 0;
+                //     float V_g_sum = 0;
+                //     for (int i = 0; i < 64 * 32; i++) {
+                //         V_g_sum += (float)g.V_c.raw_ptr[i];
+                //         V_smem_sum += (float)V_smem.data[i];
+                //     }
+                //     printf("V_g_sum: %f\n", (float)(V_g_sum));
+                //     printf("V_smem_sum: %f\n", (float)(V_smem_sum));
+                // }
+            }
         }
     };
     struct storer {
-        // Uses 4 full pages for outputs.
         static __device__ void run(const globals &g, state<config> &s) {
-        }
+            if (laneid() == 0) {
+                wait(outputs_arrived(s), 0);
+                int O_page_pid = s.pid(3);
+                globals::o_st &O_smem = *reinterpret_cast<globals::o_st*>(s.pages[O_page_pid].data);
+    
+                tma::store_async(g.O, O_smem, {0, 0, 0, 0});
+                tma::store_async_read_wait();
+                arrive(s.page_finished[O_page_pid], config::NUM_CONSUMER_WARPS);
+            }
+            warp::sync();
+         }
     };
 };
 
--- tests/vm/rope_gqa_partial/test.py
+++ tests/vm/rope_gqa_partial/test.py
@@ -6,9 +6,9 @@ from rope_gqa_partial import rope_gqa_partial
 
 NUM_BLOCKS = 148
 INSTRUCTION_WIDTH = 32
-ROPE_GQA_PARTIAL_OPCODE = 2
+ROPE_GQA_PARTIAL_OPCODE = 1
 
-def generate_inputs(N: int, N_new: int, H_q: int, H_kv: int, D_h: int):
+def generate_inputs(B: int, N: int, N_new: int, H_q: int, H_kv: int, D_h: int):
 
     '''Generate inputs for the GQA kernel.
     Args:
@@ -22,10 +22,10 @@ def generate_inputs(N: int, N_new: int, H_q: int, H_kv: int, D_h: int):
     torch.manual_seed(42)
 
     # Normal random does not support fp8 yet
-    Q   = torch.randn(B, N_new, H_q, D_h, dtype=torch.bfloat16, device='cuda')
-    K_c = torch.randn(B, N, H_kv, D_h, dtype=torch.bfloat16, device='cuda')
-    V_c = torch.randn(B, N, H_kv, D_h, dtype=torch.bfloat16, device='cuda')
-    O   = torch.randn(B, N_new, H_q, D_h, dtype=torch.bfloat16, device='cuda')
+    Q   = torch.randn(B, N_new, H_q, D_h, dtype=torch.bfloat16, device='cuda:0')
+    K_c = torch.randn(B, N_new, H_kv, D_h, dtype=torch.bfloat16, device='cuda:0')
+    V_c = torch.randn(B, N_new, H_kv, D_h, dtype=torch.bfloat16, device='cuda:0')
+    O   = torch.zeros(B, N_new, H_q, 32, dtype=torch.bfloat16, device='cuda:0')
 
     return Q, K_c, V_c, O
 
@@ -38,11 +38,17 @@ def generate_instructions_and_timings():
     instructions[0].append([ROPE_GQA_PARTIAL_OPCODE] + [0] * (INSTRUCTION_WIDTH - 1))
     instruction_idx += 1
 
-    while instruction_idx % NUM_BLOCKS != 0:
-        instructions[instruction_idx % NUM_BLOCKS].append([0] * INSTRUCTION_WIDTH)
+    # all blocks must have same number of instructions
+    max_instructions = -1
+    for i in range(NUM_BLOCKS):
+        max_instructions = max(max_instructions, len(instructions[i]))
+    for i in range(NUM_BLOCKS):
+        while len(instructions[i]) < max_instructions:
+            instructions[i].append([0] * INSTRUCTION_WIDTH)
         instruction_idx += 1
 
     # (BlockIdx, InstructionIdx, Instruction)
+    # If opcode (instructions[:, :, 0]) is invalid, the instruction is ignored
     instructions = torch.tensor(instructions, dtype=torch.int32).to(device=0)
     timings = torch.zeros((NUM_BLOCKS, instruction_idx // NUM_BLOCKS, 128), dtype=torch.int32).to(device=0)
 
@@ -56,11 +62,40 @@ H_q = 32
 H_kv = 8
 D_h = 64
 
+N = 1 # for testing for now
+H_q = 32
+H_kv = 32
+D_h = 64
+
 print("Generating inputs and instructions...")
-Q, K_c, V_c, O = generate_inputs(B, N, H_q, H_kv, D_h)
+Q, K_c, V_c, O = generate_inputs(B, N, N, H_q, H_kv, D_h)
 instructions, timings = generate_instructions_and_timings()
 
+
+print(torch.isnan(Q).any())
+print(torch.isnan(K_c).any())
+print(torch.isnan(V_c).any())
+print(torch.isnan(O).any())
+print(torch.sum(Q))
+print(torch.sum(K_c))
+print(torch.sum(V_c))
+print(torch.sum(O))
+
+
+
 # Run the matmul kernel)
+print('Q shape:', Q.shape)
+print('K_c shape:', K_c.shape)
+print('V_c shape:', V_c.shape)
+print('O shape:', O.shape)
+print('Instruction shape:', instructions.shape)
+print('Timings shape:', timings.shape) 
 print("Running the kernel...")
 rope_gqa_partial(instructions, timings, Q, K_c, V_c, O)
-torch.cuda.synchronize()
+torch.cuda.synchronize(0)
+
+print(O)
+print(torch.sum(O))
+O_ref = torch.matmul(Q, K_c.transpose(-1, -2))
+print(O_ref)
+print(torch.sum(O_ref))
