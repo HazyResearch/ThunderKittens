c4fe089c9df70541d4dea63c7614eebe82814a7b
Jordan Benjamin <jordan@mindcharger.com>
2025-04-23 09:04:57 +0000

    inter-instruction semaphores in attention reduction

--- tests/vm/llama_official/attention_reduction.cu
+++ tests/vm/llama_official/attention_reduction.cu
@@ -5,32 +5,31 @@ using namespace kittens;
 using namespace kittens::prototype;
 using namespace kittens::prototype::vm;
 
+using globals = llama_1b_globals;
+using config = default_config;
 
 using l_partial_sv = sv_fl<16>;
 using o_sv = sv_fl<globals::head_dim>;
 using o_rv = rv_fl<globals::head_dim>;
 using o_final_sv = sv_bf<globals::head_dim>;
 
-
 template <typename config = config, typename globals = globals>
-struct attention_reduction {
+struct attention_reduction
+{
     static constexpr int opcode = OPCODE_AttentionReduction;
     static constexpr int opcode_partial = OPCODE_PartialAttention;
     static constexpr int NUM_STAGES = 3;
 
-    // --- Instruction Parsing ---
-    // Instruction format:
-    // [0] = opcode 
-    // [1] = layer_idx
-    // [2] = num_partials
-    struct parsed_instruction {
+    struct parsed_instruction
+    {
         int layer_idx;
-        int num_partials;
         int q_head_idx;
-        __device__ inline parsed_instruction(state<config> &s) {
+        int num_partials;
+        __device__ inline parsed_instruction(state<config> &s)
+        {
             layer_idx = s.instruction()[1];
-            num_partials = s.instruction()[2];
-            q_head_idx = blockIdx.x;
+            q_head_idx = s.instruction()[2];
+            num_partials = s.instruction()[3];
         }
     };
 
@@ -44,12 +43,15 @@ struct attention_reduction {
     // --- Shared Memory Page Management Helpers ---
     static constexpr int SHARED_DATA_PAGE = 0; // Use only the first logical page
 
-    __device__ static inline void wait_shared_page(state<config> &s) {
+    __device__ static inline void wait_shared_page(state<config> &s)
+    {
         s.wait_page_ready(s.pid(SHARED_DATA_PAGE));
     }
-    __device__ static inline void finish_shared_page(state<config> &s) {
-        if (warp::laneid() == 0) {
-             arrive(s.page_finished[s.pid(SHARED_DATA_PAGE)], config::NUM_CONSUMER_WARPS);
+    __device__ static inline void finish_shared_page(state<config> &s)
+    {
+        if (warp::laneid() == 0)
+        {
+            arrive(s.page_finished[s.pid(SHARED_DATA_PAGE)], config::NUM_CONSUMER_WARPS);
         }
     }
 
@@ -60,30 +62,37 @@ struct attention_reduction {
     static_assert(partials_region_size + sizeof(o_final_sv) <= config::PAGE_SIZE,
                   "Required shared memory exceeds configured page size.");
 
-    __device__ static inline l_partial_sv &get_L_partial_smem(state<config> &s, int stage) {
+    __device__ static inline l_partial_sv &get_L_partial_smem(state<config> &s, int stage)
+    {
         int pid = s.pid(SHARED_DATA_PAGE);
         char *base_ptr = reinterpret_cast<char *>(s.pages[pid].data);
         size_t offset = stage * (sizeof(l_partial_sv) + sizeof(o_sv));
-        return *reinterpret_cast<l_partial_sv*>(base_ptr + offset);
+        return *reinterpret_cast<l_partial_sv *>(base_ptr + offset);
     }
-     __device__ static inline o_sv &get_O_partial_smem(state<config> &s, int stage) {
+    __device__ static inline o_sv &get_O_partial_smem(state<config> &s, int stage)
+    {
         int pid = s.pid(SHARED_DATA_PAGE);
         char *base_ptr = reinterpret_cast<char *>(s.pages[pid].data);
         size_t offset = stage * (sizeof(l_partial_sv) + sizeof(o_sv)) + sizeof(l_partial_sv);
-        return *reinterpret_cast<o_sv*>(base_ptr + offset);
+        return *reinterpret_cast<o_sv *>(base_ptr + offset);
     }
-    __device__ static inline o_final_sv &get_O_final_smem(state<config> &s) {
+    __device__ static inline o_final_sv &get_O_final_smem(state<config> &s)
+    {
         int pid = s.pid(SHARED_DATA_PAGE);
         char *base_ptr = reinterpret_cast<char *>(s.pages[pid].data);
-        return *reinterpret_cast<o_final_sv*>(base_ptr + final_o_offset);
+        return *reinterpret_cast<o_final_sv *>(base_ptr + final_o_offset);
     }
 
-    struct controller {
-        static __device__ int release_lid(const globals &g, typename config::instruction_t &instruction, int &query) {
+    struct controller
+    {
+        static __device__ int release_lid(const globals &g, typename config::instruction_t &instruction, int &query)
+        {
             return query;
         }
-        static __device__ int init_semaphores(const globals &g, state<config> &s) {
-            for (int i = 0; i < NUM_STAGES; i++) {
+        static __device__ int init_semaphores(const globals &g, state<config> &s)
+        {
+            for (int i = 0; i < NUM_STAGES; i++)
+            {
                 init_semaphore(L_partial_arrived(s, i), 0, 1);
                 init_semaphore(O_partial_arrived(s, i), 0, 1);
                 init_semaphore(L_partial_finished(s, i), 0, 1);
@@ -94,31 +103,39 @@ struct attention_reduction {
         }
     };
 
-    struct loader {
-        static __device__ void run(const globals &g, state<config> &s) {
+    struct loader
+    {
+        static __device__ void run(const globals &g, state<config> &s)
+        {
             parsed_instruction inst{s};
             int laneid = warp::laneid();
-            if (laneid >= 1 && laneid < config::NUM_PAGES) arrive(s.page_finished[s.pid(laneid)], config::NUM_CONSUMER_WARPS); 
-            if (laneid == 0) {
-                while (*(volatile int *)&g.Bar[{inst.layer_idx, opcode_partial, (inst.q_head_idx / 4) * 4}] < inst.num_partials) {
+            if (laneid >= 1 && laneid < config::NUM_PAGES)
+                arrive(s.page_finished[s.pid(laneid)], config::NUM_CONSUMER_WARPS);
+            if (laneid == 0)
+            {
+                while (*(volatile int *)&g.Bar[{inst.layer_idx, opcode_partial - 1, inst.q_head_idx}] < inst.num_partials)
+                {
                     __nanosleep(20);
                 }
 
                 wait_shared_page(s);
 
-                for (int i = 0; i < inst.num_partials; ++i) {
+                for (int i = 0; i < inst.num_partials; ++i)
+                {
                     int stage = i % NUM_STAGES;
                     l_partial_sv &L_smem = get_L_partial_smem(s, stage);
                     o_sv &O_smem = get_O_partial_smem(s, stage);
 
-                    if (i >= NUM_STAGES) {
+                    if (i >= NUM_STAGES)
+                    {
                         int prev_phase = (i / NUM_STAGES - 1) % 2;
                         wait(L_partial_finished(s, stage), prev_phase);
                         wait(O_partial_finished(s, stage), prev_phase);
                     }
 
                     L_smem.data[0] = __ldg(&g.attn_lse_intermediates.raw_ptr[(inst.q_head_idx * g.attn_lse_intermediates.cols()) + i]);
-                    if (warp::laneid()==0) arrive(L_partial_arrived(s, stage));
+                    if (warp::laneid() == 0)
+                        arrive(L_partial_arrived(s, stage));
 
                     tma::expect(O_partial_arrived(s, stage), O_smem);
                     tma::load_async<cache_policy::EVICT_FIRST>(O_smem, g.attn_out_intermediates, {0, inst.q_head_idx, i, 0}, O_partial_arrived(s, stage));
@@ -128,15 +145,26 @@ struct attention_reduction {
         }
     };
 
-    struct launcher {
-        static __device__ void run(const globals &g, state<config> &s) { }
+    struct launcher
+    {
+        static __device__ void run(const globals &g, state<config> &s)
+        {
+            // launcher does nothing here, since this doesn't use tensor cores.
+            {
+                s.wait_tensor_ready();
+                kittens::warp::arrive(s.tensor_finished, config::NUM_CONSUMER_WARPS);
+            }
+        }
     };
 
-    struct consumer {
-        static __device__ void run(const globals &g, state<config> &s) {
+    struct consumer
+    {
+        static __device__ void run(const globals &g, state<config> &s)
+        {
             parsed_instruction inst{s};
 
-            if (warpid() == 0) {
+            if (warpid() == 0)
+            {
                 o_rv accumulated_out;
                 float accumulated_lse = -INFINITY;
 
@@ -146,7 +174,8 @@ struct attention_reduction {
                 warp::zero(accumulated_out);
 
                 // --- Reduction Pipeline ---
-                for (int i = 0; i < inst.num_partials; ++i) {
+                for (int i = 0; i < inst.num_partials; ++i)
+                {
                     int stage = i % NUM_STAGES;
                     warp::wait(L_partial_arrived(s, stage), (i / NUM_STAGES) % 2);
                     warp::wait(O_partial_arrived(s, stage), (i / NUM_STAGES) % 2);
@@ -193,20 +222,21 @@ struct attention_reduction {
     };
 
     // Storer Warp: Responsible for storing data from shared memory back to global memory.
-    struct storer {
-        static __device__ void run(const globals &g, state<config> &s) {
+    struct storer
+    {
+        static __device__ void run(const globals &g, state<config> &s)
+        {
             parsed_instruction inst{s};
-            if (warp::laneid() == 0) {
+            if (warp::laneid() == 0)
+            {
                 o_final_sv &O_final_smem = get_O_final_smem(s);
                 wait(final_O_ready(s), 0);
                 tma::store_async<cache_policy::NORMAL>(g.attn_out, O_final_smem, {0, inst.q_head_idx, 0, 0});
                 tma::store_async_read_wait();
                 finish_shared_page(s);
-                atomicAdd(&g.Bar[{inst.layer_idx, opcode, inst.q_head_idx}], 1);
+                atomicAdd(&g.Bar[{inst.layer_idx, opcode - 1, inst.q_head_idx}], 1);
             }
             warp::sync();
-         }
+        }
     };
 };
-
-
