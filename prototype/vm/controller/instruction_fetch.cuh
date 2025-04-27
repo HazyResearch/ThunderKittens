3de76dc9c0e31827e19f6094e7768f41bfae2f4c
Jordan Benjamin <jordan@mindcharger.com>
2025-04-27 07:59:26 +0000

    big format

--- prototype/vm/controller/instruction_fetch.cuh
+++ prototype/vm/controller/instruction_fetch.cuh
@@ -4,63 +4,73 @@
 #include "../../common/common.cuh"
 #include "../util.cuh"
 
-namespace kittens {
-namespace prototype {
-namespace vm {
-namespace controller {
-template<typename config, typename globals>
-__device__ void inline load_instructions(int *instruction, int instruction_index, const globals &g, kittens::semaphore &bar) {
+namespace kittens
+{
+    namespace prototype
+    {
+        namespace vm
+        {
+            namespace controller
+            {
+                template <typename config, typename globals>
+                __device__ void inline load_instructions(int *instruction, int instruction_index, const globals &g, kittens::semaphore &bar)
+                {
 
-    auto src_ptr = &g.instructions[kittens::coord<>{(int)(blockIdx.x), instruction_index, 0}];
-    // static assert it's an int*
-    static_assert(std::is_same<decltype(src_ptr), int*>::value, "src_ptr is not an int*");
+                    auto src_ptr = &g.instructions[kittens::coord<>{(int)(blockIdx.x), instruction_index, 0}];
+                    // static assert it's an int*
+                    static_assert(std::is_same<decltype(src_ptr), int *>::value, "src_ptr is not an int*");
 
-    // Load instructions using a simple for loop from global memory
-    for (int i = 0; i < config::INSTRUCTION_WIDTH; i++) {
-        instruction[i] = src_ptr[i];
-    }
-    
-    arrive(bar, 1);
+                    // Load instructions using a simple for loop from global memory
+                    for (int i = 0; i < config::INSTRUCTION_WIDTH; i++)
+                    {
+                        instruction[i] = src_ptr[i];
+                    }
 
-    // constexpr int bytes = config::INSTRUCTION_WIDTH*sizeof(int);
-    // ::kittens::tma::expect_bytes(bar, bytes);
-    // uint32_t mbar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(&bar));
-    // uint32_t dst_ptr  = static_cast<uint32_t>(__cvta_generic_to_shared(instruction));
-    // uint64_t src_ptr  = (uint64_t)(&g.instructions[kittens::coord<>{(int)(blockIdx.x), instruction_index, 0}]);
-    // asm volatile (
-    //     "cp.async.bulk.shared::cluster.global.mbarrier::complete_tx::bytes [%0], [%1], %3, [%2];\n"
-    //     :
-    //     : "r"(dst_ptr), "l"(src_ptr), "r"(mbar_ptr), "n"(bytes)
-    //     : "memory"
-    // );
-}
+                    arrive(bar, 1);
 
-template<typename config, typename globals> __device__ void inline instruction_fetch_loop(const globals &g, ::kittens::prototype::vm::state<config> &kvms) {
-    static_assert(config::INSTRUCTION_PIPELINE_STAGES <= 16, "This would be an absurd thing to do.");
-    int num_iters = g.instructions.rows();
-    for(kvms.instruction_index = 0, kvms.instruction_ring = 0; kvms.instruction_index < num_iters; kvms.instruction_index++, kvms.instruction_ring = ring_advance<config::INSTRUCTION_PIPELINE_STAGES>(kvms.instruction_ring)) {
-        int phasebit = (kvms.instruction_index / config::INSTRUCTION_PIPELINE_STAGES - 1) & 1;
-        if (kvms.instruction_index >= config::INSTRUCTION_PIPELINE_STAGES) wait(kvms.instruction_finished[kvms.instruction_ring], phasebit);
-        load_instructions<config, globals>(&kvms.instruction()[0], kvms.instruction_index, g, kvms.instruction_arrived[kvms.instruction_ring]);
+                    // constexpr int bytes = config::INSTRUCTION_WIDTH*sizeof(int);
+                    // ::kittens::tma::expect_bytes(bar, bytes);
+                    // uint32_t mbar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(&bar));
+                    // uint32_t dst_ptr  = static_cast<uint32_t>(__cvta_generic_to_shared(instruction));
+                    // uint64_t src_ptr  = (uint64_t)(&g.instructions[kittens::coord<>{(int)(blockIdx.x), instruction_index, 0}]);
+                    // asm volatile (
+                    //     "cp.async.bulk.shared::cluster.global.mbarrier::complete_tx::bytes [%0], [%1], %3, [%2];\n"
+                    //     :
+                    //     : "r"(dst_ptr), "l"(src_ptr), "r"(mbar_ptr), "n"(bytes)
+                    //     : "memory"
+                    // );
+                }
 
-        // TODO: fix after fixing timings arrival
-        // if(kvms.instruction_index < config::INSTRUCTION_PIPELINE_STAGES) {
-        //     /* This requires some explanation.
-        //      * 
-        //      * For an instruction stage to be ready to use, two things must happen:
-        //      * 1. The instruction must be loaded.
-        //      * 2. Any previous timing writeout must be finished.
-        //      * 
-        //      * Correspondingly, the instruction_arrived semaphore takes two separate arrivals.
-        //      * One is triggered by this thread, and the other is triggered by the timing store thread.
-        //      * However, for the first N stages, there is no timing store, so we need to trigger the arrival manually.
-        //      */
-        //     arrive(kvms.instruction_arrived[kvms.instruction_ring], 1);
-        // }
-    }
-}
+                template <typename config, typename globals>
+                __device__ void inline instruction_fetch_loop(const globals &g, ::kittens::prototype::vm::state<config> &kvms)
+                {
+                    static_assert(config::INSTRUCTION_PIPELINE_STAGES <= 16, "This would be an absurd thing to do.");
+                    int num_iters = g.instructions.rows();
+                    for (kvms.instruction_index = 0, kvms.instruction_ring = 0; kvms.instruction_index < num_iters; kvms.instruction_index++, kvms.instruction_ring = ring_advance<config::INSTRUCTION_PIPELINE_STAGES>(kvms.instruction_ring))
+                    {
+                        int phasebit = (kvms.instruction_index / config::INSTRUCTION_PIPELINE_STAGES - 1) & 1;
+                        if (kvms.instruction_index >= config::INSTRUCTION_PIPELINE_STAGES)
+                            wait(kvms.instruction_finished[kvms.instruction_ring], phasebit);
+                        load_instructions<config, globals>(&kvms.instruction()[0], kvms.instruction_index, g, kvms.instruction_arrived[kvms.instruction_ring]);
 
-} // namespace controller
-} // namespace vm
-} // namespace prototype
+                        // TODO: fix after fixing timings arrival
+                        // if(kvms.instruction_index < config::INSTRUCTION_PIPELINE_STAGES) {
+                        //     /* This requires some explanation.
+                        //      *
+                        //      * For an instruction stage to be ready to use, two things must happen:
+                        //      * 1. The instruction must be loaded.
+                        //      * 2. Any previous timing writeout must be finished.
+                        //      *
+                        //      * Correspondingly, the instruction_arrived semaphore takes two separate arrivals.
+                        //      * One is triggered by this thread, and the other is triggered by the timing store thread.
+                        //      * However, for the first N stages, there is no timing store, so we need to trigger the arrival manually.
+                        //      */
+                        //     arrive(kvms.instruction_arrived[kvms.instruction_ring], 1);
+                        // }
+                    }
+                }
+
+            } // namespace controller
+        } // namespace vm
+    } // namespace prototype
 } // namespace kittens
--- prototype/vm/vm.cuh
+++ prototype/vm/vm.cuh
@@ -12,132 +12,159 @@
 #include "consumer.cuh"
 #include "noop.cuh"
 
-namespace kittens {
-namespace prototype {
-namespace vm {
+namespace kittens
+{
+    namespace prototype
+    {
+        namespace vm
+        {
 
-template<typename config, typename globals, typename... ops>
-__device__ inline void kvm_internal(const globals &g) {
-    uint64_t start_time = (uint64_t)clock64();
+            template <typename config, typename globals, typename... ops>
+            __device__ inline void kvm_internal(const globals &g)
+            {
+                uint64_t start_time = (uint64_t)clock64();
 #ifdef KVM_DEBUG
-    if(threadIdx.x == 0) printf("Thread %d: Kernel launched\n", threadIdx.x); group<config::NUM_WARPS>::sync(15);
+                if (threadIdx.x == 0)
+                    printf("Thread %d: Kernel launched\n", threadIdx.x);
+                group<config::NUM_WARPS>::sync(15);
 #endif
-    __shared__ alignas(128) instruction_state_t<config> instruction_state[config::INSTRUCTION_PIPELINE_STAGES];
-    __shared__ kittens::semaphore page_finished[config::NUM_PAGES],
-                                  instruction_arrived[config::INSTRUCTION_PIPELINE_STAGES],
-                                  instruction_finished[config::INSTRUCTION_PIPELINE_STAGES],
-                                  tensor_finished,
-                                  semaphores_ready;
-    extern __shared__ int __shm[];
-    void *aligned_shm_addr = (void*)((1023 + (uint64_t)&__shm[0]) & ~(uint64_t)1023);
-    typename state<config>::page_array_t &pages = *reinterpret_cast<typename state<config>::page_array_t*>(aligned_shm_addr);
-    typename state<config>::tensor_allocator_t tensor_alloc{};
-
-#ifdef KVM_DEBUG
-    if(threadIdx.x == 0) printf("Thread %d: Pre-KVMS creation\n", threadIdx.x); group<config::NUM_WARPS>::sync(15);
-#endif
-    state<config> kvms {
-        instruction_state,
-        instruction_arrived, instruction_finished,
-        0, 0,
-        { /* ... */ },
-        pages,
-        page_finished,
-        tensor_finished,
-        semaphores_ready,
-        start_time,
-        tensor_alloc
-    }; // kittens virtual machine state
+                __shared__ alignas(128) instruction_state_t<config> instruction_state[config::INSTRUCTION_PIPELINE_STAGES];
+                __shared__ kittens::semaphore page_finished[config::NUM_PAGES],
+                    instruction_arrived[config::INSTRUCTION_PIPELINE_STAGES],
+                    instruction_finished[config::INSTRUCTION_PIPELINE_STAGES],
+                    tensor_finished,
+                    semaphores_ready;
+                extern __shared__ int __shm[];
+                void *aligned_shm_addr = (void *)((1023 + (uint64_t)&__shm[0]) & ~(uint64_t)1023);
+                typename state<config>::page_array_t &pages = *reinterpret_cast<typename state<config>::page_array_t *>(aligned_shm_addr);
+                typename state<config>::tensor_allocator_t tensor_alloc{};
 
 #ifdef KVM_DEBUG
-    if(threadIdx.x == 0) printf("Thread %d: Created KVMS\n", threadIdx.x); group<config::NUM_WARPS>::sync(15);
+                if (threadIdx.x == 0)
+                    printf("Thread %d: Pre-KVMS creation\n", threadIdx.x);
+                group<config::NUM_WARPS>::sync(15);
 #endif
-
-    // Zero initial timings memory.
-    if(threadIdx.x < config::TIMING_WIDTH) {
-        #pragma unroll
-        for(int i = 0; i < config::INSTRUCTION_PIPELINE_STAGES; i++) {
-            instruction_state[i].timings[threadIdx.x] = 0;
-        }
-    }
-    
-    if(threadIdx.x < config::INSTRUCTION_PIPELINE_STAGES) {
-        init_semaphore(instruction_arrived[threadIdx.x], 2); // One arrival for instruction arriving, one for timing writeout finishing.
-        init_semaphore(instruction_finished[threadIdx.x], config::NUM_WARPS); // All warps but the controller warp arrive here, and the semaphore initializer thread also arrives here.
-    }
-    if(threadIdx.x < config::NUM_PAGES) {
-        init_semaphore(page_finished[threadIdx.x], config::NUM_CONSUMER_WARPS);
-        arrive(page_finished[threadIdx.x], config::NUM_CONSUMER_WARPS); // Flip to state 0, to mark that it starts as available.
-    }
-    if(threadIdx.x == 0) {
-        init_semaphore(tensor_finished, config::NUM_CONSUMER_WARPS);
-        arrive(tensor_finished, config::NUM_CONSUMER_WARPS); // Flip to state 0, to mark that it starts as available.
-        init_semaphore(semaphores_ready, 1);
-    }
-
-    asm volatile("fence.proxy.async.shared::cta;\n" ::: "memory");
-    __syncthreads();
-
-    if(config::CLUSTER_BLOCKS == 1) everyone::sync(15); // all warps must arrive here, confirming semaphore initialization is visible to all threads.
-    else everyone::tma::cluster::sync();
+                state<config> kvms{
+                    instruction_state,
+                    instruction_arrived,
+                    instruction_finished,
+                    0,
+                    0,
+                    {/* ... */},
+                    pages,
+                    page_finished,
+                    tensor_finished,
+                    semaphores_ready,
+                    start_time,
+                    tensor_alloc}; // kittens virtual machine state
 
 #ifdef KVM_DEBUG
-    if(blockIdx.x == 0 && threadIdx.x == 0) kvms.print();
+                if (threadIdx.x == 0)
+                    printf("Thread %d: Created KVMS\n", threadIdx.x);
+                group<config::NUM_WARPS>::sync(15);
 #endif
 
-    if(warpid() < config::NUM_CONSUMER_WARPS) {
-        warpgroup::increase_registers<104>();
-        ::kittens::prototype::vm::consumer::main_loop<config, globals, ops...>(g, kvms);
-    }
-    else {
-        warpgroup::decrease_registers<64>();
-        switch(warpgroup::warpid()) {
-            case 0:
-                ::kittens::prototype::vm::loader::main_loop<config, globals, ops...>(g, kvms);
-                break;
-            case 1:
-                ::kittens::prototype::vm::storer::main_loop<config, globals, ops...>(g, kvms);
-                break;
-            case 2:
-                ::kittens::prototype::vm::launcher::main_loop<config, globals, ops...>(g, kvms);
-                break;
-            case 3:
-                ::kittens::prototype::vm::controller::main_loop<config, globals, ops...>(g, kvms);
-                break;
-            default:
-                asm volatile("trap;");
-        }
-    }
+                // Zero initial timings memory.
+                if (threadIdx.x < config::TIMING_WIDTH)
+                {
+#pragma unroll
+                    for (int i = 0; i < config::INSTRUCTION_PIPELINE_STAGES; i++)
+                    {
+                        instruction_state[i].timings[threadIdx.x] = 0;
+                    }
+                }
+
+                if (threadIdx.x < config::INSTRUCTION_PIPELINE_STAGES)
+                {
+                    init_semaphore(instruction_arrived[threadIdx.x], 2);                  // One arrival for instruction arriving, one for timing writeout finishing.
+                    init_semaphore(instruction_finished[threadIdx.x], config::NUM_WARPS); // All warps but the controller warp arrive here, and the semaphore initializer thread also arrives here.
+                }
+                if (threadIdx.x < config::NUM_PAGES)
+                {
+                    init_semaphore(page_finished[threadIdx.x], config::NUM_CONSUMER_WARPS);
+                    arrive(page_finished[threadIdx.x], config::NUM_CONSUMER_WARPS); // Flip to state 0, to mark that it starts as available.
+                }
+                if (threadIdx.x == 0)
+                {
+                    init_semaphore(tensor_finished, config::NUM_CONSUMER_WARPS);
+                    arrive(tensor_finished, config::NUM_CONSUMER_WARPS); // Flip to state 0, to mark that it starts as available.
+                    init_semaphore(semaphores_ready, 1);
+                }
+
+                asm volatile("fence.proxy.async.shared::cta;\n" ::: "memory");
+                __syncthreads();
+
+                if (config::CLUSTER_BLOCKS == 1)
+                    everyone::sync(15); // all warps must arrive here, confirming semaphore initialization is visible to all threads.
+                else
+                    everyone::tma::cluster::sync();
 
 #ifdef KVM_DEBUG
-    printf("Thread %d arriving at final barrier\n", threadIdx.x);
+                if (blockIdx.x == 0 && threadIdx.x == 0)
+                    kvms.print();
 #endif
 
-    if(config::CLUSTER_BLOCKS > 1) everyone::tma::cluster::sync();
-    else everyone::sync(15);
+                if (warpid() < config::NUM_CONSUMER_WARPS)
+                {
+                    warpgroup::increase_registers<104>();
+                    ::kittens::prototype::vm::consumer::main_loop<config, globals, ops...>(g, kvms);
+                }
+                else
+                {
+                    warpgroup::decrease_registers<64>();
+                    switch (warpgroup::warpid())
+                    {
+                    case 0:
+                        ::kittens::prototype::vm::loader::main_loop<config, globals, ops...>(g, kvms);
+                        break;
+                    case 1:
+                        ::kittens::prototype::vm::storer::main_loop<config, globals, ops...>(g, kvms);
+                        break;
+                    case 2:
+                        ::kittens::prototype::vm::launcher::main_loop<config, globals, ops...>(g, kvms);
+                        break;
+                    case 3:
+                        ::kittens::prototype::vm::controller::main_loop<config, globals, ops...>(g, kvms);
+                        break;
+                    default:
+                        asm volatile("trap;");
+                    }
+                }
 
 #ifdef KVM_DEBUG
-    uint64_t end_time = (uint64_t)clock64();
-    if(threadIdx.x == 0) printf("Overall VM execution time: %lu\n", end_time - start_time);
+                printf("Thread %d arriving at final barrier\n", threadIdx.x);
 #endif
-}
 
-// Forward a NoOp to the VM, to ensure that the VM can support zeros.
-template<typename config, typename globals, typename... ops>
-struct kittens_virtual_machine {
-    __device__ inline static void run(const globals &g) {
-        kvm_internal<config, globals, NoOp<config>, ops...>(g);
-    }
-};
+                if (config::CLUSTER_BLOCKS > 1)
+                    everyone::tma::cluster::sync();
+                else
+                    everyone::sync(15);
 
+#ifdef KVM_DEBUG
+                uint64_t end_time = (uint64_t)clock64();
+                if (threadIdx.x == 0)
+                    printf("Overall VM execution time: %lu\n", end_time - start_time);
+#endif
+            }
 
-template<typename config, typename globals, typename... ops>
-__launch_bounds__(config::NUM_THREADS, 1)
-__cluster_dims__(config::CLUSTER_BLOCKS)
-__global__ void kvm(const __grid_constant__ globals g) {
-    kittens_virtual_machine<config, globals, ops...>::run(g);
-}
+            // Forward a NoOp to the VM, to ensure that the VM can support zeros.
+            template <typename config, typename globals, typename... ops>
+            struct kittens_virtual_machine
+            {
+                __device__ inline static void run(const globals &g)
+                {
+                    kvm_internal<config, globals, NoOp<config>, ops...>(g);
+                }
+            };
 
-} // namespace vm
-} // namespace prototype
+            template <typename config, typename globals, typename... ops>
+            __launch_bounds__(config::NUM_THREADS, 1)
+                __cluster_dims__(config::CLUSTER_BLOCKS)
+                    __global__ void kvm(const __grid_constant__ globals g)
+            {
+                kittens_virtual_machine<config, globals, ops...>::run(g);
+            }
+
+        } // namespace vm
+    } // namespace prototype
 } // namespace kittens
