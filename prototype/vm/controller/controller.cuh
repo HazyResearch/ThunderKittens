af75c30ac726481745992d7a5d1c4115cad5743b
Jordan Benjamin <jordan@mindcharger.com>
2025-04-28 03:27:43 +0000

    re-add timings

--- prototype/vm/controller/controller.cuh
+++ prototype/vm/controller/controller.cuh
@@ -26,18 +26,24 @@ namespace kittens
 
                     int num_iters = g.instructions.rows();
                     int num_semaphores[2];
+
                     for (kvms.instruction_index = 0, kvms.instruction_ring = 0;
                          kvms.instruction_index < num_iters;
                          kvms.instruction_index++, kvms.instruction_ring = ring_advance<config::INSTRUCTION_PIPELINE_STAGES>(kvms.instruction_ring))
                     {
 
-                        // Step 0. Wait for the previous instruction to complete & invalidate its semaphores
+                        // Step 0. if the slot was used in the previous iteration, wait for the previous instruction to complete & invalidate its semaphores
                         if (kvms.instruction_index >= config::INSTRUCTION_PIPELINE_STAGES)
                         {
                             int phasebit = (kvms.instruction_index / config::INSTRUCTION_PIPELINE_STAGES - 1) & 1;
                             wait(kvms.instruction_finished[kvms.instruction_ring], phasebit);
                             for (int i = 0; i < num_semaphores[kvms.instruction_ring]; i++)
                                 invalidate_semaphore(kvms.all_instructions[kvms.instruction_ring].semaphores[i]);
+
+                            store_timings<config, globals>(&kvms.timing()[0], kvms.instruction_index, g);
+                            kittens::tma::store_async_read_wait();
+                            uint32_t src_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(&kvms.timing()[0]));
+                            asm volatile("st.bulk.weak [%0], %1, 0;\n" ::"r"(src_ptr), "n"(config::TIMING_WIDTH * sizeof(int))); // Reinitialize timing memory as zeros.
                         }
 
                         // Step 1. Load instructions (no semaphores used)
@@ -45,7 +51,8 @@ namespace kittens
 
                         // Step 2. Establish physical page order
                         int last_instruction_ring = (kvms.instruction_ring + config::INSTRUCTION_PIPELINE_STAGES - 1) % config::INSTRUCTION_PIPELINE_STAGES;
-                        int last_opcode = kvms.all_instructions[last_instruction_ring].instructions[0];
+                        auto last_opcode = kvms.all_instructions[last_instruction_ring].instructions[0];
+
                         for (int i = 0; i < config::NUM_PAGES; i++)
                         {
                             if (kvms.instruction_index == 0)
@@ -69,6 +76,8 @@ namespace kittens
                             num_semaphores[kvms.instruction_ring] = dispatch_op<semaphore_constructor_op_dispatcher<config, globals>::dispatcher, ops...>::template run<int, config, globals, ::kittens::prototype::vm::state<config>>(
                                 opcode, g, kvms);
                         }
+
+                        last_opcode = opcode;
 
                         // Step 4. Let the rest of the world know that next instruction is ready to roll!
                         arrive(kvms.instruction_arrived[kvms.instruction_ring], 1);
