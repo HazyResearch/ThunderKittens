4e55717e7eb4a49d45cbabd37f203b2232d09006
shirley <shirley@together.ai>
2025-05-13 04:46:32 +0000

    store

--- kernels/gang_api_examples/reduce_add/test.cu
+++ kernels/gang_api_examples/reduce_add/test.cu
@@ -11,9 +11,11 @@ using namespace kittens;
 // Changed float to bf16 throughout the layouts
 using global_layout   =  gl<bf16, 1, 1, -1, -1>;    
 using st_tile = kittens::st<bf16, 16, 128>;
+using rt_tile = kittens::rt<bf16, 16, 128>;
+using sv_vec = kittens::sv<bf16, 128>;
 
 constexpr int LLAMA_8B_HEAD_DIM = 128;
-constexpr int GQA_RATIO = 8;
+constexpr int GQA_RATIO = 4;
 
 __device__ static inline void load_Q_async(st_tile &dst, global_layout src, int q_head_start_idx)
 {
@@ -42,14 +44,72 @@ __device__ static inline void load_Q_async(st_tile &dst, global_layout src, int 
     asm volatile("cp.async.commit_group;\n" ::: "memory");
 }
 
+template <ducks::sv::all SV, ducks::rt::all RT>
+__device__ static inline void store_4_rows(SV (&dst)[4], const RT &src)
+{
+    static_assert(RT::rows == 16, "src rows must be 16.");
+    static_assert(SV::length == src.cols, "dst length must match src cols.");
+
+    using T2 = RT::dtype;
+    using T = base_types::packing<T2>::unpacked_type;
+    using U = SV::dtype;
+    using U2 = base_types::packing<U>::packed_type;
+
+    uint32_t dst_ptr[4];
+    #pragma unroll
+    for (int i = 0; i < 4; ++i)
+    {
+        dst_ptr[i] = static_cast<uint32_t>(__cvta_generic_to_shared(&dst[i].data[0]));
+    }
+
+    
+    int laneid = kittens::laneid();
+
+    
+    if (laneid < 16)
+    {
+        int local_row_idx = laneid / 4;
+        int local_col_idx = laneid % 4;
+
+        for (int j = 0; j < src.width; j++)
+        {
+            U2 tmp[2];
+
+            tmp[0] = base_types::convertor<U2, T2>::convert(src.tiles[0][j].data[0]);
+            tmp[1] = base_types::convertor<U2, T2>::convert(src.tiles[0][j].data[2]);
+
+            int col_idx = local_col_idx * 2 + j * 16;
+
+            move<U2>::sts(dst_ptr[local_row_idx] + sizeof(U) * col_idx, tmp[0]);
+            move<U2>::sts(dst_ptr[local_row_idx] + sizeof(U) * (col_idx + 8), tmp[1]);
+        }
+    }
+}
+
 __global__ void test_kernel(global_layout a, global_layout b) {
+    // extern __shared__ kittens::alignment_dummy __shm[];
+    // kittens::shared_allocator al((int*)&__shm[0]);
+    // st_tile (&s_tile) = al.allocate<st_tile>();
+    // load_Q_async(s_tile, a, 0);
+    // load_async_wait();
+
+    // kittens::store(b, s_tile, {});
+
+    // __syncwarp();
+
     extern __shared__ kittens::alignment_dummy __shm[];
     kittens::shared_allocator al((int*)&__shm[0]);
-    st_tile (&s_tile) = al.allocate<st_tile>();
-    load_Q_async(s_tile, a, 0);
-    load_async_wait();
+    sv_vec (&s_vec)[4] = al.allocate<sv_vec, 4>();
 
-    kittens::store(b, s_tile, {});
+    rt_tile r_tile;
+    kittens::load(r_tile, a, {0, 0});
+    store_4_rows(s_vec, r_tile);
+
+    kittens::store(b, s_vec[0], {0, 0});
+    kittens::store(b, s_vec[1], {1, 0});
+    kittens::store(b, s_vec[2], {2, 0});
+    kittens::store(b, s_vec[3], {3, 0});
+
     __syncwarp();
 }
 
