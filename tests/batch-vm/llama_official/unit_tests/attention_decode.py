d3fb440d2de4b6d757cb5729129ac8885f6d199d
Stuart Sul <ssstuartss@gmail.com>
2025-05-15 07:01:20 +0000

    correct unit test

--- tests/batch-vm/llama_official/unit_tests/attention_decode.py
+++ tests/batch-vm/llama_official/unit_tests/attention_decode.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from time import time
 
 import torch
 torch.set_printoptions(sci_mode=False)
@@ -45,7 +46,7 @@ LAYER_IDX = 0
 print('\nGenerating inputs...')
 device = torch.device('cuda:0')
 torch.manual_seed(42)
-Bar = torch.zeros((NUM_LAYERS, NUM_OPS, NUM_ATTENTION_HEADS + 2 * NUM_KV_HEADS), dtype=torch.uint32, device=device)
+Bar = torch.zeros((NUM_LAYERS, NUM_OPS, BATCH_SIZE, NUM_ATTENTION_HEADS + 2 * NUM_KV_HEADS), dtype=torch.uint32, device=device)
 instructions = None # set below
 timings = None # set below
 qkv_weights = torch.zeros(NUM_LAYERS, (NUM_ATTENTION_HEADS + 2 * NUM_KV_HEADS) * HEAD_DIM, HIDDEN_DIM, dtype=torch.bfloat16, device=device)
@@ -68,10 +69,12 @@ gate_silu_intermediates = torch.zeros(BATCH_SIZE, INTERMEDIATE_DIM, dtype=torch.
 q_post_rope = torch.randn(BATCH_SIZE, HIDDEN_DIM, dtype=torch.bfloat16, device=device)
 attn_out = torch.zeros(BATCH_SIZE, HIDDEN_DIM, dtype=torch.bfloat16, device=device)
 silu_out = torch.zeros(BATCH_SIZE, INTERMEDIATE_DIM, dtype=torch.bfloat16, device=device)
+rms_lm_head_intermediates = torch.zeros(BATCH_SIZE, HIDDEN_DIM, dtype=torch.bfloat16, device=device)
 logits = torch.zeros(BATCH_SIZE, VOCAB_SIZE, dtype=torch.bfloat16, device=device)
 pos_id = 15
 attn_scale = 1 / (HEAD_DIM ** 0.5)
 rms_norm_eps = 1e-5
+batch_size = BATCH_SIZE
 
 # Generate instructions
 instructions = [[] for _ in range(SM_COUNT)]
@@ -93,6 +96,10 @@ for i in range(SM_COUNT):
 
 # Convert to tensor
 instructions = torch.tensor(instructions, dtype=torch.int32, device=device)
+print(instructions.shape)
+
+# Set barrriers
+Bar[:, :, :, :] = 1
 
 # Generate timings
 timings = torch.zeros((SM_COUNT, instructions.shape[1], TIMING_WIDTH), dtype=torch.int32, device=device)
@@ -123,14 +130,15 @@ kvm_llama(
     hidden_states,
     rms_rope_intermediates,
     rms_gate_intermediates,
-    gate_silu_intermediates,
     q_post_rope,
     attn_out,
     silu_out,
+    rms_lm_head_intermediates,
     logits,
     pos_id,
     attn_scale,
     rms_norm_eps,
+    batch_size,
 )
 torch.cuda.synchronize()
 
@@ -146,3 +154,81 @@ attn_out_ref = gqa_decode(
 
 diff = (attn_out - attn_out_ref).abs()
 print(f'attn_out -- max abs diff: {diff.max()}, mean abs diff: {diff.mean()}')
+
+
+###
+#  Check speed
+###
+print('\nChecking speed...')
+for i in range(2):
+    kvm_llama(
+        Bar,
+        instructions,
+        timings,
+        qkv_weights,
+        attn_norm_weights,
+        o_weights,
+        mlp_norm_weights,
+        up_weights,
+        gate_weights,
+        down_weights,
+        lm_head_norm_weights,
+        lm_head_weights,
+        k_cache,
+        v_cache,
+        rope_cos,
+        rope_sin,
+        hidden_states,
+        rms_rope_intermediates,
+        rms_gate_intermediates,
+        q_post_rope,
+        attn_out,
+        silu_out,
+        rms_lm_head_intermediates,
+        logits,
+        pos_id,
+        attn_scale,
+        rms_norm_eps,
+        batch_size,
+    )
+    torch.cuda.synchronize()
+NUM_ITERS = 10
+times = []
+for i in range(NUM_ITERS):
+    torch.cuda.synchronize()
+    start_time = time()    
+    kvm_llama(
+        Bar,
+        instructions,
+        timings,
+        qkv_weights,
+        attn_norm_weights,
+        o_weights,
+        mlp_norm_weights,
+        up_weights,
+        gate_weights,
+        down_weights,
+        lm_head_norm_weights,
+        lm_head_weights,
+        k_cache,
+        v_cache,
+        rope_cos,
+        rope_sin,
+        hidden_states,
+        rms_rope_intermediates,
+        rms_gate_intermediates,
+        q_post_rope,
+        attn_out,
+        silu_out,
+        rms_lm_head_intermediates,
+        logits,
+        pos_id,
+        attn_scale,
+        rms_norm_eps,
+        batch_size,
+    )
+    torch.cuda.synchronize()
+    end_time = time()
+    times.append(end_time - start_time)
+avg_time = sum(times) / NUM_ITERS
+print(f'Average time per iter: {avg_time * 1e6} us')
