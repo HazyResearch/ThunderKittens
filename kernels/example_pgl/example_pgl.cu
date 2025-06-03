29c802d9e642e11b7314d67ce43627dde9bf2d6e
Stuart Sul <ssstuartss@gmail.com>
2025-06-02 23:17:04 -0700

    Update pgl example bind

--- kernels/example_pgl/example_pgl.cu
+++ kernels/example_pgl/example_pgl.cu
@@ -1,88 +1,37 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h> // for automatic Python list -> std::vector conversion
-
-#include <random>
 #include "kittens.cuh"
+#include "pyutils/pyutils.cuh"
 
 using namespace kittens;
 
-__global__ void test_kernel(int *in, int *out) {
-    // 32 threads, single block, array of size 32
+struct globals {
+    using matrix_layout = pgl<gl<float, -1, -1, -1, 1024 * 1024 * 2>, 4>;
+
+    matrix_layout in_mat;
+    matrix_layout out_mat;
+    int n;
+
+    dim3 grid() { return dim3(4); }
+    dim3 block() { return dim3(kittens::WARP_THREADS); }
+    int dynamic_shared_memory() { return kittens::MAX_SHARED_MEMORY; }
+};
+
+__global__ void example_pgl_kernel(const __grid_constant__ globals g) {
     int tid = threadIdx.x;
-    out[tid] = in[tid];
-}
+    int bid = blockIdx.x;
+    int index = tid + bid * blockDim.x;
 
-void test(pybind11::list ins, pybind11::list outs, std::shared_ptr<KittensClub> club) {
-
-    if (!pybind11::hasattr(ins[0], "__class__") && 
-            ins[0].attr("__class__").attr("__name__").cast<std::string>() == "Tensor")
-        throw std::runtime_error("Expected a list of torch.Tensor");
-    if (!ins[0].attr("is_contiguous")().cast<bool>())
-        throw std::runtime_error("Tensor must be contiguous");
-    if (ins[0].attr("device").attr("type").cast<std::string>() == "cpu")
-        throw std::runtime_error("Tensor must be on CUDA device");
-
-    int device_ids[4];
-    for (int i = 0; i < 4; i++) device_ids[i] = i;
-
-    // Assume all tensors are of shape (32,)
-    // Assume num_devices = 4
-    int *in_ds[4];
-    int *out_ds[4];
-
-    for (int i = 0; i < 4; i++) {
-        in_ds[i] = reinterpret_cast<int*>(ins[i].attr("data_ptr")().cast<uint64_t>());
-        out_ds[i] = reinterpret_cast<int*>(outs[i].attr("data_ptr")().cast<uint64_t>());
+    if (index < g.n) {
+        g.out_mat[0].raw_ptr[index] = g.in_mat[0].raw_ptr[index];
     }
-
-    pgl<gl<int, 1, 1, 1, 32>, 4> in_pgl{device_ids, in_ds, nullptr, nullptr, nullptr, nullptr};
-    pgl<gl<int, 1, 1, 1, 32>, 4> out_pgl{device_ids, out_ds, nullptr, nullptr, nullptr, nullptr};
-
-    club->execute([&](int worker_id) {
-        if (worker_id == 0) {
-            test_kernel<<<1, 32>>>(in_pgl.mc_vas[0], out_pgl.mc_vas[0]);
-            CUDACHECK(cudaDeviceSynchronize());
-        }
-    });
-}
-
-void run_club(std::shared_ptr<KittensClub> club) {
-    club->execute([](int worker_id) {
-        printf("Hello, from device %d!\n", worker_id);
-    });
 }
 
 PYBIND11_MODULE(example_pgl, m) {
     m.doc() = "example_pgl python module";
-
-    pybind11::class_<KittensClub, std::shared_ptr<KittensClub>>(m, "KittensClub")
-        .def(pybind11::init([](const std::vector<int>& device_ids) {
-            int device_count;
-            CUDACHECK(cudaGetDeviceCount(&device_count));
-            if (device_count < device_ids.size())
-                throw std::runtime_error("Not enough CUDA devices available");
-            auto club = std::make_shared<KittensClub>(device_ids.data(), device_ids.size());
-            club->execute([&](int worker_idx) {
-                // Warmup + enable peer access
-                for (int idx = 0; idx < device_ids.size(); idx++) {
-                    if (idx == worker_idx) continue;
-                    int can_access = 0;
-                    CUDACHECK(cudaDeviceCanAccessPeer(&can_access, device_ids[worker_idx], device_ids[idx]));
-                    if (!can_access)
-                        throw std::runtime_error("Device " + std::to_string(device_ids[worker_idx]) + " cannot access device " + std::to_string(device_ids[idx]));
-                    CUDACHECK(cudaDeviceEnablePeerAccess(device_ids[idx], 0));
-                }
-            }); 
-            return club;
-        }), pybind11::arg("device_ids"));
-
-    // pybind11::class_<pgl, std::shared_ptr<pgl>>(m, "pgl")
-    //     .def(pybind11::init([](const std::vector<int>& device_ids) {
-    //         auto pgl = std::make_shared<pgl>(device_ids.data(), device_ids.size());
-    //         pgl->execute([](int worker_id) {}); // warmup
-    //         return pgl;
-    //     }), pybind11::arg("device_ids"));
-
-    m.def("run_club", &run_club, "Run a club");
-    m.def("test", &test, "Test");
+    kittens::py::bind_multigpu_kernel<example_pgl_kernel>(
+        m,
+        "example_pgl",
+        &globals::in_mat,
+        &globals::out_mat,
+        &globals::n
+    );
 }
--- kernels/example_pgl/test.py
+++ kernels/example_pgl/test.py
@@ -3,16 +3,21 @@ import torch
 import example_pgl
 
 input_tensors = [
-    (torch.randn(32, dtype=torch.float32, device=i) * 100).to(torch.int32)
+    (torch.randn(1024 * 1024 * 2, dtype=torch.float32, device=i) * 100).to(torch.int32)
     for i in range(4)
 ]
 output_tensors = [
-    torch.zeros(32, dtype=torch.int32, device=i)
+    torch.zeros(1024 * 1024 * 2, dtype=torch.int32, device=i)
     for i in range(4)
 ]
-club = example_pgl.KittensClub([0, 1, 2, 3])
 
-example_pgl.test(input_tensors, output_tensors, club)
+device_ids = [0, 1, 2, 3]
+example_pgl.enable_all_p2p_access(device_ids)
+club = example_pgl.KittensClub(device_ids)
+kernel_globals = example_pgl.Globals(input_tensors, output_tensors, 1024 * 1024 * 2)
+
+example_pgl.example_pgl(kernel_globals, club)
+
 print(output_tensors[0] - input_tensors[0])
 print(output_tensors[1] - input_tensors[1])
 print(output_tensors[2] - input_tensors[2])
