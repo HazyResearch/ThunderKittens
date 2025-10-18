e1cedaad7bbed59fc9d220fe88fe900161c887da
Stuart Sul <ssstuartss@gmail.com>
2025-10-18 01:22:14 -0500

    Add inter-SM/inter-GPU synchronization primitives

--- include/ops/device/device.cuh
+++ include/ops/device/device.cuh
@@ -17,31 +17,27 @@ static constexpr int NUM_DEVICES = _NUM_DEVICES;
 
 #ifdef KITTENS_HOPPER
 
-using barrier_t = pgl<gl<int, 1, 1, 1, -1>, NUM_DEVICES, true>;
+using barrier_t = pgl<gl<int, -1, -1, -1, -1>, NUM_DEVICES, true>;
 
-/**
- * @brief Multi-GPU synchronization barrier for coordinated kernel exit
- * 
- * Performs a synchronization across all devices to ensure all GPUs complete 
- * their work before any kernel exits. Does not synchronize intra-node threads
- * or threadblocks.
- * 
- * @param barrier Pre-allocated barrier structure, must be initialized to 0
- * @param dev_idx Current device index (0 to NUM_DEVICES - 1)
- * @param id Synchronization point identifier (default: 0). 0 is fine for most cases
- * 
- */
-__device__ static inline void sync_on_exit(const barrier_t &barrier, const int dev_idx, const int id = 0) {
-    if (blockIdx.x == 0 && blockIdx.y == 0 && blockIdx.z == 0 &&
-        threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) {
-        cuda::atomic_ref<int, cuda::thread_scope_system> barrier_uc(barrier[dev_idx][{id}]);
+__device__ static inline void signal(const barrier_t &barrier, const coord<ducks::default_type> &idx, const int dst_dev_idx, const int val) {
+    asm volatile("{red.release.sys.global.add.s32 [%0], %1;}" :: "l"(&barrier[dst_dev_idx][idx]), "r"(val) : "memory");
+}
 
-        // Inter-note check-in
-        multimem<int>::red<reduce_op::ADD>(barrier.mc_ptr_at({id}), 1);
-        asm volatile ("{fence.proxy.alias;}" ::: "memory");
-        while (barrier_uc.load(cuda::memory_order_acquire) < NUM_DEVICES);
-        barrier_uc.fetch_sub(NUM_DEVICES, cuda::memory_order_release);
-    }
+__device__ static inline void signal_all(const barrier_t &barrier, const coord<ducks::default_type> &idx, const int val) {
+    asm volatile("{multimem.red.release.sys.global.add.s32 [%0], %1;}" :: "l"(barrier.mc_ptr_at(idx)), "r"(val) : "memory");
+}
+
+__device__ static inline void wait(const barrier_t &barrier, const coord<ducks::default_type> &idx, const int dev_idx, const int expected) {
+    int val;
+    do {
+        asm volatile("{ld.relaxed.sys.global.s32 %0, [%1];}" : "=r"(val) : "l"(&barrier[dev_idx][idx]) : "memory");
+    } while (val != expected);
+}
+
+__device__ static inline void barrier(const barrier_t &barrier, const coord<ducks::default_type> &idx, const int dev_idx) {
+    signal_all(barrier, idx, 1);
+    wait(barrier, idx, dev_idx, NUM_DEVICES);
+    asm volatile("{red.release.sys.global.add.s32 [%0], %1;}" :: "l"(&barrier[dev_idx][idx]), "r"(-NUM_DEVICES) : "memory");
 }
 
 #endif
