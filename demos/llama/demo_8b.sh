85d01891344e4d555820c47752d6d34fdd6efded
Stuart Sul <ssstuartss@gmail.com>
2025-11-17 17:55:08 -0600

    Clean

--- demos/llama/demo_8b.sh
+++ demos/llama/demo_8b.sh
@@ -1,12 +1,9 @@
+#!/bin/bash
 
-
-CONFIG_DIR='../configs/'   
+CONFIG_DIR='./configs/'   
 
 CUDA_VISIBLE_DEVICES=0 python -Wignore demo_llama_hf.py \
     --model_config_path ${CONFIG_DIR}/llama_3.1_8b_model_config.yaml \
     --num_generations 1 \
     --max_new_tokens 50  \
     --model_type tk_attention 
-    
-
-
