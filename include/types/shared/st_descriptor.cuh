810c4de3631e915b5c23da6b04a502ef08cf9bc2
Stuart Sul <ssstuartss@gmail.com>
2025-11-22 01:00:33 +0000

    on the way

--- include/types/shared/st_descriptor.cuh
+++ include/types/shared/st_descriptor.cuh
@@ -19,9 +19,64 @@ struct identifier {};
 }
 
 namespace detail {
-// see https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#asynchronous-warpgroup-level-matrix-shared-memory-layout-matrix-descriptor
+
+// See https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#asynchronous-warpgroup-level-matrix-shared-memory-layout-matrix-descriptor
 __device__ static inline uint64_t matrix_descriptor_encode(uint64_t x) { return (((x) & 0x3FFFF) >> 0x4); }
+
+template<int width, int height, bool transpose, bool swizzle>
+__device__ static inline uint64_t st_descriptor_raw(uint64_t addr) {
+#ifdef KITTENS_BLACKWELL
+    // see https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#tcgen05-shared-memory-descriptor
+    uint64_t desc = matrix_descriptor_encode(addr) | (1llu<<46); // needed for blackwell shared memory descriptors
+#else
+    uint64_t desc = matrix_descriptor_encode(addr);
+#endif
+    if constexpr (transpose) { // transpose mode
+        if constexpr (!swizzle) {
+            // desc |= matrix_descriptor_encode(??) << 16;
+            // desc |= matrix_descriptor_encode(??) << 32;
+            // desc |= 0llu << 62; // set wgmma_swizzle mode
+        } else if constexpr (width%4 == 0) {
+            desc |= matrix_descriptor_encode((uint64_t)2048*height) << 16;
+            desc |= matrix_descriptor_encode((uint64_t)1024) << 32;
+            desc |= 1llu << 62; // set wgmma_swizzle mode
+        }
+        else if constexpr (width%2 == 0) {
+            desc |= matrix_descriptor_encode((uint64_t)1024*height) << 16;
+            desc |= matrix_descriptor_encode((uint64_t)512) << 32;
+            desc |= 2llu << 62; // set wgmma_swizzle mode
+        }
+        else {
+            desc |= matrix_descriptor_encode((uint64_t)512*height) << 16;
+            desc |= matrix_descriptor_encode((uint64_t)256) << 32;
+            desc |= 3llu << 62; // set wgmma_swizzle mode
+        }
+    }
+    else { // normal mode
+        if constexpr (!swizzle) {
+            // desc |= matrix_descriptor_encode(??) << 16;
+            // desc |= matrix_descriptor_encode(??) << 32;
+            // desc |= 0llu << 62; // set wgmma_swizzle mode
+        } else if constexpr (width%4 == 0) {
+            desc |= matrix_descriptor_encode((uint64_t)16) << 16;   // this line doesn't matter
+            desc |= matrix_descriptor_encode((uint64_t)1024) << 32; // 128 byte swizzle x 8 for core matrix rows
+            desc |= 1llu << 62; // set wgmma_swizzle mode
+        }
+        else if constexpr (width%2 == 0) {
+            desc |= matrix_descriptor_encode((uint64_t)16) << 16;  // this line doesn't matter
+            desc |= matrix_descriptor_encode((uint64_t)512) << 32; // 64 byte swizzle x 8 for core matrix rows
+            desc |= 2llu << 62; // set wgmma_swizzle mode
+        }
+        else {
+            desc |= matrix_descriptor_encode((uint64_t)16) << 16;  // this line doesn't matter
+            desc |= matrix_descriptor_encode((uint64_t)256) << 32; // 32 byte swizzle x 8 for core matrix rows
+            desc |= 3llu << 62; // set wgmma_swizzle mode
+        }
+    }
+    return desc;
 }
+
+} // namespace detail
 
 template<kittens::ducks::st::all _ST, int transpose>
 struct st_descriptor {
@@ -31,55 +86,14 @@ struct st_descriptor {
     static constexpr int width  = ST::width;
     using T = ST::T;
     uint64_t base_desc;
-    __device__ inline st_descriptor(const ST &tile) {
-        static_assert(ST::swizzle, "Non-swizzled MMA shard descriptors are not supported yet.");
-#ifdef KITTENS_BLACKWELL
-        base_desc = detail::matrix_descriptor_encode((uint64_t)(&tile.data[0])) | (1llu<<46); // needed for blackwell shared memory descriptors.
-#else
-        base_desc = detail::matrix_descriptor_encode((uint64_t)(&tile.data[0]));
-#endif
-        if constexpr (transpose) { // transpose mode
-            if constexpr (ST::width%4 == 0) {
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)2048*ST::height) << 16;
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)1024) << 32;
-                base_desc |= 1llu << 62; // set wgmma_swizzle mode
-            }
-            else if constexpr (ST::width%2 == 0) {
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)1024*ST::height) << 16;
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)512) << 32;
-                base_desc |= 2llu << 62; // set wgmma_swizzle mode
-            }
-            else {
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)512*ST::height) << 16;
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)256) << 32;
-                base_desc |= 3llu << 62; // set wgmma_swizzle mode
-            }
-        }
-        else { // normal mode
-            if constexpr (ST::width%4 == 0) {
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)16) << 16;   // this line doesn't matter
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)1024) << 32; // 128 byte swizzle x 8 for core matrix rows
-                base_desc |= 1llu << 62; // set wgmma_swizzle mode
-            }
-            else if constexpr (ST::width%2 == 0) {
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)16) << 16;  // this line doesn't matter
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)512) << 32; // 64 byte swizzle x 8 for core matrix rows
-                base_desc |= 2llu << 62; // set wgmma_swizzle mode
-            }
-            else {
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)16) << 16;  // this line doesn't matter
-                base_desc |= detail::matrix_descriptor_encode((uint64_t)256) << 32; // 32 byte swizzle x 8 for core matrix rows
-                base_desc |= 3llu << 62; // set wgmma_swizzle mode
-            }
-        }
-    }
-    __device__ inline st_descriptor(const st_descriptor<ST, transpose> &other) : base_desc(other.base_desc) {
-        static_assert(ST::swizzle, "Non-swizzled MMA shard descriptors are not supported yet.");
-    } // copy constructor
+    __device__ inline st_descriptor(const ST &tile) : base_desc(detail::st_descriptor_raw<ST::width, ST::height, transpose, ST::swizzle>((uint64_t)(&tile.data[0]))) {}
+    __device__ inline st_descriptor(const st_descriptor<ST, transpose> &other) : base_desc(other.base_desc) {} // copy constructor
     __device__ inline uint64_t chunk_descriptor(int chunk_idx) {
-        static_assert(ST::swizzle, "Non-swizzled MMA shard descriptors are not supported yet.");
         if constexpr (transpose) { // transpose mode
-            if constexpr (ST::width%4 == 0) {
+            if constexpr (!ST::swizzle) {
+                // return base_desc + detail::matrix_descriptor_encode(chunk_idx*??);
+                return 0;
+            } else if constexpr (ST::width%4 == 0) {
                 return base_desc + detail::matrix_descriptor_encode(chunk_idx*2048);
             }
             else if constexpr (ST::width%2 == 0) {
@@ -90,7 +104,10 @@ struct st_descriptor {
             }
         }
         else { // normal mode
-            if constexpr (ST::width%4 == 0) {
+            if constexpr (!ST::swizzle) {
+                // return base_desc + detail::matrix_descriptor_encode(chunk_idx*??);
+                return 0;
+            } else if constexpr (ST::width%4 == 0) {
                 return base_desc + detail::matrix_descriptor_encode((chunk_idx%4)*32 + (chunk_idx/4)*ST::height*2048);
             }
             else if constexpr (ST::width%2 == 0) {
