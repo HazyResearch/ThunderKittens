45e7d8dd3965d058698b95cb701694f99e62ecd9
Stuart Sul <ssstuartss@gmail.com>
2025-12-09 21:59:19 +0000

    Opt

--- include/common/util.cuh
+++ include/common/util.cuh
@@ -329,14 +329,25 @@ __device__ static inline int cluster_ctarank() {
 }
 #endif
 
-template<int half> 
-__device__ static inline bool get_phasebit(uint32_t bitfield, int ring_id) {
-    return (bitfield & (1 << (half*16 + ring_id))) != 0;
+template<int half>
+__device__ static inline int get_phasebit(uint32_t bitfield, int ring_id) {
+    if constexpr (half == 0)
+        return (bitfield >> (ring_id)) & 0b1;
+    else if constexpr (half == 1)
+        return (bitfield >> (ring_id + 16)) & 0b1;
+    else
+        asm volatile ("brkpt;\n");
+    return -1;
 }
 
 template<int half> 
 __device__ static inline void update_phasebit(uint32_t &bitfield, int ring_id) {
-    bitfield ^= (1 << (half*16 + ring_id));
+    if constexpr (half == 0)
+        bitfield ^= (1 << (ring_id));
+    else if constexpr (half == 1)
+        bitfield ^= (1 << (ring_id + 16));
+    else
+        asm volatile ("brkpt;\n");
 }
 
 template<int N> __device__ static inline int ring_advance(int ring, int distance=1) { return (ring + distance) % N; }
